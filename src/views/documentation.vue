85b00f12eacce283d2bf6177dc205c7603689e2b
Antoni André <antoniandre.web@gmail.com>
2020-04-13 21:12:02 +1000

    Show-case buttons and add the first header background. To be changed.

--- src/views/documentation.vue
+++ src/views/documentation.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
 div
-  w-btn Welcome
+  w-btn(color="primary" dark) Welcome
+  w-btn(color="primary" dark x-small) x-small
+  w-btn(color="primary" dark small) small
+  w-btn(color="primary" dark medium) medium
+  w-btn(color="primary" dark large) large
+  w-btn(color="primary" dark x-large) x-large
 </template>
 
 <script>
@@ -10,3 +15,34 @@ export default {
   components: { WBtn }
 }
 </script>
+
+<style lang="scss">
+header {
+  position: relative;
+  height: 40vh;
+  background: url('~@/assets/the-great-wave-off.png') left bottom;
+  background-size: contain;
+  margin-bottom: 1rem;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 25vh;
+    right: 0;
+    // background: linear-gradient(to right, transparent 0%, #fff 15%);
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: url('~@/assets/wave-pattern.svg') bottom;
+    background-size: 45%;
+    z-index: -1;
+  }
+}
+</style>
