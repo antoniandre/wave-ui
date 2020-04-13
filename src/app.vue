a35e0b9dbaef108728d9c3d4ed25e636ae7808d5
Antoni André <antoniandre.web@gmail.com>
2020-04-13 21:08:33 +1000

    Add pug in app, and use the w-app component.

--- src/app.vue
+++ src/app.vue
@@ -1,32 +1,61 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+w-app(:colors="colors")
+  .toolbar
+    | Wave UI
+    .spacer
+    router-link(to="/") Home
+    router-link(to="/about") About
+  header
+  .content-wrap
+    router-view
 </template>
 
+<script>
+import WApp from '@/components/w-app'
+
+export default {
+  components: { WApp },
+  data: () => ({
+    colors: {
+      primary: '#234781'
+    }
+  })
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+$primary: #234781;
+$layout-padding: 1rem;
+
+* {margin: 0;padding: 0;}
+
+body {
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #333;
 }
 
-#nav {
-  padding: 30px;
+a {text-decoration: none;color: $primary;}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+// Wave UI framework classes.
+// --------------------------
+.primary {background-color: $primary;}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.spacer {flex-grow: 1;}
+
+.content-wrap {padding-left: $layout-padding;padding-right: $layout-padding;}
+
+.toolbar {
+  line-height: 3rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  background-color: #fff;
 }
 </style>
