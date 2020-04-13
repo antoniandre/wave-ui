4e3746c19d2e0fdd9a7124e2d751cca01aaca702
Antoni André <antoniandre.web@gmail.com>
2020-04-13 19:06:07 +1000

    Create the initial Wave-UI project!

---
+++ src/components/w-btn.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  button
+    slot
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+.home
+  w-btn Welcome
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import WBtn from '@/components/w-btn'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { WBtn }
 }
 </script>
