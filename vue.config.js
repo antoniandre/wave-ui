699a7ef126eb70ce1de95dfbe155d192b1433bb6
Antoni Andre <antoniandre.web@gmail.com>
2020-04-15 22:32:39 +1000

    Add an svg loader and font-awesome 4 package.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   // publicPath: '',
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .oneOf('inline')
+      .resourceQuery(/inline/)
+      .use('babel-loader')
+      .loader('babel-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]',
+      });
+  },
   css: {
     loaderOptions: {
       sass: {
-        prependData: `@import "@/wave-ui/scss/_variables.scss";`
+        prependData: `@import "@/wave-ui/scss/_variables.scss";@import "@/documentation/scss/_variables.scss";`
       }
     }
   }
