651a74ff61988a1de8a93e205f06ef13a8142453
Antoni Andre <antoniandre.web@gmail.com>
2020-04-18 22:49:49 +1000

    Expose WaveUI class at the root of vue.

--- src/wave-ui/index.js
+++ src/wave-ui/index.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import config, { mergeConfig } from './utils/config'
 import * as components from './components'
 // import * as directives from './directives'
@@ -27,12 +28,31 @@ class WaveUI {
   }
 
   constructor (options = {}) {
+    Vue.prototype.$waveUI = this
+
     // Merge user options into default config.
     mergeConfig(options)
 
     // Load the icons fonts on demand.
     if (config.icons.includes('fa')) require('font-awesome/css/font-awesome.min.css')
     if (config.icons.includes('ion')) require('ionicons/dist/css/ionicons.min.css')
+  }
+
+  mounted () {
+    //  Inject global dynamic CSS classes in document head.
+    if (!document.getElementById('wave-ui-styles')) {
+      const css = document.createElement('style')
+      css.id = 'wave-ui-styles'
+      css.innerHTML = ''
+
+      for (const color in config.colors) {
+        css.innerHTML += `
+          .w-app .${color} {background-color: ${config.colors[color]};}
+          .w-app .${color}--text {color: ${config.colors[color]};}
+        `
+      }
+      document.head.append(css)
+    }
   }
 }
 export default WaveUI
