b398233a3eb47f8bdeec2b5c6d6b7a5a1d404053
Antoni Andre <antoniandre.web@gmail.com>
2020-04-20 23:56:14 +1000

    Put the logic in w-app to not expose methods in $waveUI.

--- src/wave-ui/components/w-app.vue
+++ src/wave-ui/components/w-app.vue
@@ -4,14 +4,51 @@
 </template>
 
 <script>
+import config from '../utils/config'
+
+const breakpointsNames = Object.keys(config.breakpoints)
+const breakpointsValues = Object.values(config.breakpoints)
+
 export default {
   name: 'w-app',
   props: {
     dark: { type: Boolean, default: false }
   },
 
+  methods: {
+    // Most performant lookup.
+    getBreakpoint () {
+      const width = window.innerWidth
+      const breakpoints = breakpointsValues.slice(0)
+      breakpoints.push(width)
+      breakpoints.sort((a, b) => a - b)
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      this.$waveUI.breakpoint = breakpointsNames[breakpoints.indexOf(width)] || 'xl'
+    }
+  },
+
   mounted () {
-    this.$waveUI.mounted()
+    // Inject global dynamic CSS classes in document head.
+    if (!document.getElementById('wave-ui-styles')) {
+      const css = document.createElement('style')
+      css.id = 'wave-ui-styles'
+      css.innerHTML = ''
+
+      for (const color in config.colors) {
+        css.innerHTML += `
+          .w-app .${color} {background-color: ${config.colors[color]};}
+          .w-app .${color}--text {color: ${config.colors[color]};}
+        `
+      }
+      document.head.append(css)
+    }
+
+    this.getBreakpoint(window.innerWidth)
+    window.addEventListener('resize', this.getBreakpoint)
+  },
+
+  beforeDestroy () {
+    window.removeEventListener('resize', this.getBreakpoint)
   }
 }
 </script>
