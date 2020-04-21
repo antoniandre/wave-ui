677d3e6a5ad3cc26f3c99e27a9069696570564e6
Antoni Andre <antoniandre.web@gmail.com>
2020-04-21 15:13:12 +1000

    Make the list items selectable and hoverable.

--- src/documentation/views/ui-components/list.vue
+++ src/documentation/views/ui-components/list.vue
@@ -3,10 +3,13 @@ div
   h1.headline.mt-4 #[span.code w-list]
 
   h2 Default items list rendering
-  w-list.mt-6(:items="listItems")
+  w-list.mt-6(:items="listItems" color="primary")
 
-  h2 Custom rendering
-  w-list.mt-6(:items="listItems")
+  h2 Default items list rendering with selection
+  w-list.mt-6(v-model="selectedItem" :items="listItems" item-value="id" color="primary")
+  .subtitle.mt-2 Selected item: #[code {{ selectedItem }}]
+  h2 Custom rendering &amp; hoverable
+  w-list.mt-6(:items="listItems" color="primary" hover)
     template(v-slot:item="{ item }")
       .layout
         | {{ item.label }}
@@ -15,19 +18,20 @@ div
 
   h2 Navigation lists
   p The navigation list expects a #[code route] in the item to create a router link.
-  w-list.mt-6(:items="listItems" nav)
+  w-list.mt-6(:items="listItems" nav color="primary")
 </template>
 
 <script>
 export default {
   data: () => ({
     listItems: [
-      { label: 'Star', icon: 'fa fa-star', route: '#route-to-star' },
-      { label: 'check', icon: 'fa fa-check', route: '#route-to-check' },
-      { label: 'cross', icon: 'fa fa-close', route: '#route-to-cross' },
-      { label: 'info', icon: 'fa fa-info', route: '#route-to-info' },
-      { label: 'warning', icon: 'fa fa-warning', route: '#route-to-warning' }
-    ]
+      { label: 'Star', id: 'star', icon: 'fa fa-star', route: '#route-to-star' },
+      { label: 'Check', id: 'check', icon: 'fa fa-check', route: '#route-to-check' },
+      { label: 'Cross', id: 'cross', icon: 'fa fa-close', route: '#route-to-cross' },
+      { label: 'Info', id: 'info', icon: 'fa fa-info', route: '#route-to-info' },
+      { label: 'Warning', id: 'warning', icon: 'fa fa-warning', route: '#route-to-warning' }
+    ],
+    selectedItem: 'check'
   })
 }
 </script>
--- src/wave-ui/components/w-list.vue
+++ src/wave-ui/components/w-list.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   ul.w-list(:class="classes" :style="styles")
-    li.w-list__item(v-for="(item, index) in items")
+    li.w-list__item(
+      v-for="(item, index) in items"
+      @click="value !== false && (selectedItem = item[itemValue])"
+      :class="{ 'w-list__item--active': value !== false && selectedItem === item[itemValue] }")
       slot(name="item" :item="item" :index="index")
         router-link(v-if="nav && item.route" :to="item.route" v-html="item[itemLabel]")
         span(v-else v-html="item[itemLabel]")
@@ -11,17 +14,37 @@ export default {
   name: 'w-list',
   props: {
     items: { type: Array, required: true },
+    value: { default: false },
+    hover: { type: Boolean, default: false },
+    color: { type: String, default: null },
+    // Navigation type adds a router-link on items with `route`.
     nav: { type: Boolean, default: false },
     itemLabel: { type: String, default: 'label' },
+    itemValue: { type: String, default: 'value' }
   },
 
+  data: () => ({
+  }),
+
   computed: {
+    selectedItem: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    },
     classes () {
       return {
+        [`${this.color}--text`]: !!this.color,
+        'w-list--hoverable': this.hover !== false,
+        'w-list--selectable': this.value !== false
       }
     },
     styles () {
-      return false
+      return {
+      }
     }
   }
 }
@@ -33,7 +56,43 @@ export default {
   margin-left: 0;
 
   &__item {
-    padding: $base-increment;
+    position: relative;
+  }
+
+  &--hoverable &__item,
+  &--selectable &__item {
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      z-index: -1;
+      background-color: currentColor;
+      opacity: 0;
+      transition: 0.2s;
+    }
+
+    padding: 2 * $base-increment;
+    &:hover:before {opacity: 0.08;}
+  }
+
+  &--selectable &__item {
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      z-index: -2;
+      background-color: currentColor;
+      opacity: 0;
+      transition: 0.1s;
+    }
+    cursor: pointer;
+    &:active:after, &:focus:after, &--active:after {opacity: 0.2;}
   }
 }
 </style>
