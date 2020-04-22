07d672869602d70658bae59cee30863fcff35f82
Antoni Andre <antoniandre.web@gmail.com>
2020-04-23 00:49:35 +1000

    Add colors.

---
+++ src/documentation/views/colors.js
@@ -0,0 +1,369 @@
+export const colors = [
+  {
+    label: 'red',
+    color: '#f44336',
+    tones: [
+      { label: 'red lighten-5', color: '#ffebee' },
+      { label: 'red lighten-4', color: '#ffcdd2' },
+      { label: 'red lighten-3', color: '#ef9a9a' },
+      { label: 'red lighten-2', color: '#e57373' },
+      { label: 'red lighten-1', color: '#ef5350' },
+      { label: 'red darken-1', color: '#e53935' },
+      { label: 'red darken-2', color: '#d32f2f' },
+      { label: 'red darken-3', color: '#c62828' },
+      { label: 'red darken-4', color: '#b71c1c' },
+      { label: 'red accent-1', color: '#ff8a80' },
+      { label: 'red accent-2', color: '#ff5252' },
+      { label: 'red accent-3', color: '#ff1744' },
+      { label: 'red accent-4', color: '#d50000' }
+    ]
+  },
+
+  {
+    label: 'pink',
+    color: '#e91e63',
+    tones: [
+      { label: 'pink lighten-5', color: '#fce4ec' },
+      { label: 'pink lighten-4', color: '#f8bbd0' },
+      { label: 'pink lighten-3', color: '#f48fb1' },
+      { label: 'pink lighten-2', color: '#f06292' },
+      { label: 'pink lighten-1', color: '#ec407a' },
+      { label: 'pink darken-1', color: '#d81b60' },
+      { label: 'pink darken-2', color: '#c2185b' },
+      { label: 'pink darken-3', color: '#ad1457' },
+      { label: 'pink darken-4', color: '#880e4f' },
+      { label: 'pink accent-1', color: '#ff80ab' },
+      { label: 'pink accent-2', color: '#ff4081' },
+      { label: 'pink accent-3', color: '#f50057' },
+      { label: 'pink accent-4', color: '#c51162' }
+    ]
+  },
+
+  {
+    label: 'purple',
+    color: '#9c27b0',
+    tones: [
+      { label: 'purple lighten-5', color: '#f3e5f5' },
+      { label: 'purple lighten-4', color: '#e1bee7' },
+      { label: 'purple lighten-3', color: '#ce93d8' },
+      { label: 'purple lighten-2', color: '#ba68c8' },
+      { label: 'purple lighten-1', color: '#ab47bc' },
+      { label: 'purple darken-1', color: '#8e24aa' },
+      { label: 'purple darken-2', color: '#7b1fa2' },
+      { label: 'purple darken-3', color: '#6a1b9a' },
+      { label: 'purple darken-4', color: '#4a148c' },
+      { label: 'purple accent-1', color: '#ea80fc' },
+      { label: 'purple accent-2', color: '#e040fb' },
+      { label: 'purple accent-3', color: '#d500f9' },
+      { label: 'purple accent-4', color: '#aa00ff' }
+    ]
+  },
+
+  {
+    label: 'deep-purple',
+    color: '#673ab7',
+    tones: [
+      { label: 'deep-purple lighten-5', color: '#ede7f6' },
+      { label: 'deep-purple lighten-4', color: '#d1c4e9' },
+      { label: 'deep-purple lighten-3', color: '#b39ddb' },
+      { label: 'deep-purple lighten-2', color: '#9575cd' },
+      { label: 'deep-purple lighten-1', color: '#7e57c2' },
+      { label: 'deep-purple darken-1', color: '#5e35b1' },
+      { label: 'deep-purple darken-2', color: '#512da8' },
+      { label: 'deep-purple darken-3', color: '#4527a0' },
+      { label: 'deep-purple darken-4', color: '#311b92' },
+      { label: 'deep-purple accent-1', color: '#b388ff' },
+      { label: 'deep-purple accent-2', color: '#7c4dff' },
+      { label: 'deep-purple accent-3', color: '#651fff' },
+      { label: 'deep-purple accent-4', color: '#6200ea' }
+    ]
+  },
+
+  {
+    label: 'indigo',
+    color: '#3f51b5',
+    tones: [
+      { label: 'indigo lighten-5', color: '#e8eaf6' },
+      { label: 'indigo lighten-4', color: '#c5cae9' },
+      { label: 'indigo lighten-3', color: '#9fa8da' },
+      { label: 'indigo lighten-2', color: '#7986cb' },
+      { label: 'indigo lighten-1', color: '#5c6bc0' },
+      { label: 'indigo darken-1', color: '#3949ab' },
+      { label: 'indigo darken-2', color: '#303f9f' },
+      { label: 'indigo darken-3', color: '#283593' },
+      { label: 'indigo darken-4', color: '#1a237e' },
+      { label: 'indigo accent-1', color: '#8c9eff' },
+      { label: 'indigo accent-2', color: '#536dfe' },
+      { label: 'indigo accent-3', color: '#3d5afe' },
+      { label: 'indigo accent-4', color: '#304ffe' }
+    ]
+  },
+
+  {
+    label: 'blue',
+    color: '#2196f3',
+    tones: [
+      { label: 'blue lighten-5', color: '#e3f2fd' },
+      { label: 'blue lighten-4', color: '#bbdefb' },
+      { label: 'blue lighten-3', color: '#90caf9' },
+      { label: 'blue lighten-2', color: '#64b5f6' },
+      { label: 'blue lighten-1', color: '#42a5f5' },
+      { label: 'blue darken-1', color: '#1e88e5' },
+      { label: 'blue darken-2', color: '#1976d2' },
+      { label: 'blue darken-3', color: '#1565c0' },
+      { label: 'blue darken-4', color: '#0d47a1' },
+      { label: 'blue accent-1', color: '#82b1ff' },
+      { label: 'blue accent-2', color: '#448aff' },
+      { label: 'blue accent-3', color: '#2979ff' },
+      { label: 'blue accent-4', color: '#2962ff' }
+    ]
+  },
+
+  {
+    label: 'light-blue',
+    color: '#03a9f4',
+    tones: [
+      { label: 'light-blue lighten-5', color: '#e1f5fe' },
+      { label: 'light-blue lighten-4', color: '#b3e5fc' },
+      { label: 'light-blue lighten-3', color: '#81d4fa' },
+      { label: 'light-blue lighten-2', color: '#4fc3f7' },
+      { label: 'light-blue lighten-1', color: '#29b6f6' },
+      { label: 'light-blue darken-1', color: '#039be5' },
+      { label: 'light-blue darken-2', color: '#0288d1' },
+      { label: 'light-blue darken-3', color: '#0277bd' },
+      { label: 'light-blue darken-4', color: '#01579b' },
+      { label: 'light-blue accent-1', color: '#80d8ff' },
+      { label: 'light-blue accent-2', color: '#40c4ff' },
+      { label: 'light-blue accent-3', color: '#00b0ff' },
+      { label: 'light-blue accent-4', color: '#0091ea' }
+    ]
+  },
+
+  {
+    label: 'cyan',
+    color: '#00bcd4',
+    tones: [
+      { label: 'cyan lighten-5', color: '#e0f7fa' },
+      { label: 'cyan lighten-4', color: '#b2ebf2' },
+      { label: 'cyan lighten-3', color: '#80deea' },
+      { label: 'cyan lighten-2', color: '#4dd0e1' },
+      { label: 'cyan lighten-1', color: '#26c6da' },
+      { label: 'cyan darken-1', color: '#00acc1' },
+      { label: 'cyan darken-2', color: '#0097a7' },
+      { label: 'cyan darken-3', color: '#00838f' },
+      { label: 'cyan darken-4', color: '#006064' },
+      { label: 'cyan accent-1', color: '#84ffff' },
+      { label: 'cyan accent-2', color: '#18ffff' },
+      { label: 'cyan accent-3', color: '#00e5ff' },
+      { label: 'cyan accent-4', color: '#00b8d4' }
+    ]
+  },
+
+  {
+    label: 'teal',
+    color: '#009688',
+    tones: [
+      { label: 'teal lighten-5', color: '#e0f2f1' },
+      { label: 'teal lighten-4', color: '#b2dfdb' },
+      { label: 'teal lighten-3', color: '#80cbc4' },
+      { label: 'teal lighten-2', color: '#4db6ac' },
+      { label: 'teal lighten-1', color: '#26a69a' },
+      { label: 'teal darken-1', color: '#00897b' },
+      { label: 'teal darken-2', color: '#00796b' },
+      { label: 'teal darken-3', color: '#00695c' },
+      { label: 'teal darken-4', color: '#004d40' },
+      { label: 'teal accent-1', color: '#a7ffeb' },
+      { label: 'teal accent-2', color: '#64ffda' },
+      { label: 'teal accent-3', color: '#1de9b6' },
+      { label: 'teal accent-4', color: '#00bfa5' }
+    ]
+  },
+
+  {
+    label: 'green',
+    color: '#4caf50',
+    tones: [
+      { label: 'green lighten-5', color: '#e8f5e9' },
+      { label: 'green lighten-4', color: '#c8e6c9' },
+      { label: 'green lighten-3', color: '#a5d6a7' },
+      { label: 'green lighten-2', color: '#81c784' },
+      { label: 'green lighten-1', color: '#66bb6a' },
+      { label: 'green darken-1', color: '#43a047' },
+      { label: 'green darken-2', color: '#388e3c' },
+      { label: 'green darken-3', color: '#2e7d32' },
+      { label: 'green darken-4', color: '#1b5e20' },
+      { label: 'green accent-1', color: '#b9f6ca' },
+      { label: 'green accent-2', color: '#69f0ae' },
+      { label: 'green accent-3', color: '#00e676' },
+      { label: 'green accent-4', color: '#00c853' }
+    ]
+  },
+
+  {
+    label: 'light-green',
+    color: '#8bc34a',
+    tones: [
+      { label: 'light-green lighten-5', color: '#f1f8e9' },
+      { label: 'light-green lighten-4', color: '#dcedc8' },
+      { label: 'light-green lighten-3', color: '#c5e1a5' },
+      { label: 'light-green lighten-2', color: '#aed581' },
+      { label: 'light-green lighten-1', color: '#9ccc65' },
+      { label: 'light-green darken-1', color: '#7cb342' },
+      { label: 'light-green darken-2', color: '#689f38' },
+      { label: 'light-green darken-3', color: '#558b2f' },
+      { label: 'light-green darken-4', color: '#33691e' },
+      { label: 'light-green accent-1', color: '#ccff90' },
+      { label: 'light-green accent-2', color: '#b2ff59' },
+      { label: 'light-green accent-3', color: '#76ff03' },
+      { label: 'light-green accent-4', color: '#64dd17' }
+    ]
+  },
+
+  {
+    label: 'lime',
+    color: '#cddc39',
+    tones: [
+      { label: 'lime lighten-5', color: '#f9fbe7' },
+      { label: 'lime lighten-4', color: '#f0f4c3' },
+      { label: 'lime lighten-3', color: '#e6ee9c' },
+      { label: 'lime lighten-2', color: '#dce775' },
+      { label: 'lime lighten-1', color: '#d4e157' },
+      { label: 'lime darken-1', color: '#c0ca33' },
+      { label: 'lime darken-2', color: '#afb42b' },
+      { label: 'lime darken-3', color: '#9e9d24' },
+      { label: 'lime darken-4', color: '#827717' },
+      { label: 'lime accent-1', color: '#f4ff81' },
+      { label: 'lime accent-2', color: '#eeff41' },
+      { label: 'lime accent-3', color: '#c6ff00' },
+      { label: 'lime accent-4', color: '#aeea00' }
+    ]
+  },
+
+  {
+    label: 'yellow',
+    color: '#ffeb3b',
+    tones: [
+      { label: 'yellow lighten-5', color: '#fffde7' },
+      { label: 'yellow lighten-4', color: '#fff9c4' },
+      { label: 'yellow lighten-3', color: '#fff59d' },
+      { label: 'yellow lighten-2', color: '#fff176' },
+      { label: 'yellow lighten-1', color: '#ffee58' },
+      { label: 'yellow darken-1', color: '#fdd835' },
+      { label: 'yellow darken-2', color: '#fbc02d' },
+      { label: 'yellow darken-3', color: '#f9a825' },
+      { label: 'yellow darken-4', color: '#f57f17' },
+      { label: 'yellow accent-1', color: '#ffff8d' },
+      { label: 'yellow accent-2', color: '#ffff00' },
+      { label: 'yellow accent-3', color: '#ffea00' },
+      { label: 'yellow accent-4', color: '#ffd600' }
+    ]
+  },
+
+  {
+    label: 'amber',
+    color: '#ffc107',
+    tones: [
+      { label: 'amber lighten-5', color: '#fff8e1' },
+      { label: 'amber lighten-4', color: '#ffecb3' },
+      { label: 'amber lighten-3', color: '#ffe082' },
+      { label: 'amber lighten-2', color: '#ffd54f' },
+      { label: 'amber lighten-1', color: '#ffca28' },
+      { label: 'amber darken-1', color: '#ffb300' },
+      { label: 'amber darken-2', color: '#ffa000' },
+      { label: 'amber darken-3', color: '#ff8f00' },
+      { label: 'amber darken-4', color: '#ff6f00' },
+      { label: 'amber accent-1', color: '#ffe57f' },
+      { label: 'amber accent-2', color: '#ffd740' },
+      { label: 'amber accent-3', color: '#ffc400' },
+      { label: 'amber accent-4', color: '#ffab00' }
+    ]
+  },
+
+  {
+    label: 'orange',
+    color: '#ff9800',
+    tones: [
+      { label: 'orange lighten-5', color: '#fff3e0' },
+      { label: 'orange lighten-4', color: '#ffe0b2' },
+      { label: 'orange lighten-3', color: '#ffcc80' },
+      { label: 'orange lighten-2', color: '#ffb74d' },
+      { label: 'orange lighten-1', color: '#ffa726' },
+      { label: 'orange darken-1', color: '#fb8c00' },
+      { label: 'orange darken-2', color: '#f57c00' },
+      { label: 'orange darken-3', color: '#ef6c00' },
+      { label: 'orange darken-4', color: '#e65100' },
+      { label: 'orange accent-1', color: '#ffd180' },
+      { label: 'orange accent-2', color: '#ffab40' },
+      { label: 'orange accent-3', color: '#ff9100' },
+      { label: 'orange accent-4', color: '#ff6d00' }
+    ]
+  },
+
+  {
+    label: 'deep-orange',
+    color: '#ff5722',
+    tones: [
+      { label: 'deep-orange lighten-5', color: '#fbe9e7' },
+      { label: 'deep-orange lighten-4', color: '#ffccbc' },
+      { label: 'deep-orange lighten-3', color: '#ffab91' },
+      { label: 'deep-orange lighten-2', color: '#ff8a65' },
+      { label: 'deep-orange lighten-1', color: '#ff7043' },
+      { label: 'deep-orange darken-1', color: '#f4511e' },
+      { label: 'deep-orange darken-2', color: '#e64a19' },
+      { label: 'deep-orange darken-3', color: '#d84315' },
+      { label: 'deep-orange darken-4', color: '#bf360c' },
+      { label: 'deep-orange accent-1', color: '#ff9e80' },
+      { label: 'deep-orange accent-2', color: '#ff6e40' },
+      { label: 'deep-orange accent-3', color: '#ff3d00' },
+      { label: 'deep-orange accent-4', color: '#dd2c00' }
+    ]
+  },
+
+  {
+    label: 'brown',
+    color: '#795548',
+    tones: [
+      { label: 'brown lighten-5', color: '#efebe9' },
+      { label: 'brown lighten-4', color: '#d7ccc8' },
+      { label: 'brown lighten-3', color: '#bcaaa4' },
+      { label: 'brown lighten-2', color: '#a1887f' },
+      { label: 'brown lighten-1', color: '#8d6e63' },
+      { label: 'brown darken-1', color: '#6d4c41' },
+      { label: 'brown darken-2', color: '#5d4037' },
+      { label: 'brown darken-3', color: '#4e342e' },
+      { label: 'brown darken-4', color: '#3e2723' }
+    ]
+  },
+
+  {
+    label: 'blue-grey',
+    color: '#607d8b',
+    tones: [
+      { label: 'blue-grey lighten-5', color: '#eceff1' },
+      { label: 'blue-grey lighten-4', color: '#cfd8dc' },
+      { label: 'blue-grey lighten-3', color: '#b0bec5' },
+      { label: 'blue-grey lighten-2', color: '#90a4ae' },
+      { label: 'blue-grey lighten-1', color: '#78909c' },
+      { label: 'blue-grey darken-1', color: '#546e7a' },
+      { label: 'blue-grey darken-2', color: '#455a64' },
+      { label: 'blue-grey darken-3', color: '#37474f' },
+      { label: 'blue-grey darken-4', color: '#263238' }
+    ]
+  },
+
+  {
+    label: 'grey',
+    color: '#9e9e9e',
+    tones: [
+      { label: 'grey lighten-5', color: '#fafafa' },
+      { label: 'grey lighten-4', color: '#f5f5f5' },
+      { label: 'grey lighten-3', color: '#eeeeee' },
+      { label: 'grey lighten-2', color: '#e0e0e0' },
+      { label: 'grey lighten-1', color: '#bdbdbd' },
+      { label: 'grey darken-1', color: '#757575' },
+      { label: 'grey darken-2', color: '#616161' },
+      { label: 'grey darken-3', color: '#424242' },
+      { label: 'grey darken-4', color: '#212121' }
+    ]
+  }
+]
--- src/documentation/views/colors.vue
+++ src/documentation/views/colors.vue
@@ -1,32 +1,63 @@
 <template lang="pug">
 div
   h1.headline.mt-4 Colors
-  p.
-    red
-    pink
-    purple
-    deepPurple
-    indigo
-    blue
-    lightBlue
-    cyan
-    teal
-    green
-    lightGreen
-    lime
-    yellow
-    amber
-    orange
-    deepOrange
-    brown
-    blueGrey
-    grey
-    white
-    black
 
+  h2.mt-4 Color Palette
+  .layout.wrap.text-center.code
+    .color.align-center.ma-1(
+      v-for="({ color, label, tones }, i) in colors"
+      :key="i"
+      :class="label")
+      .title.mt-2.mb-0 {{ label }}
+      div.mb-2 {{ color }}
+      .color.color--tone.layout.column.align-center.px-4(
+        v-for="({ color, label }, i) in tones"
+        :key="i"
+        :class="label")
+        strong.pt-2.pb-1 {{ label }}
+        | {{ color }}
+
+    .color.align-center.ma-1
+      .color.color--tone.layout.column.align-center.px-4.ma-1.black
+          strong.pt-2.pb-1 black
+          | #000
+      .color.color--tone.layout.column.align-center.px-4.ma-1.white.black--text
+          strong.pt-2.pb-1 white
+          | #fff
+      .color.color--tone.layout.column.align-center.px-4.ma-1.transparent.black--text
+          strong.pt-2.pb-1 transparent
+          | transparent
 </template>
 
 <script>
+import { colors } from './colors'
+
 export default {
+  data: () => ({
+    colors
+  })
 }
 </script>
+
+<style lang="scss">
+.page--colors {
+  .color {
+    font-size: 0.9em;
+    padding: 2px;
+    color: #fff;
+    text-shadow: 0 0 1px rgba(0, 0, 0, 0.25), 0 0 6px rgba(0, 0, 0, 0.2);
+
+    &--tone {
+      min-width: 180px;
+      height: 50px;
+      color: rgba(0, 0, 0, 0.9);
+      text-shadow: none;
+    }
+
+    &.lime, &.yellow,
+    &.black--text {text-shadow: none;color: black;}
+    &.black {text-shadow: none;color: white;}
+    &.white, &.transparent {border: 1px solid #999;}
+  }
+}
+</style>
