6f0ab71a9daaf912a810a60a25601a48cb12f26b
Antoni Andre <antoniandre.web@gmail.com>
2020-04-23 00:51:58 +1000

    Update readme and intro text.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Wave UI - NOT YET READY.
 
-An emerging UI framework for Vue.js with only the bright side.
+An emerging UI framework for Vue.js with only the bright side. `:bulb:`
 
-Like other UI frameworks, it is very flexible and easy to use. But Wave UI is also:
+Like other UI frameworks, Wave UI is very flexible and easy to use. But it is also:
 
 - Lightweight
 - Very easy to override via CSS
 - No `!important` everywhere
-- No useless DOM wrappers inside UI elements
+- No useless DOM wrappers inside UI elements, really.
 - A single `$base-font-size` variable rules all the sizes and spacings
+- Powerful elements, e.g. write a navigation menu in a single line
 
 
 ## install - Not yet ready.
--- src/documentation/views/getting-started.vue
+++ src/documentation/views/getting-started.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
 div
-  h1.headline
-    | Wave UI
-  P An emerging new UI framework for Vue.js with only the bright side.
-  p Like other UI frameworks, it is very flexible and easy to use. But Wave UI is also:
+  h1.headline Wave UI
+  P An emerging new UI framework for Vue.js with only the bright side. #[code :bulb:]
+  p Like other UI frameworks, Wave UI is very flexible and easy to use. But it is also:
   ul
     li Lightweight
     li Very easy to override via CSS
     li No #[span.code !important] everywhere
-    li No useless DOM wrappers inside UI elements
+    li No useless DOM wrappers inside UI elements, really.
     li A single #[span.code $base-font-size] variable rules all the sizes and spacings
+    li Powerful elements, e.g. write a navigation menu in a single line
+
+  h2 Installation
+  npm i --D wave-ui
 
   h2 Presets
   h3 Buttons
