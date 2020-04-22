7d537e2e3ed5a321f99065e2048c2bdc5c9e550d
Antoni André <antoniandre.web@gmail.com>
2020-04-22 21:33:48 +1000

    Add w-card component.

--- src/wave-ui/components/w-card.vue
+++ src/wave-ui/components/w-card.vue
@@ -1,17 +1,34 @@
 <template lang="pug">
   .w-card(:class="classes" :style="styles")
-    slot
+    .w-card__title(v-if="cardTitle" v-html="cardTitle" :class="titleClass || false")
+    .w-card__title(v-else-if="$slots.title" :class="titleClass || false")
+      slot(name="title")
+    .w-card__content(:class="contentClass || false")
+      slot
+    .w-card__actions
+      slot(name="actions")
 </template>
 
 <script>
 export default {
   name: 'w-card',
   props: {
+    color: { type: String, default: '' },
+    cardTitle: { type: String, default: '' },
+    shadow: { type: Boolean, default: false },
+    border: { type: Boolean, default: true },
+    tile: { type: Boolean, default: false },
+    titleClass: { type: String, default: '' },
+    contentClass: { type: String, default: '' },
   },
 
   computed: {
     classes () {
       return {
+        [this.color]: !!this.color,
+        'w-card--no-border': !this.border,
+        'w-card--tile': this.tile,
+        'w-card--shadow': this.shadow
       }
     },
     styles () {
@@ -23,5 +40,21 @@ export default {
 
 <style lang="scss">
 .w-card {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  &:not(&--tile) {border-radius: $border-radius;}
+  &--shadow {box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);}
+  &:not(&--no-border):not(&--shadow) {border: 1px solid rgba(0, 0, 0, 0.15);}
+
+  &__title {
+    padding: (2 * $base-increment) (4 * $base-increment);
+  }
+  &__content {
+    display: flex;
+    flex: 1 1 auto;
+    padding: 4 * $base-increment;
+  }
 }
 </style>
