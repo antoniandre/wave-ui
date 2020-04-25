fc28443d278cd16825a1c4b2cbc5483cb6825b25
Antoni Andre <antoniandre.web@gmail.com>
2020-04-25 17:04:40 +1000

    w-overlay accepts color, closeOnClick & has a Vue transition.

--- src/wave-ui/components/w-overlay.vue
+++ src/wave-ui/components/w-overlay.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
-  .w-overlay(
-    @click="$event.target.classList.contains('w-overlay') && $emit('input', false)"
-    :class="{ 'w-overlay--show': value }"
-    :style="(value && styles) || false")
-    slot()
+  transition(name="fade" mode="out-in" appear)
+    .w-overlay(
+      v-if="value"
+      :style="(value && styles) || false"
+      @click="closeOnClick && $event.target.classList.contains('w-overlay') && $emit('input', false)")
+      slot
 </template>
 
 <script>
@@ -11,15 +12,20 @@ export default {
   name: 'w-overlay',
   props: {
     value: { type: Boolean, default: false },
-    opacity: { type: Number, default: null },
-    zIndex: { type: Number, default: null }
+    opacity: { type: [Number, String], default: null },
+    color: { type: String, default: null },
+    zIndex: { type: [Number, String], default: null },
+    closeOnClick: { type: Boolean, default: true }
   },
 
   computed: {
+    backgroundColor () {
+      return this.color || (this.opacity && `rgba(0, 0, 0, ${this.opacity})`) || false
+    },
     styles () {
       return {
-        backgroundColor: this.opacity ? `rgba(0, 0, 0, ${this.opacity})` : false,
-        zIndex: this.zIndex || false
+        backgroundColor: this.backgroundColor,
+        zIndex: this.zIndex || this.zIndex === 0 || false
       }
     }
   }
@@ -28,19 +34,12 @@ export default {
 
 <style lang="scss">
 .w-overlay {
+  z-index: 500;
   position: fixed;
   top: 0;
   left: 0;
   bottom: 0;
-  width: 0;
-  z-index: 499;
-  background-color: rgba(0, 0, 0, 0);
-  transition: background-color 0.3s, width 0s 0.3s;
-
-  &--show {
-    width: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    transition: background-color 0.3s;
-  }
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
 }
 </style>
