32d436b8c3cac60579ff038b48ad7ef788676e4d
Antoni Andre <antoniandre.web@gmail.com>
2020-04-25 17:11:40 +1000

    Update w-overlay documentation.

--- src/documentation/views/ui-components/overlay.vue
+++ src/documentation/views/ui-components/overlay.vue
@@ -2,11 +2,42 @@
 div
   h1.headline.mt-4 #[span.code w-overlay]
 
+  p.
+    By default the overlay has a #[span.code 0.3] opacity with a black color and a z-index
+    of #[span.code 500].#[br]
+    It is very easy to overrides these defaults via CSS, but you can also pass them as
+    parameters to the #[span.code w-overlay].
+
+  h2 Overlay with custom opacity and control on #[span.code closeOnClick]
+  .layout.wrap.align-center.mb-2
+    w-button.mr-2(color="primary" dark @click="showOverlay = true") Show overlay
+    w-button.mr-2(
+      :color="closeOnClick ? 'green' : 'red'"
+      dark
+      @click="closeOnClick = !closeOnClick") {{ closeOnClick ? 'Closing' : 'Not closing' }} on overlay click
+    | Overlay opacity
+    input.ml-2(v-model="opacity" type="number" step="0.1" min="0" max="1")
+
+  h2 Overlay with custom background color
+  w-button.mr-2(color="primary" dark @click="showBlueOverlay = true") Show a blue overlay
+
+  w-overlay.layout.align-center.justify-center(
+    v-model="showOverlay"
+    :close-on-click="closeOnClick"
+    :opacity="opacity")
+    w-button(color="primary" dark @click="showOverlay = false") Hide overlay
+  w-overlay.layout.align-center.justify-center(v-model="showBlueOverlay" color="rgba(35, 71, 129, 0.4)")
+    w-button(color="primary" dark @click="showBlueOverlay = false") Hide overlay
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    showOverlay: false,
+    showBlueOverlay: false,
+    opacity: 0.3,
+    closeOnClick: true
+  })
 }
 </script>
 
