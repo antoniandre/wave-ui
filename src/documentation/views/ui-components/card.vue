dae441e3fb28f073116c5a0f368202ec58b267df
Antoni Andre <antoniandre.web@gmail.com>
2020-04-26 16:50:24 +1000

    Show case w-card with shadow.

--- src/documentation/views/ui-components/card.vue
+++ src/documentation/views/ui-components/card.vue
@@ -38,6 +38,16 @@ div
       Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
       dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
       aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+
+  h2 Card with shadow
+  w-card.mt-4(shadow)
+    p.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+      odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
+      Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+      dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+      aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
     //- template(v-slot:actions)
     //- template(v-slot:title)
 </template>
