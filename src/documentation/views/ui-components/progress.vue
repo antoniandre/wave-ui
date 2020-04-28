b97b18c374f597ffd24573fa1382efc6e79e1809
Antoni Andre <antoniandre.web@gmail.com>
2020-04-28 20:59:29 +1000

    Update progress bar documentation.

--- src/documentation/views/ui-components/progress.vue
+++ src/documentation/views/ui-components/progress.vue
@@ -2,36 +2,37 @@
 div(style="max-width: 900px")
   h1.headline.mt-4 #[span.code w-progress]
 
-  h2 Default &amp; color
+  h2 Default indeterminate progress &amp; colors
   w-progress
-  w-progress.mt-4(value="38" color="blue")
-  w-progress.mt-4(value="38" color="green")
+  w-progress.mt-4(color="blue")
+  w-progress.mt-4(color="green")
   h3 Mixing colors
-  w-progress.mt-4(value="38" color="yellow" bg-color="cyan")
+  w-progress.mt-4(color="yellow" bg-color="cyan")
 
   h2 Size
   w-progress.mt-4(value="38" size="1em" color="blue")
   w-progress.mt-4(value="38" size="2.5em" color="blue")
 
   h2 Tile &amp; round
-  w-progress.mt-4(value="38" size="1.5em" tile color="light-blue")
-  w-progress.mt-4(value="38" size="1.5em" round color="light-blue")
+  w-progress.mt-4(value="38" size="1.4em" tile color="light-blue")
+  w-progress.mt-4(value="38" size="1.4em" round color="light-blue")
 
   h2 Outline &amp; shadow
   w-progress.mt-4(value="38" size="1em" outline round color="light-blue")
   w-progress.mt-4(value="38" size="1em" shadow color="light-blue")
 
-  h2 Stripes
+  h2 Stripes (only if determinate)
   w-progress.mt-4(value="38" size="1em" outline round color="light-blue" stripes)
-  w-progress.mt-4(value="38" size="1.5em" outline round color="primary" stripes)
+  w-progress.mt-4(value="38" size="1.4em" outline round color="primary" stripes)
 
-  h2 Indeterminate
-  //- w-progress.mt-4(size="1em" outline round color="primary" stripes)
-  w-progress.mt-4(size="1em" outline round color="primary")
+  //- h2 Indeterminate (can't have stripes)
+  //- w-progress.mt-4(size="1em" outline round color="primary")
+  //- w-progress.mt-4(size="1em" round color="primary")
 
   h2 Progress value with label
   h3 Default label
-  w-progress.mt-4(v-model="progress1" size="1.5em" outline round color="primary" label)
+  w-progress.mt-4(v-model="progress1" size="1.3em" round color="primary" label)
+  w-progress.mt-4(v-model="progress1" size="1.3em" outline round color="primary" label)
 
   h3 Custom label
   w-progress.mt-4(v-model="progress1" size="1.5em" outline round color="primary")
