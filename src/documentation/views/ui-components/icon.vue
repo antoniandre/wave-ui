913625a0752a9b7f68c274941b92ca222bd8f819
Antoni Andre <antoniandre.web@gmail.com>
2020-04-29 18:31:25 +1000

    Update Icons page.

--- src/documentation/views/ui-components/icon.vue
+++ src/documentation/views/ui-components/icon.vue
@@ -7,76 +7,94 @@ div
     You can also use your own font as long as you import it.#[br]
     You can use multiple icons sources at the same time but it is not recommended.
 
-  h2 Ionicons 4 (iOS &amp; Material Design)
+  h2 Ionicons 4 (iOS &amp; Material Design) - Free
   p
     a(href="https://ionicons.com/v4/cheatsheet.html" target="_blank") Ionicons V4 Cheatsheet
-  w-icon.mr-1(xs color="primary") ion-md-star
-  w-icon.mr-1(sm color="primary") ion-md-star
-  w-icon.mr-1(md color="primary") ion-md-star
-  w-icon.mr-1(lg color="primary") ion-md-star
-  w-icon.mr-1(xl color="primary") ion-md-star
-  w-icon.ml-8.mr-1(:size="50" color="primary") ion-md-star
-  w-icon(size="5.6em" color="primary") ion-md-star
-  br
-  w-icon.mr-1(xs color="primary") ion-md-star-outline
-  w-icon.mr-1(sm color="primary") ion-md-star-outline
-  w-icon.mr-1(md color="primary") ion-md-star-outline
-  w-icon.mr-1(lg color="primary") ion-md-star-outline
-  w-icon.mr-1(xl color="primary") ion-md-star-outline
-  br
-  w-icon.mr-1(xs color="primary") ion-md-heart
-  w-icon.mr-1(sm color="primary") ion-md-heart
-  w-icon.mr-1(md color="primary") ion-md-heart
-  w-icon.mr-1(lg color="primary") ion-md-heart
-  w-icon.mr-1(xl color="primary") ion-md-heart
-  br
-  w-icon.mr-1(xs color="primary") ion-md-heart-empty
-  w-icon.mr-1(sm color="primary") ion-md-heart-empty
-  w-icon.mr-1(md color="primary") ion-md-heart-empty
-  w-icon.mr-1(lg color="primary") ion-md-heart-empty
-  w-icon.mr-1(xl color="primary") ion-md-heart-empty
-  br
-  w-icon.mr-1(xs color="primary") ion-ios-heart
-  w-icon.mr-1(sm color="primary") ion-ios-heart
-  w-icon.mr-1(md color="primary") ion-ios-heart
-  w-icon.mr-1(lg color="primary") ion-ios-heart
-  w-icon.mr-1(xl color="primary") ion-ios-heart
-  br
-  w-icon.mr-1(xs color="primary") ion-ios-heart-empty
-  w-icon.mr-1(sm color="primary") ion-ios-heart-empty
-  w-icon.mr-1(md color="primary") ion-ios-heart-empty
-  w-icon.mr-1(lg color="primary") ion-ios-heart-empty
-  w-icon.mr-1(xl color="primary") ion-ios-heart-empty
 
-  h2 Font Awesome 4
+  .layout.mt-4.wrap
+    div.mr-8
+      w-icon.mr-1(xs color="primary") ion-md-star
+      w-icon.mr-1(sm color="primary") ion-md-star
+      w-icon.mr-1(md color="primary") ion-md-star
+      w-icon.mr-1(lg color="primary") ion-md-star
+      w-icon.mr-1(xl color="primary") ion-md-star
+      br
+      w-icon.mr-1(xs color="primary") ion-md-star-outline
+      w-icon.mr-1(sm color="primary") ion-md-star-outline
+      w-icon.mr-1(md color="primary") ion-md-star-outline
+      w-icon.mr-1(lg color="primary") ion-md-star-outline
+      w-icon.mr-1(xl color="primary") ion-md-star-outline
+      br
+      w-icon.mr-1(xs color="primary") ion-ios-star
+      w-icon.mr-1(sm color="primary") ion-ios-star
+      w-icon.mr-1(md color="primary") ion-ios-star
+      w-icon.mr-1(lg color="primary") ion-ios-star
+      w-icon.mr-1(xl color="primary") ion-ios-star
+      br
+      w-icon.mr-1(xs color="primary") ion-ios-star-outline
+      w-icon.mr-1(sm color="primary") ion-ios-star-outline
+      w-icon.mr-1(md color="primary") ion-ios-star-outline
+      w-icon.mr-1(lg color="primary") ion-ios-star-outline
+      w-icon.mr-1(xl color="primary") ion-ios-star-outline
+      br
+      w-icon.mr-1(xs color="primary") ion-md-heart
+      w-icon.mr-1(sm color="primary") ion-md-heart
+      w-icon.mr-1(md color="primary") ion-md-heart
+      w-icon.mr-1(lg color="primary") ion-md-heart
+      w-icon.mr-1(xl color="primary") ion-md-heart
+      br
+      w-icon.mr-1(xs color="primary") ion-md-heart-empty
+      w-icon.mr-1(sm color="primary") ion-md-heart-empty
+      w-icon.mr-1(md color="primary") ion-md-heart-empty
+      w-icon.mr-1(lg color="primary") ion-md-heart-empty
+      w-icon.mr-1(xl color="primary") ion-md-heart-empty
+      br
+      w-icon.mr-1(xs color="primary") ion-ios-heart
+      w-icon.mr-1(sm color="primary") ion-ios-heart
+      w-icon.mr-1(md color="primary") ion-ios-heart
+      w-icon.mr-1(lg color="primary") ion-ios-heart
+      w-icon.mr-1(xl color="primary") ion-ios-heart
+      br
+      w-icon.mr-1(xs color="primary") ion-ios-heart-empty
+      w-icon.mr-1(sm color="primary") ion-ios-heart-empty
+      w-icon.mr-1(md color="primary") ion-ios-heart-empty
+      w-icon.mr-1(lg color="primary") ion-ios-heart-empty
+      w-icon.mr-1(xl color="primary") ion-ios-heart-empty
+    div
+      w-icon.mr-1(:size="50" color="primary") ion-md-star
+      w-icon(size="5.6em" color="primary") ion-md-star
+
+  h2 Font Awesome 4 - Free
   p
     a(href="https://fontawesome.com/v4.7.0/icons/" target="_blank") Font Awesome V4 Cheatsheet
-  w-icon.mr-1(xs color="primary") fa fa-star
-  w-icon.mr-1(sm color="primary") fa fa-star
-  w-icon.mr-1(md color="primary") fa fa-star
-  w-icon.mr-1(lg color="primary") fa fa-star
-  w-icon.mr-1(xl color="primary") fa fa-star
-  br
-  w-icon.mr-1(xs color="primary") fa fa-star-o
-  w-icon.mr-1(sm color="primary") fa fa-star-o
-  w-icon.mr-1(md color="primary") fa fa-star-o
-  w-icon.mr-1(lg color="primary") fa fa-star-o
-  w-icon.mr-1(xl color="primary") fa fa-star-o
-  w-icon.ml-8.mr-1(:size="50" color="primary") fa fa-star
-  w-icon(size="5em" color="primary") fa fa-star
-  br
-  w-icon.mr-1(xs color="primary") fa fa-heart
-  w-icon.mr-1(sm color="primary") fa fa-heart
-  w-icon.mr-1(md color="primary") fa fa-heart
-  w-icon.mr-1(lg color="primary") fa fa-heart
-  w-icon.mr-1(xl color="primary") fa fa-heart
-  br
-  w-icon.mr-1(xs color="primary") fa fa-heart-o
-  w-icon.mr-1(sm color="primary") fa fa-heart-o
-  w-icon.mr-1(md color="primary") fa fa-heart-o
-  w-icon.mr-1(lg color="primary") fa fa-heart-o
-  w-icon.mr-1(xl color="primary") fa fa-heart-o
-
+  .layout.mt-4.wrap
+    div.mr-8
+      w-icon.mr-1(xs color="primary") fa fa-star
+      w-icon.mr-1(sm color="primary") fa fa-star
+      w-icon.mr-1(md color="primary") fa fa-star
+      w-icon.mr-1(lg color="primary") fa fa-star
+      w-icon.mr-1(xl color="primary") fa fa-star
+      br
+      w-icon.mr-1(xs color="primary") fa fa-star-o
+      w-icon.mr-1(sm color="primary") fa fa-star-o
+      w-icon.mr-1(md color="primary") fa fa-star-o
+      w-icon.mr-1(lg color="primary") fa fa-star-o
+      w-icon.mr-1(xl color="primary") fa fa-star-o
+      br
+      w-icon.mr-1(xs color="primary") fa fa-heart
+      w-icon.mr-1(sm color="primary") fa fa-heart
+      w-icon.mr-1(md color="primary") fa fa-heart
+      w-icon.mr-1(lg color="primary") fa fa-heart
+      w-icon.mr-1(xl color="primary") fa fa-heart
+      br
+      w-icon.mr-1(xs color="primary") fa fa-heart-o
+      w-icon.mr-1(sm color="primary") fa fa-heart-o
+      w-icon.mr-1(md color="primary") fa fa-heart-o
+      w-icon.mr-1(lg color="primary") fa fa-heart-o
+      w-icon.mr-1(xl color="primary") fa fa-heart-o
+    div
+      w-icon.mr-1(:size="50" color="primary") fa fa-star
+      w-icon(size="5em" color="primary") fa fa-star
 </template>
 
 <script>
