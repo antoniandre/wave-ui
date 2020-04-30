7ce0c0b46f87d2dc51859881c9bb7708044657b3
Antoni André <antoniandre.web@gmail.com>
2020-05-01 00:07:14 +1000

    Add behavior for the tag component.

--- src/documentation/views/ui-components/tag.vue
+++ src/documentation/views/ui-components/tag.vue
@@ -2,14 +2,24 @@
 div
   h1.headline.mt-4 #[span.code w-tag]
 
+  h2 Default tag
+  w-tag Test
+
+  h2 Colors
+  w-tag.mr-2(color="primary") Test
+  w-tag.mr-2(bg-color="primary" dark) Test
+  w-tag.mr-2(bg-color="primary" color="white") Test
+  h3 Mixing colors
+  w-tag.mr-2(bg-color="primary" color="yellow") Test
+
+  h2 Closable tag
+  w-tag(v-model="tagOn" close color="primary") Test
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    tagOn: true
+  })
 }
 </script>
-
-<style lang="scss">
-
-</style>
--- src/wave-ui/components/w-tag.vue
+++ src/wave-ui/components/w-tag.vue
@@ -1,17 +1,36 @@
 <template lang="pug">
-  .w-tag(:class="classes" :style="styles")
+  span.w-tag(:class="classes" :style="styles")
     slot
+    w-icon.w-tag__close(v-if="close") ion-md-close
 </template>
 
 <script>
 export default {
   name: 'w-tag',
   props: {
+    value: { type: [Number, String, Boolean], default: -1 },
+    color: { type: String, default: '' },
+    bgColor: { type: String, default: '' },
+    dark: { type: Boolean, default: false },
+    size: { type: String, default: '' },
+    shadow: { type: Boolean, default: false },
+    tile: { type: Boolean, default: false },
+    round: { type: Boolean, default: false },
+    close: { type: Boolean, default: false },
+    outline: { type: Boolean, default: false }
   },
 
   computed: {
     classes () {
       return {
+        [this.color]: this.color,
+        [`${this.bgColor}--bg`]: this.bgColor,
+        'w-tag--dark': this.dark && !this.outline,
+        'w-tag--clickable': this.value !== -1,
+        'w-tag--outline': this.outline,
+        'w-tag--tile': this.tile,
+        'w-tag--round': this.round,
+        'w-tag--shadow': this.shadow
       }
     },
     styles () {
@@ -23,5 +42,64 @@ export default {
 
 <style lang="scss">
 .w-tag {
+  display: inline-flex;
+  position: relative;
+  border-radius: $border-radius;
+  border: 1px solid rgba(0, 0, 0, 0.08);
+  background-color: rgba(255, 255, 255, 0.85);
+  padding-left: 2 * $base-increment;
+  padding-right: 2 * $base-increment;
+  font-size: round(0.85 * $base-font-size);
+
+  &--dark {color: rgba(255, 255, 255, 0.95);}
+  &--outline {background-color: transparent;border-color: currentColor;}
+  &--round {
+    border-radius: 12 * $base-increment;
+    padding-left: round(3 * $base-increment);
+    padding-right: round(3 * $base-increment);
+  }
+  &--tile {border-radius: initial;}
+  &--shadow {box-shadow: $box-shadow;}
+
+  .w-tag__close {line-height: inherit;}
+
+  &--clickable {
+    cursor: pointer;
+    user-select: none;
+
+    &:hover {}
+
+    // Overlay to mark the focus and active state.
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      opacity: 0;
+      background-color: transparent;
+      border-radius: inherit;
+      transition: 0.15s;
+    }
+
+    // Hover state.
+    &:hover:before {background-color: currentColor;opacity: 0.08;}
+    &--dark:hover:before {background-color: rgba(255, 255, 255, 0.15);opacity: 1;}
+    &--outline:hover:before,
+    &--text:hover:before {background-color: currentColor;opacity: 0.12;}
+
+    // Focus state.
+    &:focus:before {background-color: rgba(0, 0, 0, 0.15);opacity: 1;}
+    &--dark:focus:before {background-color: rgba(255, 255, 255, 0.15);}
+    &--outline:focus:before,
+    &--text:focus:before {background-color: currentColor;opacity: 0.12;}
+
+    // Active state.
+    &:active:before {background-color: rgba(0, 0, 0, 0.3);opacity: 1;}
+    &--dark:active:before {background-color: rgba(255, 255, 255, 0.3);}
+    &--outline:active:before,
+    &--text:active:before {background-color: currentColor;opacity: 0.25;}
+  }
 }
 </style>
