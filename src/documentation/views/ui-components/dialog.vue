43c32c2b2b1e8940ff70a4a24b3c70e524ae335a
Antoni André <antoniandre.web@gmail.com>
2020-05-09 17:09:57 +1000

    Start the dialog component.

--- src/documentation/views/ui-components/dialog.vue
+++ src/documentation/views/ui-components/dialog.vue
@@ -1,12 +1,26 @@
 <template lang="pug">
 div
   h1.headline.mt-4 #[span.code w-dialog]
+  w-button(@click="showDialog = !showDialog") Open dialog
+  w-checkbox(v-model="fullscreen" label="Fullscreen")
+  w-radio(v-model="fullscreen" label="Fullscreen")
+  p {{fullscreen}}
+
+  w-dialog(v-model="showDialog" :fullscreen="fullscreen")
+    template(v-slot:title) Title
+    div Content
+    template(v-slot:actions)
+      .spacer
+      w-button(@click="showDialog = false") Close
 
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    showDialog: false,
+    fullscreen: false
+  })
 }
 </script>
 
--- src/wave-ui/components/w-dialog.vue
+++ src/wave-ui/components/w-dialog.vue
@@ -1,17 +1,26 @@
 <template lang="pug">
-  .w-dialog(:class="classes" :style="styles")
-    slot
+  w-overlay.w-dialog(v-if="value" v-model="value" :class="classes" :style="styles")
+    w-card.w-dialog__content(no-border :class="{ 'grow fill-height': fullscreen }")
+      template(v-slot:title)
+        slot(name="title")
+      slot
+      template(v-slot:actions)
+        slot(name="actions")
 </template>
 
 <script>
 export default {
   name: 'w-dialog',
   props: {
+    value: { type: Boolean, default: false },
+    fullscreen: { type: Boolean, default: false },
+    tile: { type: Boolean, default: false },
   },
 
   computed: {
     classes () {
       return {
+        'w-dialog--fullscreen': this.fullscreen,
       }
     },
     styles () {
@@ -23,5 +32,15 @@ export default {
 
 <style lang="scss">
 .w-dialog {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 500;
+
+  &__content {
+    background-color: #fff;
+  }
 }
 </style>
