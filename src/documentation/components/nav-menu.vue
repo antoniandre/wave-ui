0d0af62fd449d7fe23b45fc555080a27994fd137
Antoni André <antoniandre.web@gmail.com>
2020-05-16 16:37:43 +1000

    Allow w-list nesting.

--- src/documentation/components/nav-menu.vue
+++ src/documentation/components/nav-menu.vue
@@ -14,6 +14,8 @@
       span(v-else v-html="item.label")
 
   .title.mt-4 UI Components
+
+  p {{components}}
   w-list(:items="components" nav color="primary")
     //- template(v-slot:item="{ item }")
       router-link(v-if="!item.disabled" :to="item.route" v-html="item.label")
@@ -26,38 +28,49 @@ export default {
     drawerOpen: { type: Boolean, default: false }
   },
 
-  data: () => ({
-    sections: [
-      { label: 'Getting started', route: { name: 'getting-started' } },
-      { label: 'Breakpoints', route: 'breakpoints' },
-      { label: 'Layout', route: 'layout' },
-      { label: 'Typography', route: 'typography' },
-      { label: 'Colors', route: 'colors' },
-    ],
-    components: [
-      { label: 'Accordion', route: 'w-accordion', disabled: true },
-      { label: 'Alert', route: 'w-alert' },
-      { label: 'App', route: 'w-app' },
-      { label: 'Button', route: 'w-button' },
-      { label: 'Calendar', route: 'w-calendar', disabled: true },
-      { label: 'Card', route: 'w-card' },
-      { label: 'Dialog', route: 'w-dialog' },
-      { label: 'Drawer', route: 'w-drawer' },
-      { label: 'Icon', route: 'w-icon' },
-      { label: 'List', route: 'w-list' },
-      { label: 'Menu', route: 'w-menu', disabled: true },
-      { label: 'Notification', route: 'w-notification', disabled: true },
-      { label: 'Overlay', route: 'w-overlay' },
-      { label: 'Progress', route: 'w-progress' },
-      { label: 'Rating', route: 'w-rating', disabled: true },
-      { label: 'Slideshow', route: 'w-slideshow', disabled: true },
-      { label: 'Tabs', route: 'w-tabs', disabled: true },
-      { label: 'Tag', route: 'w-tag' },
-      { label: 'Toolbar', route: 'w-toolbar' },
-      { label: 'Tooltip', route: 'w-tooltip', disabled: true },
-      { label: 'Form elements', route: '#form', disabled: true }
-    ]
-  })
+  data () {
+    return {
+      sections: [
+        { label: 'Getting started', route: { name: 'getting-started' } },
+        { label: 'Breakpoints', route: 'breakpoints' },
+        { label: 'Layout', route: 'layout' },
+        { label: 'Typography', route: 'typography' },
+        { label: 'Colors', route: 'colors' },
+      ],
+      components: [
+        { label: 'Accordion', route: 'w-accordion', disabled: true },
+        { label: 'Alert', route: 'w-alert' },
+        { label: 'App', route: 'w-app' },
+        { label: 'Button', route: 'w-button' },
+        { label: 'Calendar', route: 'w-calendar', disabled: true },
+        { label: 'Card', route: 'w-card' },
+        { label: 'Dialog', route: 'w-dialog' },
+        { label: 'Drawer', route: 'w-drawer' },
+        { label: 'Icon', route: 'w-icon' },
+        { label: 'List', route: 'w-list' },
+        { label: 'Menu', route: 'w-menu', disabled: true },
+        { label: 'Notification', route: 'w-notification', disabled: true },
+        { label: 'Overlay', route: 'w-overlay' },
+        { label: 'Progress', route: 'w-progress' },
+        { label: 'Rating', route: 'w-rating', disabled: true },
+        { label: 'Slideshow', route: 'w-slideshow', disabled: true },
+        { label: 'Tabs', route: 'w-tabs', disabled: true },
+        { label: 'Tag', route: 'w-tag' },
+        { label: 'Toolbar', route: 'w-toolbar' },
+        { label: 'Tooltip', route: 'w-tooltip', disabled: true },
+        {
+          label: 'Form elements',
+          route: '#form',
+          children: [
+            { label: 'Checkbox', route: 'w-checkbox' },
+            { label: 'Radio', route: 'w-radio' },
+            { label: 'Checkboxes', route: 'w-checkboxes' },
+            { label: 'Radios', route: 'w-radios' },
+          ]
+        }
+      ]
+    }
+  }
 }
 </script>
 
