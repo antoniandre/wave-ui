037a88470e8fbebc4f1e9dd6d5e139ca956fff53
Antoni Andre <antoniandre.web@gmail.com>
2020-05-18 14:59:14 +1000

    Add style on the w-radio component.

--- src/wave-ui/components/w-radio.vue
+++ src/wave-ui/components/w-radio.vue
@@ -3,9 +3,9 @@
     input(
       type="radio"
       :name="name"
-      :class="{ 'mr-2': hasLabel }"
       :checked="isChecked"
       @change="isChecked = !isChecked")
+    .w-radio__input(:class="{ 'mr-2': hasLabel }")
     template(v-if="hasLabel")
       slot {{ label }}
 </template>
@@ -17,7 +17,7 @@ export default {
     value: { type: Boolean, default: false }, // v-model to check or uncheck.
     name: { type: String, default: '' },
     label: { type: String, default: '' },
-    color: { type: String, default: null }
+    color: { type: String, default: 'primary' }
   },
 
   computed: {
@@ -43,6 +43,55 @@ export default {
 </script>
 
 <style lang="scss">
+$outline-width: 2px;
 .w-radio {
+  display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+
+  // The hidden real radio button.
+  input {
+    position: absolute;
+    opacity: 0;
+    z-index: -100;
+  }
+
+  // The fake radio button to substitute.
+  &__input {
+    position: relative;
+    border-radius: 100%;
+    width: round(1.3 * $base-font-size);
+    height: round(1.3 * $base-font-size);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: $outline-width solid currentColor;
+  }
+  &__input:before {
+    content: '';
+    border-radius: 100%;
+    border: 0 solid currentColor;
+    @include default-transition;
+  }
+  input:checked + &__input:before {
+    border-width: 4px;
+  }
+
+  // The focus outline.
+  &__input:after {
+    content: '';
+    position: absolute;
+    top: -$outline-width;
+    bottom: -$outline-width;
+    left: -$outline-width;
+    right: -$outline-width;
+    display: flex;
+    border-radius: 100%;
+    opacity: 0.25;
+    box-shadow: 0 0 0 0 currentColor;
+    @include default-transition;
+  }
+
+  input:focus + &__input:after {box-shadow: 0 0 0 5px currentColor;}
 }
 </style>
