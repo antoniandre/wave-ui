a5bb35f03a646fedf1612873014c3a1f9978235b
Antoni Andre <antoniandre.web@gmail.com>
2020-05-19 23:19:01 +1000

    Improve w-checkbox and start the documentation.

--- src/documentation/views/ui-components/checkbox.vue
+++ src/documentation/views/ui-components/checkbox.vue
@@ -1,15 +1,87 @@
 <template lang="pug">
 div
-  h1.headline.mt-4 #[span.code w-checkbox]
+  h1.headline.mt-4 #[span.code w-checkbox] &amp; #[span.code w-checkboxes]
+
+  h2 Multiple checkboxes using the #[code w-checkboxes] tag
+  p.
+    The #[code w-checkboxes] component allows a fast and easy rendering of
+    multiple checkboxes by passing the items through a prop.#[br]
+    It accepts an inline parameter to display the checkboxes inline,
+    and allows you to customize the label of each checkbox through slot.
+
+  h3 V-model
+  p.
+    If a value is set in the choice's data, it will be returned by the v-model when selected.#[br]
+    If no value is set, the choice's label will be returned instead.
+  .layout
+    w-checkboxes(v-model="selectedCheckbox1" :items="checkboxes1")
+    div.subtitle.ml-8 Selected choice: #[code {{ selectedCheckbox1 || 'null' }}]
+
+
+  h3 Default column layout
+  w-checkboxes(:items="checkboxes2")
+
+  h3 Inline layout
+  w-checkboxes(:items="checkboxes2" inline)
+
+  h3 Color
+  p.
+    By default the #[code w-checkboxes] &amp; #[code w-checkbox] components will
+    use the primary color.
+  w-checkboxes(:items="checkboxes2" color="purple")
+
+  h3 Custom label content
+  p.
+    #[code w-checkboxes] provides a #[code label] slot to customize the label of each checkbox
+    button.
+  w-checkboxes(:items="checkboxes3" color="green")
+    template(v-slot:label="{ item }")
+      span.pr-2 {{ item.label }}
+      w-icon.deep-purple {{ item.icon }}
+
+  w-divider.ma-10.ml-0
+
+  h2 Individual checkbox
+  p.
+    It is possible to use the #[code w-checkbox] component to render a single checkbox
+    at once.#[br]
+    A set of checkboxes will only work together if they have the same v-model,
+    or the same #[code name] attribute (standard HTML).
+
+  w-checkbox.mr-2(label="Choice 1" name="checkboxes2" color="success")
+  w-checkbox.mr-2(label="Choice 2" name="checkboxes2" color="warning")
+  w-checkbox(label="Choice 3" name="checkboxes2" color="error")
+
+  h3 Custom label content
+  p.
+    #[code w-checkbox] provides a default slot to customize the content.
+  w-checkbox.mr-2(name="checkboxes2" color="success")
+    span.pr-2 Choice 1
+    w-icon.primary wi-star
+
 
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    selectedCheckbox1: null,
+    checkboxes1: [
+      { label: 'Choice 1', value: 1 },
+      { label: 'Choice 2', value: 2 },
+      { label: 'Choice 3', value: 3 },
+    ],
+    checkboxes2: [
+      { label: 'Choice 1' },
+      { label: 'Choice 2' },
+      { label: 'Choice 3' },
+      { label: 'Choice 4' }
+    ],
+    checkboxes3: [
+      { label: 'Square', icon: 'mdi mdi-square' },
+      { label: 'Circle', icon: 'mdi mdi-circle' },
+      { label: 'Triangle', icon: 'mdi mdi-triangle' }
+    ]
+  })
 }
 </script>
-
-<style lang="scss">
-
-</style>
--- src/wave-ui/components/w-checkbox.vue
+++ src/wave-ui/components/w-checkbox.vue
@@ -1,23 +1,25 @@
 <template lang="pug">
-  component.w-checkbox(:is="hasLabel ? 'label' : 'span'" :class="classes")
+  component.w-checkbox(:is="hasLabel ? 'label' : 'span'")
     input(
       type="checkbox"
       :name="name"
-      :class="{ 'mr-2': hasLabel }"
       :checked="isChecked"
       @change="isChecked = !isChecked")
-    template(v-if="hasLabel")
-      slot {{ label }}
+    .w-checkbox__input(:class="{ 'mr-2': hasLabel, [this.color]: true }")
+    slot {{ label }}
 </template>
 
 <script>
 export default {
   name: 'w-checkbox',
   props: {
-    value: { type: Boolean, default: false }, // v-model to check or uncheck.
+    value: { default: false }, // v-model to check or uncheck.
+    // When `value` is taken by a v-model and multiple w-checkbox are plugged on
+    // the same v-model, this allow returning to the v-model a custom value.
+    returnValue: {},
     name: { type: String, default: '' },
     label: { type: String, default: '' },
-    color: { type: String, default: null }
+    color: { type: String, default: 'primary' }
   },
 
   computed: {
@@ -26,16 +28,11 @@ export default {
     },
     isChecked: {
       get () {
-        return this.value
+        return this.returnValue !== undefined ? this.returnValue === this.value : this.value
       },
       set (value) {
-        this.$emit('input', value)
-        this.$emit('change', value)
-      }
-    },
-    classes () {
-      return {
-        [this.color]: this.color
+        this.$emit('input', this.returnValue || value)
+        this.$emit('change', this.returnValue || value)
       }
     }
   }
@@ -43,8 +40,57 @@ export default {
 </script>
 
 <style lang="scss">
+$outline-width: 2px;
 .w-checkbox {
   display: inline-flex;
   align-items: center;
+  cursor: pointer;
+
+  // The hidden real checkbox.
+  input[type="checkbox"] {
+    position: absolute;
+    opacity: 0;
+    z-index: -100;
+  }
+
+  // The fake checkbox to substitute.
+  &__input {
+    position: relative;
+    border-radius: $border-radius;
+    width: round(1.3 * $base-font-size);
+    height: round(1.3 * $base-font-size);
+    display: flex;
+    flex: 0 0 auto; // Prevent stretching width or height.
+    align-items: center;
+    justify-content: center;
+    border: $outline-width solid currentColor;
+  }
+  &__input:before {
+    content: '';
+    position: absolute;
+    border-radius: 0;
+    border: 0 solid currentColor;
+    @include default-transition;
+  }
+  input:checked + &__input:before {
+    border-width: 4px;
+  }
+
+  // The focus outline.
+  &__input:after {
+    content: '';
+    position: absolute;
+    top: -$outline-width;
+    bottom: -$outline-width;
+    left: -$outline-width;
+    right: -$outline-width;
+    display: flex;
+    border-radius: inherit;
+    opacity: 0.25;
+    box-shadow: 0 0 0 0 currentColor;
+    @include default-transition;
+  }
+
+  input:focus + &__input:after {box-shadow: 0 0 0 4px currentColor;}
 }
 </style>
