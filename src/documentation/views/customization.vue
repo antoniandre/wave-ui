c04fe09d96d6bb6171612c46a17d90ebfd4acdc2
Antoni Andre <antoniandre.web@gmail.com>
2020-05-26 15:09:47 +1000

    Allow override of the default CSS scope.

--- src/documentation/views/customization.vue
+++ src/documentation/views/customization.vue
@@ -18,3 +18,15 @@ div
     $box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
                  0 2px 2px 0 rgba(0, 0, 0, 0.15),
                  0 1px 5px 0 rgba(0, 0, 0, 0.15);
+
+  h2 Basic overrides
+  h2 Give Wave UI CSS more priority
+  p It is also possible to control Wave UI CSS rules priorities by overriding the CSS base scope:
+  pre $css-scope: '.w-app';
+</template>
+
+<script>
+export default {
+
+}
+</script>
