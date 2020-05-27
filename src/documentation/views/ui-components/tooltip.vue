35ec6dd5cdb201d183b622d852235da0da75b026
Antoni Andre <antoniandre.web@gmail.com>
2020-05-27 23:44:46 +1000

    Create the w-tooltip component and demo.

--- src/documentation/views/ui-components/tooltip.vue
+++ src/documentation/views/ui-components/tooltip.vue
@@ -2,14 +2,50 @@
 div
   h1.headline.mt-4 #[span.code w-tooltip]
 
+  h2 Default
+  .w-flex.wrap
+    w-tooltip(left)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Home
+      | Home
+
+    w-tooltip(top)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Home
+      | Home
+
+    w-tooltip
+      template(v-slot:activator="{ on }")
+        //- w-icon(xl v-on="on") mdi mdi-home
+        span.ma-2(v-on="on") Home
+      | Home
+
+    w-tooltip(right)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Home
+      | Home
+
+  h2 Toggle on click
+  .w-flex.wrap
+
+    w-tooltip(show-on-click left)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Star
+      | Star
+
+    w-tooltip(show-on-click top)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Star
+      | Star
+
+    w-tooltip(show-on-click)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Star
+      | Star
+
+    w-tooltip(show-on-click right)
+      template(v-slot:activator="{ on }")
+        span.ma-2(v-on="on") Star
+      | Star
+
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style lang="scss">
-
-</style>
--- src/wave-ui/components/w-tooltip.vue
+++ src/wave-ui/components/w-tooltip.vue
@@ -1,27 +1,194 @@
 <template lang="pug">
-  .w-tooltip(:class="classes" :style="styles")
-    slot
+div
+  slot(name="activator" :on="eventHandlers")
+  transition(name="fade")
+    .w-tooltip(v-if="showTooltip" :class="classes" :style="styles")
+      slot
 </template>
 
 <script>
+import { consoleWarn } from '@/wave-ui/utils/console'
+
 export default {
   name: 'w-tooltip',
   props: {
+    showOnClick: { type: Boolean, default: false },
+    detach: { type: [String, Object, Boolean], default: false },
+    fixed: { type: Boolean, default: false },
+    top: { type: Boolean, default: false },
+    bottom: { type: Boolean, default: false },
+    left: { type: Boolean, default: false },
+    right: { type: Boolean, default: false },
+    zIndex: { type: [Number, String, Boolean], default: false },
+    color: { type: String, default: '' },
+    bgColor: { type: String, default: '' }
   },
 
+  data: () => ({
+    showTooltip: false,
+    visibleTooltip: false,
+    // The activator coordinates.
+    coordinates: {
+      top: 0,
+      left: 0,
+      width: 0,
+      height: 0
+    }
+  }),
+
   computed: {
+    detachTarget () {
+      let target = this.detach
+      if (target === true) target = '.w-app'
+      if (target && !['object', 'string'].includes(typeof target)) target = '.w-app'
+      if (typeof target === 'string') target = document.querySelector(target)
+      return target
+    },
+
+    position () {
+      return (
+        (this.top && 'top') ||
+        (this.bottom && 'bottom') ||
+        (this.left && 'left') ||
+        (this.right && 'right') ||
+        'bottom'
+      )
+    },
+
+    tooltipCoordinates () {
+      const coords = {}
+      const { top, left, width, height } = this.coordinates
+      switch(this.position) {
+        case 'top': {
+          coords.top = top
+          coords.left = left + width / 2 // left: 50%.
+          break
+        }
+        case 'bottom': {
+          coords.top = top + height
+          coords.left = left + width / 2 // left: 50%.
+          break
+        }
+        case 'left': {
+          coords.top = top + height / 2 // top: 50%.
+          coords.left = left
+          break
+        }
+        case 'right': {
+          coords.top = top + height / 2 // top: 50%.
+          coords.left = left + width
+          break
+        }
+      }
+
+      return coords
+    },
+
     classes () {
       return {
+        [this.color]: this.color,
+        [`${this.bgColor}--bg`]: this.bgColor,
+        [`w-tooltip--${this.position}`]: true,
+        'w-tooltip--fixed': this.fixed,
+        'w-tooltip--active': this.visibleTooltip
       }
     },
+
     styles () {
-      return false
+      return {
+        zIndex: this.zIndex || this.zIndex === 0 || null,
+        top: `${~~this.tooltipCoordinates.top}px`,
+        left: `${~~this.tooltipCoordinates.left}px`
+      }
+    },
+
+    eventHandlers () {
+      let handlers = { keydown: this.toggle }
+      if (this.showOnClick) handlers.click = this.toggle
+      else handlers = { ...handlers, mouseenter: this.toggle, mouseleave: this.toggle }
+      return handlers
+    }
+  },
+
+  mounted () {
+    if (this.detach && !this.detachTarget) {
+      consoleWarn(`Unable to locate ${this.detach ? `target ${this.detach}` : '.w-app'}`, this)
+    }
+  },
+
+  methods: {
+    toggle (e) {
+      if (e.type === 'click' && this.showOnClick) this.showTooltip = !this.showTooltip
+      if (e.type === 'mouseenter' && !this.showOnClick) this.showTooltip = true
+      if (e.type === 'mouseleave' && !this.showOnClick) this.showTooltip = false
+
+      if (this.showTooltip) this.coordinates = this.getCoordinates(e)
+      // Could be useful to have this class?
+      e.target.classList[this.showTooltip ? 'add' : 'remove']('w-tooltip-activator')
+
+      setTimeout(() => (this.visibleTooltip = this.showTooltip), 0)
+    },
+
+    getCoordinates (e) {
+      const { top, left, width, height } = e.target.getBoundingClientRect()
+      let coords = { top, left, width, height }
+      if (!this.fixed) {
+        const app = document.querySelector('.w-app')
+        const { top: appTop, left: appLeft } = app.getBoundingClientRect()
+        coords = { ...coords, top: top - appTop, left: left - appLeft }
+      }
+      return coords
     }
   }
 }
 </script>
 
 <style lang="scss">
+.w-tooltip-activator {position: relative;}
+
 .w-tooltip {
+  display: flex;
+  position: absolute;
+  padding: round($base-increment / 2) $base-increment;
+  border-radius: $border-radius;
+  border: $border;
+  background-color: #fff;
+  pointer-events: none;
+  transition: $transition-duration ease-in-out;
+
+  &--fixed {position: fixed;z-index: 1000;}
+
+  &--top {
+    transform: translate(-50%, -100%);
+    margin-top: -2 * $base-increment;
+
+    &.w-tooltip--active {
+      transform: translate(-50%, -100%);
+    }
+  }
+  &--bottom {
+    transform: translateX(-50%);
+    margin-top: 2 * $base-increment;
+
+    &.w-tooltip--active {
+      transform: translate(-50%, $base-increment);
+    }
+  }
+  &--left {
+    transform: translate(-100%, -50%);
+    margin-left: -2 * $base-increment;
+
+    &.w-tooltip--active {
+      transform: translate(-100%, -50%);
+    }
+  }
+  &--right {
+    transform: translateY(-50%);
+    margin-left: 2 * $base-increment;
+
+    &.w-tooltip--active {
+      transform: translate($base-increment, -50%);
+    }
+  }
 }
 </style>
