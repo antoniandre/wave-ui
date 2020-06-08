75635802bd6dd6011c00db9a7e55a973c7ce0a6c
Antoni André <antoniandre.web@gmail.com>
2020-06-08 18:48:24 +1000

    Add the w-input component.

--- src/documentation/views/ui-components/input.vue
+++ src/documentation/views/ui-components/input.vue
@@ -2,6 +2,105 @@
 div
   h1.headline.mt-4 #[span.code w-input]
 
+  w-input.mb-1(label="First name" required)
+  w-input.mb-1 First name
+  w-input.mb-1(placeholder="First name")
+
+  h2 Round &amp; Tile
+  w-input.mb-1(label="Round" placeholder="Round" round)
+  w-input.mb-1(label="Tile" placeholder="Tile" tile)
+
+  h2 Shadow
+  w-input.mb-1(label="Round" placeholder="Round" shadow)
+
+  h2 Underline style
+  w-input.mb-1(label="Round" underline)
+  w-input.mb-1(label="Round" color="info" underline)
+
+  h2 Color
+  p You can set the background color and the text color.
+  p By default, the text has the "primary" color.
+  w-input.mb-1(value="Some content")
+  w-input.mb-1(color="red" value="Some content")
+  w-input.mb-1(bg-color="red-lighten-5" value="Some content")
+  w-input.mb-1(color="red-darken-1" bg-color="red-lighten-5" value="Some content")
+  w-input.mb-1(color="orange-darken-1" bg-color="orange-lighten-5" value="Some content")
+
+  h2 Label position
+  p The default label position is "inside", but it can also be "left" or "right".
+  w-input.mb-1(label="Left" label-position="left")
+  w-input.mb-1(label="Inside" label-position="inside")
+  w-input.mb-1(label="Right" label-position="right")
+  p.mt-4.
+    #[strong Notes:] when the label is positioned inside and a placeholder is set,
+    the label will be hidden and the placeholder will be displayed.
+
+  h2 Types
+  p The default type is "text".
+  h3 Textual inputs
+  ul.input-types
+    li
+      .code.mb-1 type="email"
+      w-input(type="email") Email address
+    li
+      .code.mb-1 type="number"
+      w-input(type="number" min="0" max="100") Age
+    li
+      .code.mb-1 type="password"
+      w-input(type="password") Password
+    li
+      .code.mb-1 type="search"
+      w-input(type="search") Search
+    li
+      .code.mb-1 type="tel"
+      w-input(type="tel") Tel
+    li
+      .code.mb-1 type="text"
+      w-input Text
+    li
+      .code.mb-1 type="url"
+      w-input(type="url") Url
+    li
+      span.code.mb-1.mr-2 type="hidden"
+      | The hidden type is also accepted.
+      w-input(type="hidden")
+
+  h3 Other types of inputs
+  p For all the other types of input listed bellow, a specific Wave UI component should be used instead:
+  ul.other-input-types
+    li
+      span.code.mr-2 type="button"
+      span.grey use #[code w-button] instead
+    li
+      span.code.mr-2 type="checkbox"
+      span.grey use #[code w-checkbox] instead
+    li
+      span.code.mr-2 type="color"
+      span.grey use #[code w-color-picker] instead
+    li
+      span.code.mr-2 type="date"
+      span.grey use #[code w-date-picker] instead
+    li
+      span.code.mr-2 type="file"
+      span.grey use #[code w-file-input] instead
+    li
+      span.code.mr-2 type="image"
+      span.grey use #[code w-image] instead
+    li
+      span.code.mr-2 type="radio"
+      span.grey use #[code w-radio] instead
+    li
+      span.code.mr-2 type="range"
+      span.grey use #[code w-slider] instead
+    li
+      span.code.mr-2 type="reset"
+      span.grey use #[code w-button] instead
+    li
+      span.code.mr-2 type="submit"
+      span.grey use #[code w-button] instead
+    li
+      span.code.mr-2 type="time"
+      span.grey use #[code w-time-picker] instead
 </template>
 
 <script>
@@ -11,5 +110,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+ul.input-types li {margin-bottom: 8px;}
 </style>
--- src/wave-ui/components/w-input.vue
+++ src/wave-ui/components/w-input.vue
@@ -1,49 +1,188 @@
 <template lang="pug">
   .w-input(:class="classes" :style="styles")
-    input(:id="`input--${_uid}`" :type="type" :name="name" :disabled="disabled")
-    label.w-input__label(v-if="$slots.default" :for="`input--${_uid}`")
-      slot
-    label.w-input__label(v-else-if="label" :for="`input--${_uid}`" v-html="label")
+    template(v-if="labelPosition === 'left'")
+      label.w-input__label.w-input__label--left(v-if="$slots.default" :for="`input--${_uid}`")
+        slot
+      label.w-input__label.w-input__label--left(v-else-if="label" :for="`input--${_uid}`" v-html="label")
+    .w-input__input-wrap(:class="{ [this.color]: this.color }")
+      input.w-input__input(
+        :id="`input--${_uid}`"
+        :type="type"
+        :name="name || null"
+        :placeholder="placeholder || null"
+        v-model="inputValue"
+        @input="onInput"
+        @focus="onFocus"
+        @blur="onBlur"
+        :disabled="disabled"
+        :required="required"
+        :class="{ [this.color]: this.color, [`${this.bgColor}--bg`]: this.bgColor }")
+    template(v-if="!inputValue && !placeholder && labelPosition === 'inside'")
+      label.w-input__label.w-input__label--inside(v-if="$slots.default" :for="`input--${_uid}`" :class="isFocused && { [this.color]: this.color }")
+        slot
+      label.w-input__label.w-input__label--inside(v-else-if="label" :for="`input--${_uid}`" v-html="label" :class="isFocused && { [this.color]: this.color }")
+    template(v-if="labelPosition === 'right'")
+      label.w-input__label.w-input__label--right(v-if="$slots.default" :for="`input--${_uid}`")
+        slot
+      label.w-input__label.w-input__label--right(v-else-if="label" :for="`input--${_uid}`" v-html="label")
 </template>
 
 <script>
 export default {
   name: 'w-input',
   props: {
-    value: { default: false },
+    value: { default: '' },
     type: { type: String, default: 'text' },
     name: { type: String, default: '' },
     label: { type: String, default: '' },
+    labelPosition: { type: String, default: 'inside' },
+    placeholder: { type: String, default: '' },
     color: { type: String, default: 'primary' },
     bgColor: { type: String, default: '' },
     disabled: { type: Boolean, default: false },
+    required: { type: Boolean, default: false },
+    minlength: { type: String, default: '' },
+    maxlength: { type: String, default: '' },
     dark: { type: Boolean, default: false },
     outline: { type: Boolean, default: false },
     round: { type: Boolean, default: false },
     shadow: { type: Boolean, default: false },
-    tile: { type: Boolean, default: false }
+    tile: { type: Boolean, default: false },
+    underline: { type: Boolean, default: false }
+  },
+
+  data () {
+    return {
+      inputValue: this.value,
+      isFocused: false
+    }
   },
 
   computed: {
     classes () {
       return {
-        [this.color]: this.color,
-        [`${this.bgColor}--bg`]: this.bgColor,
+        'w-input--focused': this.isFocused,
         'w-text-input--dark': this.dark && !this.outline,
         'w-input--outline': this.outline,
         'w-input--round': this.round,
         'w-input--tile': this.tile,
+        'w-input--underline': this.underline,
         'w-input--shadow': this.shadow
       }
     },
     styles () {
       return false
     }
+  },
+
+  methods: {
+    onInput () {
+      this.$emit('input', this.inputValue)
+    },
+    onFocus () {
+      this.$emit('focus', this.inputValue)
+      this.isFocused = true
+    },
+    onBlur () {
+      this.$emit('blur', this.inputValue)
+      this.isFocused = false
+    }
+  },
+
+  watch: {
+    value (value) {
+      this.inputValue = value
+    }
   }
 }
 </script>
 
 <style lang="scss">
+$size: round(2 * $base-font-size);
+
 .w-input {
+  position: relative;
+  display: flex;
+  align-items: center;
+  outline: none;
+  height: $size;
+  font-size: $base-font-size;
+
+  // Input field wrapper.
+  // ------------------------------------------------------
+  &__input-wrap {
+    display: inline-flex;
+    flex-grow: 1;
+    height: 100%;
+  }
+
+  &--underline &__input-wrap:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    border-bottom: 2px solid currentColor;
+    transition: $transition-duration;
+    transform: translateX(-50%);
+  }
+
+  &--underline.w-input--focused &__input-wrap:after {
+    width: 100%;
+  }
+
+  // Input field.
+  // ------------------------------------------------------
+  &__input {
+    border-radius: $border-radius;
+    border: $border;
+    padding-left: 2 * $base-increment;
+    padding-right: 2 * $base-increment;
+    width: 100%;
+    font-size: 0.9em;
+    transition: $transition-duration;
+  }
+
+  &--underline &__input {
+    border-radius: initial;
+    border-width: 0 0 1px;
+    padding-left: 0;
+  }
+
+  &--round &__input {
+    border-radius: 4em;
+    padding-left: round(3 * $base-increment);
+    padding-right: round(3 * $base-increment);
+  }
+  &--tile &__input {border-radius: initial;}
+  &--shadow &__input {box-shadow: $box-shadow;}
+
+  &__input:focus {border-color: currentColor;}
+
+  // Label.
+  // ------------------------------------------------------
+  &__label {
+    font-size: 0.9em;
+
+    &--inside {
+      position: absolute;
+      top: 50%;
+      left: 0;
+      padding-left: 2 * $base-increment;
+      transform: translateY(-50%);
+      pointer-events: none;
+    }
+
+    &--left {margin-right: 2 * $base-increment;}
+    &--right {margin-left: 2 * $base-increment;}
+  }
+
+  &--underline &__label--inside {padding-left: 0;padding-right: 0;}
+  &--round &__label--inside {
+    padding-left: round(3 * $base-increment);
+    padding-right: round(3 * $base-increment);
+  }
+
+  &--focused &__label--inside {color: currentColor;}
 }
 </style>
