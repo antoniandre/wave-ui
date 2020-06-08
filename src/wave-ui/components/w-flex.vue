39d7c51d20d599eeea8e73e409e0c290841ecf27
Antoni André <antoniandre.web@gmail.com>
2020-06-08 23:33:34 +1000

    Add a gutter option on w-flex.

--- src/wave-ui/components/w-flex.vue
+++ src/wave-ui/components/w-flex.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-  .w-flex(:class="classes")
+  .w-flex-wrap(v-if="gutter")
+    .w-flex(:class="classes")
+      slot
+  .w-flex(v-else :class="classes")
     slot
 </template>
 
@@ -16,7 +19,8 @@ export default {
     alignCenter: { type: Boolean, default: false },
     justifyCenter: { type: Boolean, default: false },
     justifyEnd: { type: Boolean, default: false },
-    justifySpaceBetween: { type: Boolean, default: false }
+    justifySpaceBetween: { type: Boolean, default: false },
+    gutter: { type: Number, default: 0 }
   },
 
   computed: {
@@ -31,7 +35,8 @@ export default {
         'align-center': this.alignCenter,
         'justify-center': this.justifyCenter,
         'justify-end': this.justifyEnd,
-        'justify-space-between': this.justifySpaceBetween
+        'justify-space-between': this.justifySpaceBetween,
+        [`gutter-${this.gutter}`]: this.gutter
       }
     }
   }
@@ -39,6 +44,14 @@ export default {
 </script>
 
 <style lang="scss">
+.w-flex-wrap {
+  @for $i from 1 through 12 {
+    $space: round($base-increment * $i / 2);
+    > .gutter-#{$i} {margin: -$space;}
+    > .gutter-#{$i} > * {margin: $space;}
+  }
+}
+
 .w-flex {
   display: flex;
   flex: 1 1 auto;
