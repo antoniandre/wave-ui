6f1e897b2b5aa3a44a1f76d9392cbf4f6b360df6
Antoni Andre <antoniandre.web@gmail.com>
2020-06-29 00:01:11 +1000

    Correct typo.

--- src/wave-ui/components/w-icon.vue
+++ src/wave-ui/components/w-icon.vue
@@ -88,11 +88,11 @@ export default {
 
   // In w-button and w-alert.
   .w-button &, .w-alert & {padding: 0;font-size: round(1.4 * $base-font-size);}
-  .w-button.size--xs, .w-alert.size--xs & {font-size: round(1 * $base-font-size);}
-  .w-button.size--sm, .w-alert.size--sm & {font-size: round(1.15 * $base-font-size);}
-  .w-button.size--md, .w-alert.size--md & {font-size: round(1.4 * $base-font-size);}
-  .w-button.size--lg, .w-alert.size--lg & {font-size: round(1.7 * $base-font-size);}
-  .w-button.size--xl, .w-alert.size--xl & {font-size: round(2 * $base-font-size);}
+  .w-button.size--xs &, .w-alert.size--xs & {font-size: round(1 * $base-font-size);}
+  .w-button.size--sm &, .w-alert.size--sm & {font-size: round(1.15 * $base-font-size);}
+  .w-button.size--md &, .w-alert.size--md & {font-size: round(1.4 * $base-font-size);}
+  .w-button.size--lg &, .w-alert.size--lg & {font-size: round(1.7 * $base-font-size);}
+  .w-button.size--xl &, .w-alert.size--xl & {font-size: round(2 * $base-font-size);}
 
   &--spin:before {animation: w-icon--spin 2s infinite linear;}
 }
