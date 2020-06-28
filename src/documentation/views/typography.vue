f5e0935a819fea884a1044f77486911dbbbc4f8c
Antoni Andre <antoniandre.web@gmail.com>
2020-06-28 15:40:14 +1000

    Add heading/subheading classes & update demo.

--- src/documentation/views/typography.vue
+++ src/documentation/views/typography.vue
@@ -1,22 +1,30 @@
 <template lang="pug">
 div
   h1.headline.mt-4 Typography
-
   p Here is a list of classes that will help you structure and emphasize your content.
-  .headline.mt-12 Headline using the #[code .headline] class.
-  .title.mt-4 Title using the #[code .title] class.
-  .subtitle.mt-4 Subtitle using the #[code .subtitle] class.
-  .body.mt-4 Body text using the #[code .body] class.
-  .caption.mt-4 Caption text using the #[code .caption] class.
 
-  .uppercase.mt-4 Uppercase text using the #[code .uppercase] class.
-  .lowercase.mt-4 Lowercase text using the #[code .lowercase] class.
-  .bold.mt-4 Bold text using the #[code .bold] class.
-  .italic.mt-4 Italic text using the #[code .italic] class.
+  h2 Content architecture
+  div.ml-8
+    .headline.mt-6 Headline using the #[code .headline] class.
+    .title.mt-4 Title using the #[code .title] class.
+    .subtitle.mt-4 Subtitle using the #[code .subtitle] class.
+    .heading.mt-4 Heading using the #[code .heading] class.
+    .subheading.mt-4 Subheading using the #[code .subheading] class.
+    .body.mt-4 Body text using the #[code .body] class.
+    .caption.mt-4 Caption text using the #[code .caption] class.
 
-  .text-left.mt-4 Left aligned text using the #[code .text-left] class.
-  .text-center Center aligned text using the #[code .text-center] class.
-  .text-right Right aligned text using the #[code .text-right] class.
+  h2 Text style
+  div.ml-8
+    .uppercase.mt-4 Uppercase text using the #[code .uppercase] class.
+    .lowercase.mt-4 Lowercase text using the #[code .lowercase] class.
+    .bold.mt-4 Bold text using the #[code .bold] class.
+    .italic.mt-4 Italic text using the #[code .italic] class.
+
+  h2 Text alignment
+  div.ml-8
+    .text-left.mt-4 Left aligned text using the #[code .text-left] class.
+    .text-center Center aligned text using the #[code .text-center] class.
+    .text-right Right aligned text using the #[code .text-right] class.
 </template>
 
 <script>
