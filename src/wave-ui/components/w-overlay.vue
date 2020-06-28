f3f8c269039f4d987316531fc668676d98701724
Antoni Andre <antoniandre.web@gmail.com>
2020-06-28 22:39:48 +1000

    Remove the w-overlay slot wrapper.

--- src/wave-ui/components/w-overlay.vue
+++ src/wave-ui/components/w-overlay.vue
@@ -8,9 +8,7 @@
       v-focus
       tabindex="0"
       :class="classes")
-      .w-overlay__content(v-if="persistent")
-        slot
-      slot(v-else)
+      slot
 </template>
 
 <script>
@@ -49,11 +47,14 @@ export default {
   },
 
   methods: {
-    onClick () {
+    onClick (e) {
+      // Don't react to a click inside the content (event bubbling).
+      if (!e.target.classList.contains('w-overlay')) return
+
       // Quickly add the animation class and remove it.
       if (this.persistent && !this.persistentNoAnimation) {
         this.persistentAnimate = true
-        setTimeout(() => (this.persistentAnimate = false), 120)
+        setTimeout(() => (this.persistentAnimate = false), 150)
       }
       else if (!this.persistent) this.$emit('input', false)
 
@@ -67,20 +68,22 @@ export default {
 .w-overlay {
   z-index: 500;
   position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
+  // -10px for the `persistent-animate` ease out back animation.
+  top: -10px;
+  left: -10px;
+  bottom: -10px;
+  right: -10px;
+  padding: 10px;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: rgba(0, 0, 0, 0.3);
 
-  &--persistent-animate .w-overlay__content {animation: 0.12s w-overlay-pop cubic-bezier(0.6, -0.28, 0.74, 0.05);}
+  &--persistent-animate {animation: 0.15s w-overlay-pop cubic-bezier(0.6, -0.28, 0.74, 0.05);}
 }
 
 @keyframes w-overlay-pop {
   0%, 100% {transform: scale(1);}
-  50% {transform: scale(1.06);}
+  50% {transform: scale(1.04);}
 }
 </style>
