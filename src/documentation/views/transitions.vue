8f6e88c2fd5fa7d2075a0ad83812e4dfbc1eb99f
Antoni Andre <antoniandre.web@gmail.com>
2020-07-08 19:16:23 +1000

    Improve expand transition.

--- src/documentation/views/transitions.vue
+++ src/documentation/views/transitions.vue
@@ -6,8 +6,10 @@ div
   ul
     li #[code w-transition-fade]
     li.mt-1 #[code w-transition-scale]
-    li.mt-1 #[code w-transition-expand]
+    li.mt-1 #[code w-transition-scale-fade]
+    li.mt-1 #[code w-transition-bounce]
     li.mt-1 #[code w-transition-twist]
+    li.mt-1 #[code w-transition-expand]
 
   //- p Each component has a css transition that you can also use in various components.
 
@@ -44,16 +46,6 @@ div
 
   w-flex.mt-3(align-center)
     w-button.mr-2(
-      @click="toggleExpand = !toggleExpand"
-      color="primary"
-      sm
-      outline
-      width="7em") Expand {{ toggleExpand ? 'out' : 'in' }}
-    w-transition-expand
-      span.px-8.py-4(v-if="toggleExpand") Expanding transition
-
-  w-flex.mt-3(align-center)
-    w-button.mr-2(
       @click="toggleBounce = !toggleBounce"
       color="primary"
       sm
@@ -61,6 +53,47 @@ div
       width="7em") Bounce {{ toggleBounce ? 'out' : 'in' }}
     w-transition-bounce
       span(v-if="toggleBounce") Bouncing transition
+
+  w-flex.mt-3(align-center)
+    w-button.mr-2(
+      disabled
+      @click="toggleTwist = !toggleTwist"
+      color="primary"
+      sm
+      outline
+      width="7em") Twist {{ toggleTwist ? 'out' : 'in' }}
+    w-transition-bounce
+      span(v-if="toggleTwist") Twisting transition
+
+  w-flex.mt-3(align-center)
+    w-button.mr-2(
+      @click="toggleExpandX = !toggleExpandX"
+      color="primary"
+      sm
+      outline
+      width="7em") {{ toggleExpandX ? 'Collapse' : 'Expand' }} X
+    w-transition-expand(x)
+      span.px-8.py-4.grey-lighter-4--bg(v-if="toggleExpandX") X-expanding transition
+
+  w-flex.mt-3(align-center)
+    w-button.mr-2(
+      @click="toggleExpandY = !toggleExpandY"
+      color="primary"
+      sm
+      outline
+      width="7em") {{ toggleExpandY ? 'Collapse' : 'Expand' }} Y
+    w-transition-expand(y)
+      span.px-8.py-4.grey-lighter-4--bg(v-if="toggleExpandY") Y-expanding transition
+
+  w-flex.mt-3(align-center)
+    w-button.mr-2(
+      @click="toggleExpandXY = !toggleExpandXY"
+      color="primary"
+      sm
+      outline
+      width="7em") {{ toggleExpandXY ? 'Collapse' : 'Expand' }} X &amp; Y
+    w-transition-expand
+      span.px-8.py-4.grey-lighter-4--bg(v-if="toggleExpandXY") X &amp; Y expanding transition
 </template>
 
 <script>
@@ -69,7 +102,10 @@ export default {
     toggleFade: false,
     toggleScale: false,
     toggleScaleFade: false,
-    toggleExpand: false,
+    toggleTwist: false,
+    toggleExpandXY: false,
+    toggleExpandX: false,
+    toggleExpandY: false,
     toggleBounce: false
   })
 }
