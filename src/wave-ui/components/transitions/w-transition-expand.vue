bc85f527c7af5d05715b1f016dde324f9b38fe63
Antoni Andre <antoniandre.web@gmail.com>
2020-07-09 09:39:32 +1000

    Add x & y axis options on expand transition.

--- src/wave-ui/components/transitions/w-transition-expand.vue
+++ src/wave-ui/components/transitions/w-transition-expand.vue
@@ -22,31 +22,76 @@
 
 <script>
 const transition = 3000 // ms.
+
 export default {
   name: 'w-transition-expand',
+
+  props: {
+    x: { type: Boolean },
+    y: { type: Boolean }
+  },
+
   data: () => ({
     el: {
       originalStyles: '',
       width: 0,
       height: 0,
-      margin: 0,
-      padding: 0
+      marginLeft: 0,
+      marginRight: 0,
+      marginTop: 0,
+      marginBottom: 0,
+      paddingLeft: 0,
+      paddingRight: 0,
+      paddingTop: 0,
+      paddingBottom: 0
     }
   }),
+
+  computed: {
+    animX () {
+      return this.x || !this.y
+    },
+    animY () {
+      return this.y || !this.x
+    }
+  },
+
   methods: {
     applyHideStyles (el) {
-      el.style.width = 0
-      el.style.height = 0
-      el.style.padding = 0
-      el.style.margin = 0
+      if (this.animX) {
+        el.style.width = 0
+        el.style.marginLeft = 0
+        el.style.marginRight = 0
+        el.style.paddingLeft = 0
+        el.style.paddingRight = 0
+      }
+      if (this.animY) {
+        el.style.height = 0
+        el.style.marginTop = 0
+        el.style.marginBottom = 0
+        el.style.paddingTop = 0
+        el.style.paddingBottom = 0
+      }
+
       el.style.overflow = 'hidden'
       el.style.whiteSpace = 'nowrap'
     },
     applyShowStyles (el) {
-      el.style.width = this.el.width + 'px'
-      el.style.height = this.el.height + 'px'
-      el.style.margin = this.el.margin
-      el.style.padding = this.el.padding
+      if (this.animX) {
+        el.style.width = this.el.width + 'px'
+        el.style.marginLeft = this.el.marginLeft
+        el.style.marginRight = this.el.marginRight
+        el.style.paddingLeft = this.el.paddingLeft
+        el.style.paddingRight = this.el.paddingRight
+      }
+      if (this.animY) {
+        el.style.height = this.el.height + 'px'
+        el.style.marginTop = this.el.marginTop
+        el.style.marginBottom = this.el.marginBottom
+        el.style.paddingTop = this.el.paddingTop
+        el.style.paddingBottom = this.el.paddingBottom
+      }
+
       el.style.transition = transition + 'ms ease-in-out'
     },
     applyOriginalStyles (el) {
@@ -54,18 +99,28 @@ export default {
       console.log('apply original styles')
     },
     beforeShow (el) {
-      // Keep original styles to restore them after transition.
+      // Keep the original styles to restore them after transition.
       this.el.originalStyles = el.style.cssText
       console.log('onBeforeEnter', el)
     },
     show (el, done) {
+      const computedStyles = window.getComputedStyle(el, null)
       console.log('onEnter', el, el.offsetHeight)
       // Save the width & height then set them to 0 as the animation starting point.
-      this.el.width = el.offsetWidth
-      this.el.height = el.offsetHeight
-      this.el.margin = window.getComputedStyle(el, null).getPropertyValue('margin')
-      this.el.padding = window.getComputedStyle(el, null).getPropertyValue('padding')
-
+      if (this.animX) {
+        this.el.width = el.offsetWidth
+        this.el.marginLeft = computedStyles.getPropertyValue('marginLeft')
+        this.el.marginRight = computedStyles.getPropertyValue('marginRight')
+        this.el.paddingLeft = computedStyles.getPropertyValue('paddingLeft')
+        this.el.paddingRight = computedStyles.getPropertyValue('paddingRight')
+      }
+      if (this.animY) {
+        this.el.height = el.offsetHeight
+        this.el.marginTop = computedStyles.getPropertyValue('marginTop')
+        this.el.marginBottom = computedStyles.getPropertyValue('marginBottom')
+        this.el.paddingTop = computedStyles.getPropertyValue('paddingTop')
+        this.el.paddingBottom = computedStyles.getPropertyValue('paddingBottom')
+      }
       this.applyHideStyles(el)
 
       setTimeout(() => this.applyShowStyles(el), 0)
