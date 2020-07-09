4ea2046e1da9bb8861da374556653a51fb711f6b
Antoni Andre <antoniandre.web@gmail.com>
2020-07-10 00:20:43 +1000

    Improve the w-badge component.

--- src/documentation/views/ui-components/badge.vue
+++ src/documentation/views/ui-components/badge.vue
@@ -3,9 +3,50 @@ div
   h1.headline.mt-4 #[span.code w-badge]
 
   h2 Default
-  .w-flex.wrap.py-6
-    w-badge
-      template(v-slot:badge)
-        span.ma-2 1
+  w-badge.mr-10
+    template(v-slot:badge) 3
+    div.grey-lighter-5--bg.pa-2.primary
+      w-icon.mr-1 mdi mdi-email
       | Emails
+
+  h2 Overlap
+  w-flex(wrap)
+    w-badge.mr-10
+      template(v-slot:badge) 3
+      div.grey-lighter-5--bg.pa-2.primary
+        w-icon.mr-1 mdi mdi-email
+        | Emails
+
+    w-badge.my-4.mr-10(overlap bottom dot bg-color="green")
+      w-icon.blue--bg.pa-6(size="2.5em" color="white") mdi mdi-account
+
+  w-flex(wrap)
+    w-badge.my-4.mr-10(overlap bottom bg-color="orange" sm color="white")
+      template(v-slot:badge) away
+      w-icon.blue--bg.pa-6(size="2.5em" color="white") mdi mdi-account
+
+  h2 Dot
+  w-flex(wrap)
+    w-badge.mr-10(dot)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
+
+  h2 Sizes
+  h3 Dots
+  w-flex(wrap)
+    w-badge.mr-10(dot xs)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
+    w-badge.mr-10(dot sm)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
+    w-badge.mr-10(dot md)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
+    w-badge.mr-10(dot lg)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
+    w-badge.mr-10(dot xl)
+      template(v-slot:badge)
+      w-icon(size="2.5em") mdi mdi-account
 </template>
