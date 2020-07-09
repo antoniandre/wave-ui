07ee1195e822ac71b1627035260d059653d38f93
Antoni Andre <antoniandre.web@gmail.com>
2020-07-09 20:12:32 +1000

    Remove no-undef eslint rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-unused-vars': 'warn',
-    'no-undef': 'warn'
+    'no-unused-vars': 'warn'
   }
 }
