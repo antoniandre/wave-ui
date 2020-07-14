f497e321444b9d66cc12445bcec82845a0e2e401
Antoni André <antoniandre.web@gmail.com>
2020-07-15 00:08:10 +1000

    Improve the w-menu & cleanup.

--- src/documentation/views/ui-components/menu.vue
+++ src/documentation/views/ui-components/menu.vue
@@ -2,10 +2,45 @@
 div
   title-link.mt-4.code(h1) w-menu
 
+  title-link(h2) Show on click or on hover
   w-menu
     template(#activator="{ on }")
-      w-button(v-on="on" primary) Show menu
+      w-button(v-on="on" outline color="primary") Show menu on click
     | Menu content
+
+  w-menu(show-on-hover)
+    template(#activator="{ on }")
+      w-button(v-on="on" outline color="primary") Show menu on hover
+    | Menu content
+
+  title-link(h2 slug="appending-to-a-dom-node") Appending to a particular DOM node
+  w-alert.text-light(type="info").
+    This step is important for absolute positionning (default): if you don't append
+    the menu to the correct DOM node, you will most likely end up with a menu not
+    following its activator on scroll.#[br]
+    The DOM element that will receive the menu must have a position (fixed, absolute or
+    relative).
+
+  title-link(h2) Color &amp; background color
+  w-menu(color="blue-darker-2" bg-color="blue-lighter-5")
+    template(#activator="{ on }")
+      w-button(v-on="on" outline color="primary") Show menu on hover
+    | Menu content
+
+  title-link(h2) Tile, round and shadow
+  w-menu(tile)
+    template(#activator="{ on }")
+      w-button(v-on="on" outline color="primary") Show a tile menu
+    | Menu content
+  w-menu(round)
+    template(#activator="{ on }")
+      w-button(v-on="on" outline color="primary") Show a round menu
+    | Menu content
+  w-menu(shadow)
+    template(#activator="{ on }")
+      w-button(v-on="on" outline color="primary") Show a shadow menu
+    | Menu content
+
 </template>
 
 <script>
@@ -13,7 +48,3 @@ export default {
 
 }
 </script>
-
-<style lang="scss">
-
-</style>
