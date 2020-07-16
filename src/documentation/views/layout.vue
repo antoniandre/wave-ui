1cac8501e06b4a3c6826510372177b194908ba4c
Antoni Andre <antoniandre.web@gmail.com>
2020-07-17 00:22:27 +1000

    Update the layout documentation page.

--- src/documentation/views/layout.vue
+++ src/documentation/views/layout.vue
@@ -2,35 +2,115 @@
 div
   title-link.mt-4(h1) Layout
 
-  title-link(h2) Layout classes
+  title-link(h2) Flex layout classes
+  p the #[code .w-flex] class allows a flex layout that you can easily control with the following classes.
   ul
     li
-      code .w-flex
-      p column, wrap
+      code.mr-2 .column
+      | Displays in column (#[span.code flex-direction: column]).
     li.mt-1
-      code .grow, .no-grow
-      p
+      code.mr-2 .wrap
+      | Wraps the flex content when it does not fit (#[span.code flex-wrap: wrap]).
     li.mt-1
-      code .shrink, .no-shrink
-      p
+      code.mr-2 .grow
+      | Tells the element to occupy the available space (#[span.code flex-grow: 1]).
     li.mt-1
-      code .spacer
+      code.mr-2 .no-grow
+      | Tells the element to #[strong not] occupy the available space (#[span.code flex-grow: 0]).
     li.mt-1
-      code .fill-height
+      code.mr-2 .shrink
+      | Tells the element to shrink if there is not enough space (#[span.code flex-shrink: 1]).
+    li.mt-1
+      code.mr-2 .no-shrink
+      | Tells the element to #[strong not] shrink if there is not enough space (#[span.code flex-shrink: 0]).
+    li.mt-1
+      code.mr-2 .fill-height
+      | Sets a height of 100%.
+
+  title-link(h3) Alignments
+  ul
+    li.mt-1
+      code.mr-2 .align-start
+      | Applies this CSS rule: #[span.code align: flex-start].
+    li.mt-1
+      code.mr-2 .align-center
+      | Applies this CSS rule: #[span.code align: center].
+    li.mt-1
+      code.mr-2 .align-end
+      | Applies this CSS rule: #[span.code align: flex-end].
+    li.mt-1
+      code.mr-2 .align-self-start
+      | Applies this CSS rule: #[span.code align-self: flex-start].
+    li.mt-1
+      code.mr-2 .align-self-center
+      | Applies this CSS rule: #[span.code align-self: center].
+    li.mt-1
+      code.mr-2 .align-self-end
+      | Applies this CSS rule: #[span.code align-self: flex-end].
+    li.mt-1
+      code.mr-2 .justify-start
+      | Applies this CSS rule: #[span.code justify: flex-start].
+    li.mt-1
+      code.mr-2 .justify-center
+      | Applies this CSS rule: #[span.code justify: center].
+    li.mt-1
+      code.mr-2 .justify-end
+      | Applies this CSS rule: #[span.code justify: flex-end].
+    li.mt-1
+      code.mr-2 .justify-self-start
+      | Applies this CSS rule: #[span.code justify-self: flex-start].
+    li.mt-1
+      code.mr-2 .justify-self-center
+      | Applies this CSS rule: #[span.code justify-self: center].
+    li.mt-1
+      code.mr-2 .justify-self-end
+      | Applies this CSS rule: #[span.code justify-self: flex-end].
+    li.mt-1
+      code.mr-2 .justify-space-between
+      | Applies this CSS rule: #[span.code justify: space-between].
+
+  p.mt-6.
+    The #[code.mx-1 .spacer] class is also very helpful to separate 2 elements as is will occupy the available
+    space and push the elements on each side.
 
   title-link(h2) Content spacing
   p.
-    Margins and paddings are incrementable from 1 to 12 using this syntax: #[span.code ma-12], #[span.code pa-7].#[br]
-    You can also set a space on a single side with #[span.code l], #[span.code r], #[span.code t], #[span.code b] for left, right, top, bottom.#[br]
-    #[span.code x] and #[span.code y] will set both left and right or top and bottom.
-  p ma mx my ml mr mt mb
-  p pa px py pl pr pt pb
+    Margins and paddings are incrementable from 1 to 12 using this syntax: #[code="ma-[number]"],
+    #[code="pa-[number]"].#[br]
+    You can also set a space on a single side with #[span.code l], #[span.code r], #[span.code t],
+    #[span.code b] for left, right, top, bottom like so: #[code="m[side]-[number]"],
+    #[code="p[side]-[number]"].#[br]#[br]
+    #[span.code x] and #[span.code y] will set both left and right or top and bottom together:
+    #[code="mx-[number]"], #[code="px-[number]"].
 
-  title-link(h2) Grid system
+  example
+    .pa-12.orange-lighter-4--bg
+      .yellow-lighter-5--bg.pa-3.
+        The orange container has a #[span.code pa-12] class which results in a padding of: 12 x 4 x
+        base-font-size pixels.#[br]
+        The default base font size is 14px but it's easy to override it in your SCSS, as well as the
+        increment for #[span.code pa-x] and #[span.code ma-x] classes. Read on in the
+        #[router-link(to="customization") customization] page.#[br]
+
+      .green-lighter-3--bg.mt-8.pa-3.
+        This green container has a #[span.code mt-8] class which results in a top margin of: 8 x 4 x
+        base-font-size pixels.
+    template(#html).
+      &lt;div class="pa-12 orange-lighter-4--bg"&gt;
+        &lt;div class="yellow-lighter-5--bg pa-3"&gt;
+          The orange container has a pa-12 class which results in a padding of: 12 x 4 x base-font-size pixels.
+          The default base font size is 14px but it's easy to override it in your SCSS, as well as the
+          increment for pa-x and ma-x classes.
+        &lt;/div&gt;
+
+        &lt;div class="green-lighter-3--bg mt-8 pa-3"&gt;
+          This green container has an mt-8 class which results in a top margin of: 8 x 4 x base-font-size pixels.
+        &lt;/div&gt;
+      &lt;/div&gt;
+
+  title-link(h2 slug="grid-system")
+    | Grid system
+    w-tag.ml-3(bg-color="red" color="white") Coming soon
+
   p 12-cell based.
 </template>
-
-<script>
-export default {
-}
-</script>
