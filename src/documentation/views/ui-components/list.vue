3e2d3c036db2ddf2ba6636d65bdc55f79215d2cd
Antoni Andre <antoniandre.web@gmail.com>
2020-07-19 18:55:16 +1000

    Add list page examples source code.

--- src/documentation/views/ui-components/list.vue
+++ src/documentation/views/ui-components/list.vue
@@ -3,93 +3,262 @@ div
   title-link.mt-4.code(h1) w-list
 
   title-link(h2) Default items list rendering
-  .w-flex.wrap
-    .grow
-      .subtitle Most basic
-      w-list.grow.mt-6(:items="listItems1" color="primary")
-    .grow
-      .subtitle With icons
-      w-list.grow.mt-6(:items="listItems1" icon="wi-check" color="primary")
+  example
+    .w-flex.wrap
+      .grow
+        .subtitle Most basic
+        w-list.grow.mt-6(:items="listItems1" color="primary")
+      .grow
+        .subtitle With icons
+        w-list.grow.mt-6(:items="listItems1" icon="wi-check" color="primary")
+    template(#html).
+      &lt;w-flex wrap&gt;
+        &lt;div class="grow"&gt;
+          &lt;div class="subtitle"&gt;Most basic&lt;/div&gt;
+          &lt;w-list class="grow mt-6" :items="listItems" color="primary" /&gt;
+        &lt;/div&gt;
+
+        &lt;div class="grow"&gt;
+          &lt;div class="subtitle"&gt;With icons&lt;/div&gt;
+          &lt;w-list class="grow mt-6" :items="listItems" icon="wi-check" color="primary" /&gt;
+        &lt;/div&gt;
+      &lt;/w-flex&gt;
+    template(#js).
+      data: () => ({
+        listItems: [
+          { label: 'Item 1', value: 1 },
+          { label: 'Item 2', value: 2 },
+          { label: 'Item 3', value: 3 },
+          { label: 'Item 4', value: 4 },
+          { label: 'Item 5', value: 5 },
+          { label: 'Item 6', value: 6, disabled: true }
+        ]
+      })
 
   title-link(h2) Custom rendering &amp; hoverable
-  w-list.mt-6(:items="listItems2" color="pink" hover)
-    template(v-slot:item="{ item }")
-      .w-flex.align-center
-        | {{ item.label }}
-        .spacer
-        w-icon(md) {{ item.icon }}
+  example
+    w-list(:items="listItems2" color="pink" hover)
+      template(#item="{ item }")
+        .w-flex.align-center.justify-space-between
+          | {{ item.label }}
+          w-icon(md) {{ item.icon }}
+    template(#html).
+      &lt;w-list :items="listItems" color="pink" hover&gt;
+        &lt;template #item="{ item }"&gt;
+          &lt;w-flex align-center justify-space-between&gt;
+            {{ '\{\{ item.label \}\}' }}
+            &lt;w-icon md&gt;{{ '\{\{ item.icon \}\}' }}&lt;/w-icon&gt;
+          &lt;/w-flex&gt;
+        &lt;/template&gt;
+      &lt;/w-list&gt;
+    template(#js).
+      data: () => ({
+        listItems: [
+          { label: 'Star', id: 'star', icon: 'mdi mdi-star' },
+          { label: 'Check', id: 'check', icon: 'mdi mdi-check' },
+          { label: 'Cross', id: 'cross', icon: 'mdi mdi-close' },
+          { label: 'Info', id: 'info', icon: 'mdi mdi-information' },
+          { label: 'Warning', id: 'warning', icon: 'mdi mdi-alert' },
+          { label: 'Disabled', id: 'disabled', icon: 'mdi mdi-minus-circle' }
+        ]
+      })
 
   title-link(h2) Default items list rendering with selection
-  w-radios.mt-4(v-model="multiple" name="multiple" :items="radios" inline)
-  w-flex(wrap align-center)
-    w-list.mt-6.grow.mr-4(
-      v-model="selectedItem1"
-      :items="listItems1"
-      color="deep-purple"
-      :multiple="multiple"
-      @item-click="itemClicked = $event")
-    div(v-if="itemClicked")
-      .subtitle clicked item (#[span.code item-click] event):
-      br
-      span.code {{ itemClicked }}
-  .subtitle.mt-2
-    | Selected item:
-    code.ml-2(v-if="!multiple || Array.isArray(selectedItem1)") {{ selectedItem1 || 'null' }}
-    code.ml-2(v-else-if="multiple") {{ (selectedItem1 ? [selectedItem1] : '[]') }}
+  example
+    w-radios(v-model="multiple" name="multiple" :items="radios" inline)
+    w-flex(wrap align-center)
+      w-list.mt-6.grow.mr-4(
+        v-model="selectedItem1"
+        :items="listItems1"
+        color="deep-purple"
+        :multiple="multiple"
+        @item-click="itemClicked = $event")
+      div(v-if="itemClicked")
+        .subtitle clicked item (#[span.code item-click] event):
+        br
+        span.code {{ itemClicked }}
+    .subtitle.mt-2
+      | Selected item:
+      code.ml-2(v-if="!multiple || Array.isArray(selectedItem1)") {{ selectedItem1 || 'null' }}
+      code.ml-2(v-else-if="multiple") {{ selectedItem1 ? [selectedItem1] : '[]' }}
+    template(#html).
+      &lt;w-radios v-model="multiple" name="multiple" :items="radios" inline /&gt;
+
+      &lt;w-flex wrap align-center&gt;
+        &lt;w-list
+          v-model="selectedItem"
+          :items="listItems"
+          color="deep-purple"
+          :multiple="multiple"
+          @item-click="itemClicked = $event"
+          class="mt-6 mr-4 grow" /&gt;
+        &lt;div v-if="itemClicked"&gt;
+          &lt;div class="subtitle"&gt;clicked item:&lt;/div&gt;
+          &lt;span&gt;{{ '\{\{ itemClicked \}\}' }}&lt;/span&gt;
+        &lt;/div&gt;
+      &lt;/w-flex&gt;
+      &lt;div class="subtitle mt-2"&gt;
+        Selected item:
+        &lt;code v-if="!multiple || Array.isArray(selectedItem)" class="ml-2"&gt;
+          {{ '\{\{ selectedItem || \'null\' \}\}' }}
+        &lt;/code&gt;
+        &lt;code v-else-if="multiple" class="ml-2"&gt;
+          {{ '\{\{ selectedItem ? [selectedItem] : \'[]\' \}\}' }}
+        &lt;/code&gt;
+      &lt;/div&gt;
+    template(#js).
+      data: () => ({
+        radios: [
+          { label: 'Single selection', value: false },
+          { label: 'Multiple selections', value: true }
+        ],
+        multiple: false,
+        listItems: [
+          { label: 'Item 1', value: 1 },
+          { label: 'Item 2', value: 2 },
+          { label: 'Item 3', value: 3 },
+          { label: 'Item 4', value: 4 },
+          { label: 'Item 5', value: 5 },
+          { label: 'Item 6', value: 6, disabled: true }
+        ],
+        selectedItem: 2,
+        itemClicked: null
+      })
   p Note: you can get the list selection from the v-model, or by listening to the #[span.code input] event.
 
   title-link(h2) Checklists
-  .w-flex.wrap
-    .grow
-      .subtitle Default
-      w-list.mt-6.mr-4(
-        v-model="selectedItem3"
-        :items="listItems1"
-        checklist
-        color="green")
-    .grow
-      .subtitle Round checkboxes
-      w-list.mt-6(
-        v-model="selectedItem4"
-        :items="listItems1"
-        checklist
-        round-checkboxes
-        color="green")
+  example
+    .w-flex.wrap
+      .grow
+        .subtitle Default
+        w-list.mt-6.mr-4(
+          v-model="selectedItem3"
+          :items="listItems1"
+          checklist
+          color="green")
+      .grow
+        .subtitle Round checkboxes
+        w-list.mt-6(
+          v-model="selectedItem4"
+          :items="listItems1"
+          checklist
+          round-checkboxes
+          color="green")
 
-  .w-flex.wrap.mt-4
-    .grow
-      .subtitle Custom rendering, default styles
-      w-list.mt-6(
-        :items="listItems2"
-        item-value="id"
-        checklist
-        color="primary")
-        template(v-slot:item="{ item }")
-          span {{ item.label }}
-          .spacer
-          w-icon(md) {{ item.icon }}
-    .grow
-      .subtitle Custom rendering &amp; styles
-      w-list.custom.mt-6(
-        :items="listItems2"
-        item-value="id"
-        checklist
-        color="primary")
-        template(v-slot:item="{ item }")
-          span {{ item.label }}
-          .spacer
-          w-icon(md) {{ item.icon }}
+    .w-flex.wrap.mt-4
+      .grow
+        .subtitle Custom rendering, default styles
+        w-list.mt-6(
+          :items="listItems2"
+          item-value="id"
+          checklist
+          color="primary")
+          template(#item="{ item }")
+            span {{ item.label }}
+            .spacer
+            w-icon(md) {{ item.icon }}
+      .grow
+        .subtitle Custom rendering &amp; styles
+        w-list.custom.mt-6(
+          :items="listItems2"
+          item-value="id"
+          checklist
+          color="primary")
+          template(#item="{ item }")
+            span {{ item.label }}
+            .spacer
+            w-icon(md) {{ item.icon }}
+    template(#html).
+      .w-flex.wrap
+        .grow
+          .subtitle Default
+          w-list.mt-6.mr-4(
+            v-model="selectedItem1"
+            :items="listItems1"
+            checklist
+            color="green")
+        .grow
+          .subtitle Round checkboxes
+          w-list.mt-6(
+            v-model="selectedItem2"
+            :items="listItems1"
+            checklist
+            round-checkboxes
+            color="green")
+
+      .w-flex.wrap.mt-4
+        .grow
+          .subtitle Custom rendering, default styles
+          w-list.mt-6(
+            :items="listItems2"
+            item-value="id"
+            checklist
+            color="primary")
+            template(#item="{ item }")
+              span {{ '\{\{ item.label \}\}' }}
+              .spacer
+              w-icon(md) {{ '\{\{ item.icon \}\}' }}
+        .grow
+          .subtitle Custom rendering &amp; styles
+          w-list.custom.mt-6(
+            :items="listItems2"
+            item-value="id"
+            checklist
+            color="primary")
+            template(#item="{ item }")
+              span {{ '\{\{ item.label \}\}' }}
+              .spacer
+              w-icon(md) {{ '\{\{ item.icon \}\}' }}
+    template(#js).
+      data: () => ({
+        listItems1: [
+          { label: 'Item 1', value: 1 },
+          { label: 'Item 2', value: 2 },
+          { label: 'Item 3', value: 3 },
+          { label: 'Item 4', value: 4 },
+          { label: 'Item 5', value: 5 },
+          { label: 'Item 6', value: 6, disabled: true }
+        ],
+        listItems2: [
+          { label: 'Star', id: 'star', icon: 'mdi mdi-star', route: '#route-to-star' },
+          { label: 'Check', id: 'check', icon: 'mdi mdi-check', route: '#route-to-check' },
+          { label: 'Cross', id: 'cross', icon: 'mdi mdi-close', route: '#route-to-cross' },
+          { label: 'Info', id: 'info', icon: 'mdi mdi-information', route: '#route-to-info' },
+          { label: 'Warning', id: 'warning', icon: 'mdi mdi-alert', route: '#route-to-warning' },
+          { label: 'Disabled', id: 'disabled', icon: 'mdi mdi-minus-circle', route: '#disabled', disabled: true }
+        ],
+        selectedItem1: [1, 3],
+        selectedItem2: 1
+      })
 
   title-link(h2) Navigation lists
   p.
     The navigation list expects a #[code route] attribute in the items that are links.#[br]
     It will create a router link or a normal link (if not using vue-router) on those items.
   p It is possible to disable a link with a #[code disabled: true] attribute in the item.
-  w-list.mt-6(:items="listItems2" nav color="primary")
-    template(v-slot:item="{ item }")
-      span {{ item.label }}
-      .spacer
-      w-icon(md) {{ item.icon }}
+  example
+    w-list.mt-6(:items="listItems2" nav color="primary")
+      template(#item="{ item }")
+        span {{ item.label }}
+        .spacer
+        w-icon(md) {{ item.icon }}
+    template(#html).
+      w-list.mt-6(:items="listItems" nav color="primary")
+        template(#item="{ item }")
+          span {{ '\{\{ item.label \}\}' }}
+          .spacer
+          w-icon(md) {{ '\{\{ item.icon \}\}' }}
+    template(#js).
+      data: () => ({
+        listItems: [
+          { label: 'Star', id: 'star', icon: 'mdi mdi-star', route: '#route-to-star' },
+          { label: 'Check', id: 'check', icon: 'mdi mdi-check', route: '#route-to-check' },
+          { label: 'Cross', id: 'cross', icon: 'mdi mdi-close', route: '#route-to-cross' },
+          { label: 'Info', id: 'info', icon: 'mdi mdi-information', route: '#route-to-info' },
+          { label: 'Warning', id: 'warning', icon: 'mdi mdi-alert', route: '#route-to-warning' },
+          { label: 'Disabled', id: 'disabled', icon: 'mdi mdi-minus-circle', route: '#disabled', disabled: true }
+        ]
+      })
 </template>
 
 <script>
