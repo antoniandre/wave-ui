d9b702e22a75772aa9e62a988aba5d6777fc68b8
Antoni Andre <antoniandre.web@gmail.com>
2020-07-22 00:16:28 +1000

    Publish documentation on Github.

--- docs/js/form-vue.b85da6ee.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["form-vue"],{"97f8":function(t,e,o){"use strict";o.r(e);var s=function(){var t=this,e=t.$createElement,o=t._self._c||e;return o("div",[o("title-link",{staticClass:"mt-4 code",attrs:{h1:"",slug:"w-form"}},[t._v("w-form"),o("w-tag",{staticClass:"ml-3",attrs:{"bg-color":"red",color:"white"}},[t._v("Coming soon")])],1)],1)},l=[],a={},n=a,r=o("2877"),c=Object(r["a"])(n,s,l,!1,null,null,null);e["default"]=c.exports}}]);
-//# sourceMappingURL=form-vue.b85da6ee.js.map
--- docs/js/slideshow-vue.4a8b63b1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["slideshow-vue"],{"5ebe":function(e,t,s){"use strict";s.r(t);var l=function(){var e=this,t=e.$createElement,s=e._self._c||t;return s("div",[s("title-link",{staticClass:"mt-4 code",attrs:{h1:""}},[e._v("w-slideshow")])],1)},n=[],a={},i=a,c=s("2877"),o=Object(c["a"])(i,l,n,!1,null,null,null);t["default"]=o.exports}}]);
-//# sourceMappingURL=slideshow-vue.4a8b63b1.js.map
