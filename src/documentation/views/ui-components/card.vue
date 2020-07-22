3a9c7179161e418900e3efb255b4093aa1960cfb
Antoni Andre <antoniandre.web@gmail.com>
2020-07-22 20:03:42 +1000

    Add card examples.

--- src/documentation/views/ui-components/card.vue
+++ src/documentation/views/ui-components/card.vue
@@ -3,67 +3,126 @@ div
   title-link.mt4.code(h1) w-card
 
   title-link(h2) Card with a title and no color
-  w-card.mt4(card-title="Card title")
-    p.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-      odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
-      Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
-      dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
-      aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+  example
+    w-card(card-title="Card title")
+      p.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
+        Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+        dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+        aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+    template(#pug).
+      w-card(card-title="Card title")
+        p.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+          quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+          odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
+          Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+          dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+          aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
 
   title-link(h2) Colored card with no title and no border
-  w-card.mt4(bg-color="secondary" no-border)
-    p.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-      odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
-      Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
-      dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
-      aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+  example
+    w-card(bg-color="secondary" no-border)
+      p.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
+        Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+        dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+        aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+    template(#pug).
+      w-card(bg-color="secondary" no-border)
+        p.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+          quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+          odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
+          Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+          dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+          aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
 
   title-link(h2) Tiled card with a top toolbar
   p.
     The w-card component will detect if there is a toolbar in the title slot or in the actions
     slot and will remove the default padding and border on this slot if there is.
-  w-card.mt4(tile)
-    template(v-slot:title)
-      w-toolbar
-        .subtitle.my0 Card title
-        .spacer
-        span.ml2 Item 1
-        span.ml2 Item 2
-        span.ml2 Item 3
-    p.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-      odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
-      Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
-      dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
-      aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+  example
+    w-card(tile)
+      template(#title)
+        w-toolbar
+          .subtitle.my0 Card title
+          .spacer
+          span.ml2 Item 1
+          span.ml2 Item 2
+          span.ml2 Item 3
+      p.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
+        Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+        dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+        aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+    template(#pug).
+      w-card(tile)
+        template(#title="")
+          w-toolbar
+            .subtitle.my0 Card title
+            .spacer
+            span.ml2 Item 1
+            span.ml2 Item 2
+            span.ml2 Item 3
+        p.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+          quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+          odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
+          Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+          dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+          aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
 
   title-link(h2) Card with shadow
-  w-card.mt4(shadow)
-    p.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-      odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
-      Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
-      dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
-      aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+  example
+    w-card(shadow)
+      p.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        odit dolor labore eveniet at vel sequi nostrum.#[br]#[br]
+        Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+        dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+        aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+    template(#pug).
+      w-card(shadow)
+        p.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+          quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+          odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
+          Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+          dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+          aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
 
   title-link(h2) Card with actions
   p The actions slot is optional but facilitates the addition of action buttons in a flex layout.
-  w-card.mt4
-    template(v-slot:title) Terms and conditions
-    p.
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-      quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-      odit dolor labore eveniet at vel sequi nostrum.
-    template(v-slot:actions)
-      .spacer
-      w-button.mr2(bg-color="error") I disagree
-      w-button(bg-color="success") I agree
+  example
+    w-card
+      template(#title) Terms and conditions
+      p.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        odit dolor labore eveniet at vel sequi nostrum.
+      template(#actions)
+        .spacer
+        w-button.mr2(bg-color="error") I disagree
+        w-button(bg-color="success") I agree
+    template(#pug).
+      w-card
+        template(#title="") Terms and conditions
+        p.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+          quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+          odit dolor labore eveniet at vel sequi nostrum.
+        template(#actions="")
+          .spacer
+          w-button.mr2(bg-color="error") I disagree
+          w-button(bg-color="success") I agree
 </template>
 
 <script>
