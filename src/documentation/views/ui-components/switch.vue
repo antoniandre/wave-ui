219f1c438852f366f2389abfced52a2428b2d399
Antoni Andre <antoniandre.web@gmail.com>
2020-07-26 12:50:10 +1000

    Add code source for switch examples.

--- src/documentation/views/ui-components/switch.vue
+++ src/documentation/views/ui-components/switch.vue
@@ -2,32 +2,59 @@
 div
   title-link.mt4.code(h1) w-switch
 
-  .w-flex.wrap.align-center
-    w-switch.mr4.mb1(:value="true" label="On")
-    w-switch.mr4.mb1(:value="false" label="Off")
-    w-switch.mr4.mb1(:value="true" disabled label="On disabled")
-    w-switch(:value="false" disabled label="Off disabled")
+  title-link(h2 slug="on-off-and-disabled") On, off &amp; disabled
+  example(content-class="pt4")
+    w-switch.ma2(:value="true" label="On")
+    w-switch.ma2(:value="false" label="Off")
+    w-switch.ma2(:value="true" disabled label="On disabled")
+    w-switch.ma2(:value="false" disabled label="Off disabled")
+    template(#pug).
+      w-switch.ma2(:value="true" label="On")
+      w-switch.ma2(:value="false" label="Off")
+      w-switch.ma2(:value="true" disabled label="On disabled")
+      w-switch.ma2(:value="false" disabled label="Off disabled")
 
   title-link(h2) V-model
-  w-flex.wrap.align-center
-    w-switch.mr3.mb1(v-model="value")
-    span.title3
+  example(content-class="w-flex wrap align-center")
+    w-switch.mr6(v-model="value")
+    span.title3.mb0
       | v-model value:
       code.ml2 {{ value }}
+    template(#pug).
+      w-switch.mr6(v-model="value")
+      span.title3.mb0
+        | v-model value:
+        code.ml2 {{ '\{\{ value \}\}' }}
+    template(#js).
+      data: () => ({
+        value: true
+      })
 
   title-link(h2) Colors
-  w-switch.mr3.mb1(:value="true" color="pink")
-  w-switch.mr3.mb1(:value="true" color="error")
-  w-switch.mr3.mb1(:value="true" color="warning")
-  w-switch.mr3.mb1(:value="true" color="success")
-  w-switch.mr3.mb1(:value="true" color="info")
+  example
+    w-switch.ma2(:value="true" color="pink")
+    w-switch.ma2(:value="true" color="error")
+    w-switch.ma2(:value="true" color="warning")
+    w-switch.ma2(:value="true" color="success")
+    w-switch.ma2(:value="true" color="info")
+    template(#pug).
+      w-switch.ma2(:value="true" color="pink")
+      w-switch.ma2(:value="true" color="error")
+      w-switch.ma2(:value="true" color="warning")
+      w-switch.ma2(:value="true" color="success")
+      w-switch.ma2(:value="true" color="info")
 
   title-link(h2) Thin version
-  .w-flex.wrap.align-center
-    w-switch.mr4.mb1(:value="true" thin label="On")
-    w-switch.mr4.mb1(:value="false" thin label="Off")
-    w-switch.mr4.mb1(:value="true" disabled thin label="On disabled")
-    w-switch(:value="false" disabled thin label="Off disabled")
+  example(content-class="pt5")
+    w-switch.ma2(:value="true" thin label="On")
+    w-switch.ma2(:value="false" thin label="Off")
+    w-switch.ma2(:value="true" disabled thin label="On disabled")
+    w-switch.ma2(:value="false" disabled thin label="Off disabled")
+    template(#pug).
+      w-switch.ma2(:value="true" thin label="On")
+      w-switch.ma2(:value="false" thin label="Off")
+      w-switch.ma2(:value="true" disabled thin label="On disabled")
+      w-switch.ma2(:value="false" disabled thin label="Off disabled")
 </template>
 
 <script>
