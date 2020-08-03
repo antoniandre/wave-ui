f969cc7060e91f2898dd96db7be5d983b33cbaab
Antoni Andre <antoniandre.web@gmail.com>
2020-08-04 00:00:31 +1000

    Publish documentation on Github.

--- docs/js/slider-vue.b75106b6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["slider-vue"],{f26f:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("title-link",{staticClass:"mt4 code",attrs:{h1:""}},[e._v("w-slider")]),n("input",{attrs:{type:"range"}}),n("w-slider")],1)},l=[],a={},i=a,r=n("2877"),u=Object(r["a"])(i,s,l,!1,null,null,null);t["default"]=u.exports}}]);
-//# sourceMappingURL=slider-vue.b75106b6.js.map
