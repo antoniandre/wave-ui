bc6d5cc0543144259674d71a5f1887de00de041f
Antoni Andre <antoniandre.web@gmail.com>
2020-08-05 11:33:48 +1000

    Build the basic w-accordion component.

--- src/documentation/views/ui-components/accordion.vue
+++ src/documentation/views/ui-components/accordion.vue
@@ -2,10 +2,34 @@
 div
   title-link.mt4.code(h1) w-accordion
 
+  w-accordion(:items="items")
+
+  w-accordion.mt6(:items="items")
+    template(#item-title="{ item }")
+      w-icon(md) wi-star
+      .title2.my0.ml2 {{ item.title }}
+    template(#item-content="{ item }")
+      p(v-html="item.content")
+
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    items: [
+      {
+        title: 'Item 1',
+        content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi ut neque enim beatae autem iure, velit labore aut ipsa voluptatem aliquam distinctio harum. Possimus aliquid voluptatum facere incidunt dolor a.'
+      },
+      {
+        title: 'Item 2',
+        content: 'Unde quo consequatur doloremque fugiat aperiam hic rerum, dolores aspernatur. Reiciendis aspernatur ab fugiat, saepe vel aut eaque molestiae eos commodi minus. Dolorum voluptatibus fugiat, incidunt magnam repudiandae fugit dolor!'
+      },
+      {
+        title: 'Item 3',
+        content: 'Enim minus excepturi totam dolorem iure nisi aperiam asperiores. Saepe blanditiis, magni ab fugit exercitationem optio. Beatae totam vitae autem consectetur accusantium dolor expedita, earum quaerat? Sed at perspiciatis aliquid.'
+      }
+    ]
+  })
 }
 </script>
--- src/wave-ui/components/w-accordion.vue
+++ src/wave-ui/components/w-accordion.vue
@@ -1,21 +1,48 @@
 <template lang="pug">
-  .w-accordion(:class="classes" :style="styles")
-    slot
+  .w-accordion
+    .w-accordion__item(
+      v-for="(item, i) in accordionItems"
+      :key="i"
+      tabindex="0"
+      @keypress.enter="item.open = true"
+      :class="{ ...itemClasses, 'w-accordion__item--open': item.open }")
+      .w-accordion__item-title(@click="item.open = !item.open")
+        w-button.w-accordion__expand-icon(:icon="expandIcon" text @click.stop="item.open = !item.open")
+        slot(name="item-title" :item="item")
+          div(v-html="item.title")
+      w-transition-expand(y)
+        .w-accordion__item-content(v-if="item.open")
+          slot(name="item-content" :item="item")
+            div(v-html="item.content")
 </template>
 
 <script>
 export default {
   name: 'w-accordion',
   props: {
+    value: { type: Array },
+    items: { type: Array },
+    itemClass: { type: String },
+    expandIcon: { type: String, default: 'wi-triangle-down' },
+    collapseIcon: { type: String },
   },
 
+  data: () => ({
+    open: []
+  }),
+
   computed: {
-    classes () {
-      return {
-      }
+    accordionItems () {
+      return this.items.slice(0).map(item => {
+        this.$set(item, 'open', false)
+        return item
+      })
     },
-    styles () {
-      return false
+
+    itemClasses () {
+      return {
+        [this.itemClass]: this.itemClass || null
+      }
     }
   }
 }
@@ -23,5 +50,44 @@ export default {
 
 <style lang="scss">
 .w-accordion {
+  z-index: 1;
+
+  &__item {
+    position: relative;
+  }
+
+  button.w-accordion__expand-icon {
+    color: #999;
+    .w-icon:before {font-size: 1.2em;}
+  }
+
+  &__item-title {
+    position: relative;
+    display: flex;
+    align-items: center;
+    font-size: round(1.5 * $base-font-size);
+    padding-top: 2 * $base-increment;
+    padding-bottom: 2 * $base-increment;
+    user-select: none;
+    cursor: pointer;
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: currentColor;
+      opacity: 0;
+      @include default-fast-transition;
+    }
+    &:hover:before {
+      opacity: 0.05;
+    }
+  }
+
+  &__item-content {
+  }
 }
 </style>
