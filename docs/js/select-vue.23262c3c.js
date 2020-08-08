5b0398307268ab38a39bdb8075cc7e89719e3b88
Antoni Andre <antoniandre.web@gmail.com>
2020-08-08 19:03:43 +1000

    Publish documentation to Github.

--- docs/js/select-vue.23262c3c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["select-vue"],{ef0b:function(e,t,l){"use strict";l.r(t);var n=function(){var e=this,t=e.$createElement,l=e._self._c||t;return l("div",[l("title-link",{staticClass:"mt4 code",attrs:{h1:""}},[e._v("w-select")])],1)},s=[],a={},c=a,i=l("2877"),u=Object(i["a"])(c,n,s,!1,null,null,null);t["default"]=u.exports}}]);
-//# sourceMappingURL=select-vue.23262c3c.js.map
