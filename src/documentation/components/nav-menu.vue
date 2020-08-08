bbd9cffff2c522287e84f98f9e91b8002f4ea5e0
Antoni Andre <antoniandre.web@gmail.com>
2020-08-08 13:35:20 +1000

    Wrap up the w-badge component & start w-slider.

--- src/documentation/components/nav-menu.vue
+++ src/documentation/components/nav-menu.vue
@@ -49,7 +49,7 @@ export default {
       { label: 'Accordion', route: 'w-accordion' },
       { label: 'Alert', route: 'w-alert' },
       { label: 'App', route: 'w-app' },
-      { label: 'Badge', route: 'w-badge', inProgress: true },
+      { label: 'Badge', route: 'w-badge' },
       { label: 'Button', route: 'w-button' },
       { label: 'Calendar', route: 'w-calendar' },
       { label: 'Card', route: 'w-card' },
@@ -77,7 +77,7 @@ export default {
           { label: 'Form', route: 'w-form', inProgress: true },
           { label: 'Radio', route: 'w-radio' },
           { label: 'Select', route: 'w-select', disabled: true },
-          { label: 'Slider', route: 'w-slider', disabled: true },
+          { label: 'Slider', route: 'w-slider', inProgress: true },
           { label: 'Switch', route: 'w-switch' },
           { label: 'Textarea', route: 'w-textarea', disabled: true }
         ]
