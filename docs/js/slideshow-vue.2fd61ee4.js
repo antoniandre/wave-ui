ed193d7ce30c001397177836eb8a634d0680240d
Antoni Andre <antoniandre.web@gmail.com>
2020-08-10 23:57:20 +1000

    Publish documentation on Github.

--- docs/js/slideshow-vue.2fd61ee4.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["slideshow-vue"],{"5ebe":function(e,t,s){"use strict";s.r(t);var l=function(){var e=this,t=e.$createElement,s=e._self._c||t;return s("div",[s("title-link",{staticClass:"mt4 code",attrs:{h1:""}},[e._v("w-slideshow")])],1)},n=[],a={},i=a,c=s("2877"),o=Object(c["a"])(i,l,n,!1,null,null,null);t["default"]=o.exports}}]);
-//# sourceMappingURL=slideshow-vue.2fd61ee4.js.map
