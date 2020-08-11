da081a61b9b734585628afc45dc8c1fc8538973d
Antoni Andre <antoniandre.web@gmail.com>
2020-08-11 19:18:44 +1000

    Start the w-select component.

--- src/wave-ui/components/w-select.vue
+++ src/wave-ui/components/w-select.vue
@@ -1,13 +1,25 @@
 <template lang="pug">
   .w-select(:class="classes" :style="styles")
-    select
-      option -
+    select(:name="name || null" :multiple="multiple")
+      option(v-for="(item, i) in items" :key="i" :value="item[itemValue]" v-html="item[itemLabel]")
 </template>
 
 <script>
 export default {
   name: 'w-select',
   props: {
+    items: { type: Array, required: true },
+    value: {}, // v-model on selected item if any.
+    name: { type: String },
+    multiple: { type: Boolean },
+    placeholder: { type: String },
+    itemLabel: { type: String, default: 'label' }, // Name of the label field.
+    itemValue: { type: String, default: 'value' }, // Name of the value field.
+    itemClass: { type: String },
+    outline: { type: Boolean },
+    round: { type: Boolean },
+    shadow: { type: Boolean },
+    tile: { type: Boolean }
   },
 
   computed: {
@@ -24,5 +36,15 @@ export default {
 
 <style lang="scss">
 .w-select {
+  display: inline-flex;
+  position: relative;
+
+  &__input-wrap--round {
+    border-radius: 5em;
+    padding-left: 3 * $base-increment;
+    padding-right: 3 * $base-increment;
+  }
+  &__input-wrap--tile {border-radius: initial;}
+  &__input-wrap--shadow {box-shadow: $box-shadow;}
 }
 </style>
