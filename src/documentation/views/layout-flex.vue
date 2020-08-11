1134ad649196843411e98bda3e01c6534e162d09
Antoni Andre <antoniandre.web@gmail.com>
2020-08-11 22:06:54 +1000

    Update the layout flex demo page.

--- src/documentation/views/layout-flex.vue
+++ src/documentation/views/layout-flex.vue
@@ -92,7 +92,10 @@ div
     The #[code.mx1 .spacer] class is also very helpful to separate 2 elements as is will occupy the
     available space and push the elements on each side.
 
-  .title4.mt6 w-flex, row direction (default)
+  .w-divider.my12
+  title-link.mt12.headline(h2) Examples
+  title-link.mt8.title2(h3) Justify
+  .title3.mt6 w-flex, row direction (default)
   example
     p.code.mb1 .justify-start (default)
     .w-flex.pa3.blue-light5--bg.justify-start
@@ -161,9 +164,9 @@ div
         padding: 12px;
       }
 
-  .title4 w-flex, column direction
+  .title3 w-flex, column direction
   example
-    w-flex
+    w-flex(basis-zero wrap)
       .grow.mx1
         p.code.mb1 .justify-start (default)
         w-flex.wrapper(column align-center justify-start)
@@ -189,7 +192,7 @@ div
           .box
           .box
     template(#html).
-      &lt;w-flex&gt;
+      &lt;w-flex basis-zero wrap&gt;
         &lt;div class="grow mx1"&gt;
           &lt;p&gt;.justify-start (default)&lt;/p&gt;
           &lt;w-flex column align-center justify-start class="wrapper"&gt;
@@ -239,52 +242,56 @@ div
         height: 140px;
       }
 
-  title-link.mt10(h3 slug="align-self") #[span.code align-self] and #[em not #[span.code justify-self]]
+  title-link.mt12.title2(h3) Align
   p.
     By default if you don't set an alignment on the #[span.code w-flex] component, the content will
     be stretched vertically.#[br]
     If it is not what you want, you can either add an alignment on the #[span.code w-flex] component
     (case 2), or a self align on the children (case 3).
+
+  .title3.mt6 w-flex, row direction (default)
   example
-    w-flex
-      .grow.mx1
+    w-flex(basis-zero wrap)
+      .grow.mx1.xs12.sm4
         p 1. Default
         w-flex.wrapper2(justify-center)
           .box
           .box
           .box
-      .grow.mx1
-        p 2. align-center
+      .grow.mx1.xs12.sm4
+        p 2. Align center
         w-flex.wrapper2(justify-center align-center)
           .box
           .box
           .box
-      .grow.mx1
-        p 3. align self start, center, end
+      .grow.mx1.xs12.sm4
+        p 3. Align self start, center, end
         w-flex.wrapper2(justify-center)
           .box.align-self-start
           .box.align-self-center
           .box.align-self-end
     template(#html).
-      &lt;w-flex&gt;
+      &lt;w-flex basis-zero&gt;
         &lt;div class="grow mx1"&gt;
-          &lt;p&gt;Default&lt;/p&gt;
+          &lt;p&gt;1. Default&lt;/p&gt;
           &lt;w-flex justify-center class="wrapper"&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
           &lt;/w-flex&gt;
         &lt;/div&gt;
+
         &lt;div class="grow mx1"&gt;
-          &lt;p&gt;2. align-center&lt;/p&gt;
+          &lt;p&gt;2. Align center&lt;/p&gt;
           &lt;w-flex justify-center align-center class="wrapper"&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
             &lt;div class="box"&gt;&lt;/div&gt;
           &lt;/w-flex&gt;
         &lt;/div&gt;
+
         &lt;div class="grow mx1"&gt;
-          &lt;p&gt;3. align self start, center, end&lt;/p&gt;
+          &lt;p&gt;3. Align self start, center, end&lt;/p&gt;
           &lt;w-flex justify-center class="wrapper"&gt;
             &lt;div class="box align-self-start"&gt;&lt;/div&gt;
             &lt;div class="box align-self-center"&gt;&lt;/div&gt;
@@ -305,8 +312,126 @@ div
         height: 140px;
       }
 
+  .title3.mt6 Align-self, column direction
+  example
+    w-flex(basis-zero wrap)
+      .grow.mx1.xs12.sm4
+        p 1. Default
+        w-flex.wrapper2(column justify-center)
+          .box
+          .box
+          .box
+      .grow.mx1.xs12.sm4
+        p 2. Align-center
+        w-flex.wrapper2(column justify-center align-center)
+          .box
+          .box
+          .box
+      .grow.mx1.xs12.sm4
+        p 3. Align self start, center, end
+        w-flex.wrapper2(column justify-center)
+          .box.align-self-start
+          .box.align-self-center
+          .box.align-self-end
+    template(#html).
+      &lt;w-flex basis-zero&gt;
+        &lt;div class="grow mx1"&gt;
+          &lt;p&gt;1. Default&lt;/p&gt;
+          &lt;w-flex column justify-center class="wrapper"&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+          &lt;/w-flex&gt;
+        &lt;/div&gt;
+
+        &lt;div class="grow mx1"&gt;
+          &lt;p&gt;2. Align-center&lt;/p&gt;
+          &lt;w-flex column justify-center align-center class="wrapper"&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+          &lt;/w-flex&gt;
+        &lt;/div&gt;
+
+        &lt;div class="grow mx1"&gt;
+          &lt;p&gt;3. Align self start, center, end&lt;/p&gt;
+          &lt;w-flex column justify-center class="wrapper"&gt;
+            &lt;div class="box align-self-start"&gt;&lt;/div&gt;
+            &lt;div class="box align-self-center"&gt;&lt;/div&gt;
+            &lt;div class="box align-self-end"&gt;&lt;/div&gt;
+          &lt;/w-flex&gt;
+        &lt;/div&gt;
+      &lt;/w-flex&gt;
+    template(#css).
+      .box {
+        background-color: #9bbff9;
+        border: 1px solid #55f;
+        padding: 12px;
+      }
+
+      .wrapper {
+        background-color: #eef7ff;
+        padding: 12px;
+        height: 140px;
+      }
+
+  title-link.mt12.title2(h3) Using margin in flex context
+  w-alert.alert--warning(warning icon-outside).
+    It's important to note that setting a margin on an element in a flex content will override its
+    default positioning.#[br]
+    Look at the following examples where the boxes have different margins applied.#[br]
+    #[code maa] = #[span.code margin: auto], #[code mxa] = #[span.code margin-left: auto;margin-right: auto;]
+    (read more in #[router-link(to="layout--spaces") Layout - spaces])
+  example
+    w-flex(basis-zero)
+      .grow.mx1
+        p 1. Row direction
+        w-flex.wrapper2(justify-center align-center)
+          .box.maa
+          .box.maa
+          .box.maa
+      .grow.mx1
+        p 2. Column direction
+        w-flex.wrapper2(column justify-center)
+          .box
+          .box.mxa
+          .box
+    template(#html).
+      &lt;w-flex basis-zero&gt;
+        &lt;div class="grow mx1"&gt;
+          &lt;p&gt;1. Row direction&lt;/p&gt;
+          &lt;w-flex justify-center align-center class="wrapper"&gt;
+            &lt;div class="box maa"&gt;&lt;/div&gt;
+            &lt;div class="box maa"&gt;&lt;/div&gt;
+            &lt;div class="box maa"&gt;&lt;/div&gt;
+          &lt;/w-flex&gt;
+        &lt;/div&gt;
+
+        &lt;div class="grow mx1"&gt;
+          &lt;p&gt;2. Column direction&lt;/p&gt;
+          &lt;w-flex column justify-center class="wrapper"&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+            &lt;div class="box mxa"&gt;&lt;/div&gt;
+            &lt;div class="box"&gt;&lt;/div&gt;
+          &lt;/w-flex&gt;
+        &lt;/div&gt;
+      &lt;/w-flex&gt;
+    template(#css).
+      .box {
+        background-color: #9bbff9;
+        border: 1px solid #55f;
+        padding: 12px;
+      }
+
+      .wrapper {
+        background-color: #eef7ff;
+        padding: 12px;
+        height: 140px;
+      }
+
+  title-link.mt12.title3(h3 slug="not-justify-self") #[em Not #[span.code justify-self]]
+
   w-alert.alert--warning(warning icon-outside)
-    strong Note:
     a.black.ml1(
       href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Box_Alignment/Box_Alignment_in_Flexbox"
       target="_blank"
