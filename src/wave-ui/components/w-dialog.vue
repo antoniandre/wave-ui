35afd8fb944e1eb40e56494f681002e2f2d7a977
Antoni Andre <antoniandre.web@gmail.com>
2020-08-12 15:40:16 +1000

    Add the title prop and slot on w-dialog.

--- src/wave-ui/components/w-dialog.vue
+++ src/wave-ui/components/w-dialog.vue
@@ -13,11 +13,12 @@
         v-if="showContent" no-border
         :title-class="titleClass"
         :content-class="contentClass"
+        :title="title || undefined"
         :style="contentStyles")
-        template(v-slot:title)
+        template(v-if="$slots.title" v-slot:title)
           slot(name="title")
         slot
-        template(v-slot:actions)
+        template(v-if="$slots.actions" v-slot:actions)
           slot(name="actions")
 </template>
 
@@ -25,12 +26,13 @@
 export default {
   name: 'w-dialog',
   props: {
-    value: { type: Boolean },
+    value: { type: Boolean, default: true },
     width: { type: Number, default: 0 },
     fullscreen: { type: Boolean },
     persistent: { type: Boolean },
     persistentNoAnimation: { type: Boolean },
     tile: { type: Boolean },
+    title: { type: String },
     transition: { type: String, default: 'fade' }, // @todo: validator.
     titleClass: { type: String },
     contentClass: { type: String },
