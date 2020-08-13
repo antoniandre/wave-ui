2a0c57c376bbc9081350cfa5d9f93cb1aca8d461
Antoni Andre <antoniandre.web@gmail.com>
2020-08-13 12:45:46 +1000

    Publish documentation on Github.

--- docs/js/tabs-vue.cd4a4c9e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["tabs-vue"],{5773:function(t,e,a){"use strict";a.r(e);var n=function(){var t=this,e=t.$createElement,a=t._self._c||e;return a("div",[a("title-link",{staticClass:"mt4 code",attrs:{h1:""}},[t._v("w-tabs")])],1)},s=[],l={},c=l,i=a("2877"),u=Object(i["a"])(c,n,s,!1,null,null,null);e["default"]=u.exports}}]);
-//# sourceMappingURL=tabs-vue.cd4a4c9e.js.map
