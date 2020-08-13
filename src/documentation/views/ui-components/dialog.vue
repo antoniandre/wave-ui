c247d5b6ac996bed048045be514b2a5e367beaac
Antoni Andre <antoniandre.web@gmail.com>
2020-08-13 15:06:25 +1000

    Update dialog examples.

--- src/documentation/views/ui-components/dialog.vue
+++ src/documentation/views/ui-components/dialog.vue
@@ -89,12 +89,12 @@ div
     template(#pug).
       w-button.px4(@click="dialog.show = true" bg-color="primary" dark) Open dialog
       w-dialog(v-model="dialog.show" title="Terms and conditions" persistent :width="550")
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
-        quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
-        odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
-        Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
-        dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
-        aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
+        | Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem dolore delectus,
+        | quisquam ipsa laudantium esse consequatur itaque similique et eligendi eum voluptas
+        | odit dolor labore eveniet at vel sequi nostrum.#[="#[br]#[br]"]
+        | Voluptates, aut distinctio esse quia doloribus commodi minima inventore neque sequi
+        | dolores perspiciatis fugiat. Fuga, reprehenderit sequi veritatis iure magnam excepturi
+        | aliquid dolore quo amet deserunt asperiores placeat maxime perferendis.
         template(#actions="")
           .spacer
           w-button.mr2(@click="dialog.show = false" bg-color="error") I disagree
