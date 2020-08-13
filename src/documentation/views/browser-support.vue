b0b874ec3f43b2d7b98daef7b39c825cf5938261
Antoni Andre <antoniandre.web@gmail.com>
2020-08-13 22:42:17 +1000

    Add a browser support page.

---
+++ src/documentation/views/browser-support.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+div
+  title-link.mt4(h1) Browser support
+  title-link(h2) All major browsers supported
+  p Wave UI supports:
+  ul
+    li Chrome
+    li Safari
+    li Firefox
+    li Edge
+    li Opera
+
+  title-link(h2) Dropping support for IE11
+  a.title3(href="https://gs.statcounter.com/browser-market-share")
+    span IE's current market share is less than 1.3% worldwide (gs.statcounter.com)
+    w-icon.ml2(md) mdi mdi-open-in-new
+
+  p.title5.
+    Many popular websites have officially drop support for IE include Youtube, GitHub, Meetup, Slack, Zendesk,
+    Trello, Atlassian, Discord, Spotify, Behance, Wix, Huddle, WhatsApp, Google Earth and Yahoo.
+    Even some of Microsoft’s own product’s, like Teams, have severely reduced support for IE.
+
+  .title4.mt8 The distance between Internet Explorer 11 (year 2015) and every other major browser is huge.
+  p.
+    Adding support for this technologically obsolete browser considerably increases the development time
+    and #[strong adding IE11 hacks and workarounds penalizes all the other browser users (more than 98.7%)].
+
+  p IE11 now only exists to support the sites that use it until they’re modernized.
+
+  p
+    strong The web has moved on.
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+.page--browser-support {
+  p {margin-top: 1.5em;}
+}
+</style>
