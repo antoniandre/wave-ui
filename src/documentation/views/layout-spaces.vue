33c2b8451950076f09e8d7e4f65981aaedeb94e6
Antoni Andre <antoniandre.web@gmail.com>
2020-08-16 23:21:41 +1000

    Add documentation about margin auto.

--- src/documentation/views/layout-spaces.vue
+++ src/documentation/views/layout-spaces.vue
@@ -28,13 +28,13 @@ div
     template(#html).
       &lt;div class="orange-light4--bg pa12"&gt;
         &lt;div class="yellow-light5--bg pa3"&gt;
-          The orange container has a pa12 class which results in a padding of: 12 x 4 x base-font-size pixels.
+          The orange container has a `pa12` class which results in a padding of: 12 x 4 x base-font-size pixels.
           The default base font size is 14px but it's easy to override it in your SCSS, as well as the
-          increment for pa-x and ma-x classes.
+          increment for `pax` and `max` classes.
         &lt;/div&gt;
 
         &lt;div class="green-light4--bg mt8 pa3"&gt;
-          This green container has an mt8 class which results in a top margin of: 8 x 4 x base-font-size pixels.
+          This green container has an `mt8` class which results in a top margin of: 8 x 4 x base-font-size pixels.
         &lt;/div&gt;
       &lt;/div&gt;
 
@@ -54,8 +54,23 @@ div
     template(#html).
       &lt;div class="orange-light4--bg pa8"&gt;
         &lt;div class="green-light4--bg mt-8 mx8 pa3"&gt;
-          This green container has a mt-8 class which results in a negative top margin of: 8 x 4 x
+          This green container has a `mt-8` class which results in a negative top margin of: 8 x 4 x
           base-font-size pixels.
+        &lt;/div&gt;
+      &lt;/div&gt;
+
+  title-link(h3) Margin auto
+  p.
+    Last but not least, margin: auto is also possible with the #[code a] modifier.#[br]
+    The syntax for it is #[code="m[side]a"]
+  example
+    .orange-light4--bg.pa8
+      .green-light4--bg.mxa.pa3(style="max-width: 240px").
+        This green container has a #[span.code mxa] class which results in a margin auto on the left &amp; right.
+    template(#html).
+      &lt;div class="orange-light4--bg pa8"&gt;
+        &lt;div class="green-light4--bg mxa pa3" style="max-width: 240px"&gt;
+          This green container has a `mxa` class which results in a margin auto on the left &amp; right.
         &lt;/div&gt;
       &lt;/div&gt;
 </template>
