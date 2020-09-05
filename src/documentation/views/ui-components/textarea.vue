68652ff90f13df099dd992dfb7d0ef1762c13a30
Antoni André <antoniandre.web@gmail.com>
2020-09-05 16:30:36 +1000

    Start the w-textarea component.

--- src/documentation/views/ui-components/textarea.vue
+++ src/documentation/views/ui-components/textarea.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
 div
-  title-link.mt4.code(h1) w-textarea
+  title-link.mt4.code(h1 slug="w-textarea")
+    .code w-textarea
+    w-tag.ml3(bg-color="orange" lg color="white") In Progress
 
+  title-link(h2)
+  example
+    w-textarea textarea
+    template(#pug).
+
+  title-link(h2)
+  example
+    w-textarea(no-autogrow rows="6") textarea
+    template(#pug).
 </template>
 
 <script>
--- src/wave-ui/components/w-textarea.vue
+++ src/wave-ui/components/w-textarea.vue
@@ -1,27 +1,374 @@
 <template lang="pug">
-  .w-textarea(:class="classes" :style="styles")
-    slot
+  component(
+    :is="formRegister ? 'w-form-element' : 'div'"
+    v-bind="formRegister && { validators, inputValue, disabled, readonly, isFocused }"
+    :valid.sync="valid"
+    @reset="$emit('input', inputValue = '')"
+    :class="classes")
+    //- Left label.
+    template(v-if="labelPosition === 'left'")
+      label.w-textarea__label.w-textarea__label--left.w-form-el-shakable(v-if="$slots.default" :for="`w-textarea--${_uid}`")
+        slot
+      label.w-textarea__label.w-textarea__label--left.w-form-el-shakable(v-else-if="label" :for="`w-textarea--${_uid}`" v-html="label")
+
+    //- Input wrapper.
+    .w-textarea__textarea-wrap(:class="inputWrapClasses")
+      w-icon.w-textarea__icon.w-textarea__icon--inner-left(
+        v-if="innerIconLeft"
+        tag="label"
+        :for="`w-textarea--${_uid}`"
+        @click="$emit('click:inner-icon-left')") {{ innerIconLeft }}
+      textarea.w-textarea__textarea(
+        ref="textarea"
+        v-model="inputValue"
+        v-on="listeners"
+        @input="onInput"
+        @focus="onFocus"
+        @blur="onBlur"
+        :id="`w-textarea--${_uid}`"
+        :name="inputName"
+        :placeholder="placeholder || null"
+        :rows="rows || null"
+        :cols="cols || null"
+        :readonly="readonly"
+        :disabled="disabled"
+        :required="required"
+        :style="textareaStyles")
+      template(v-if="labelPosition === 'inside' && showLabelInside")
+        label.w-textarea__label.w-textarea__label--inside.w-form-el-shakable(
+          v-if="$slots.default"
+          :for="`w-textarea--${_uid}`"
+          :class="isFocused && { [valid === false ? 'error' : this.color]: this.color || valid === false }")
+          slot
+        label.w-textarea__label.w-textarea__label--inside.w-form-el-shakable(
+          v-else-if="label"
+          :for="`w-textarea--${_uid}`"
+          v-html="label"
+          :class="isFocused && { [valid === false ? 'error' : this.color]: this.color || valid === false }")
+      w-icon.w-textarea__icon.w-textarea__icon--inner-right(
+        v-if="innerIconRight"
+        tag="label"
+        :for="`w-textarea--${_uid}`"
+        @click="$emit('click:inner-icon-right')") {{ innerIconRight }}
+
+      //- Right label.
+      template(v-if="labelPosition === 'right'")
+        label.w-textarea__label.w-textarea__label--right.w-form-el-shakable(v-if="$slots.default" :for="`w-textarea--${_uid}`")
+          slot
+        label.w-textarea__label.w-textarea__label--right.w-form-el-shakable(v-else-if="label" :for="`w-textarea--${_uid}`" v-html="label")
 </template>
 
 <script>
+import FormElementMixin from '../mixins/form-elements'
+
 export default {
   name: 'w-textarea',
+  mixins: [FormElementMixin],
   props: {
+    value: { default: '' },
+    label: { type: String },
+    labelPosition: { type: String, default: 'inside' },
+    innerIconLeft: { type: String },
+    innerIconRight: { type: String },
+    // When label is inside, allows to move the label above on focus or when filled.
+    moveLabel: { type: Boolean, default: true },
+    placeholder: { type: String },
+    color: { type: String, default: 'primary' },
+    bgColor: { type: String },
+    dark: { type: Boolean },
+    outline: { type: Boolean },
+    round: { type: Boolean },
+    shadow: { type: Boolean },
+    noAutogrow: { type: Boolean },
+    tile: { type: Boolean },
+    rows: { type: [String, Number], default: 3 },
+    cols: { type: [String, Number] },
+    // Also name, disabled, readonly, required and validators in the mixin.
+  },
+
+  data () {
+    return {
+      inputValue: this.value,
+      isFocused: false,
+      // When autogrow, calculate the height from the number of carriage return & font size.
+      height: null,
+      lineHeight: null,
+      fontSize: null
+    }
   },
 
   computed: {
+    listeners () {
+      // Remove the events that are fired separately, so they don't fire twice.
+      // eslint-disable-next-line no-unused-vars
+      const { input, focus, blur, ...listeners } = this.$listeners
+      return listeners
+    },
+    hasValue () {
+      return this.inputValue
+    },
+    hasLabel () {
+      return this.label || this.$slots.default
+    },
+    showLabelInside () {
+      return this.moveLabel || (!this.hasValue && !this.placeholder)
+    },
     classes () {
       return {
+        'w-textarea': true,
+        'w-textarea--disabled': this.disabled,
+        'w-textarea--filled': this.hasValue,
+        'w-textarea--focused': this.isFocused,
+        'w-textarea--dark': this.dark,
+        'w-textarea--floating-label': this.hasLabel && this.labelPosition === 'inside' && this.moveLabel && !(this.readonly && !this.hasValue),
+        'w-textarea--no-padding': !this.outline && !this.bgColor && !this.shadow && !this.round,
+        'w-textarea--has-placeholder': this.placeholder,
+        'w-textarea--inner-icon-left': this.innerIconLeft,
+        'w-textarea--inner-icon-right': this.innerIconRight
       }
     },
-    styles () {
-      return false
+    inputWrapClasses () {
+      return {
+        [this.valid === false ? 'error' : this.color]: this.color || this.valid === false,
+        [`${this.bgColor}--bg`]: this.bgColor,
+        'w-textarea__textarea-wrap--round': this.round,
+        'w-textarea__textarea-wrap--tile': this.tile,
+        // Box adds a padding on input. If there is a bgColor or shadow, a padding is needed.
+        'w-textarea__textarea-wrap--box': this.outline || this.bgColor || this.shadow,
+        'w-textarea__textarea-wrap--underline': !this.outline,
+        'w-textarea__textarea-wrap--shadow': this.shadow,
+        'w-textarea__textarea-wrap--no-padding': !this.outline && !this.bgColor && !this.shadow && !this.round
+      }
+    },
+
+    textareaStyles () {
+      return {
+        height: this.noAutogrow ? null : `${this.height}px`
+      }
+    }
+  },
+
+  methods: {
+    onInput (e) {
+      this.computeHeight()
+      this.$emit('input', this.inputValue)
+    },
+    onFocus (e) {
+      this.isFocused = true
+      this.$emit('focus', e)
+    },
+    onBlur (e) {
+      this.isFocused = false
+      this.$emit('blur', e)
+    },
+    computeHeight () {
+      const lines = (this.inputValue.match(/\n/g) || []).length + 1
+      this.height = Math.max(lines, this.rows) * this.lineHeight
+    },
+    getLineHeight () {
+      const computedStyles = window.getComputedStyle(this.$refs.textarea, null)
+      this.fontSize = parseFloat(computedStyles.getPropertyValue('fontSize'))
+      this.lineHeight = parseFloat(computedStyles.getPropertyValue('line-height'))
+    }
+  },
+
+  mounted () {
+    if (!this.noAutogrow) this.getLineHeight()
+  },
+
+  watch: {
+    value (value) {
+      this.inputValue = value
+      this.computeHeight()
+    },
+    noAutogrow (value) {
+      if (value) this.getLineHeight()
+      else this.height = null
     }
   }
 }
 </script>
 
 <style lang="scss">
+$size: round(2 * $base-font-size);
+$line-height: 1.2;
+$inactive-color: #777;
+
 .w-textarea {
+  position: relative;
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  align-items: center;
+  font-size: $base-font-size;
+
+  // textarea wrapper.
+  // ------------------------------------------------------
+  &__textarea-wrap {
+    position: relative;
+    display: inline-flex;
+    flex: 1 1 auto;
+    align-items: center;
+    min-height: $size;
+    border-radius: $border-radius;
+    border: $border;
+    transition: border $transition-duration;
+  }
+
+  &--floating-label &__textarea-wrap {
+    margin-top: 3 * $base-increment;
+  }
+
+  &__textarea-wrap--underline {
+    border-bottom-left-radius: initial;
+    border-bottom-right-radius: initial;
+    border-width: 0 0 1px;
+  }
+
+  // &__textarea-wrap--box {}
+
+  &__textarea-wrap--round {border-radius: 9em;}
+  &__textarea-wrap--tile {border-radius: initial;}
+  &__textarea-wrap--shadow {box-shadow: $box-shadow;}
+
+  &--focused &__textarea-wrap {border-color: currentColor;}
+
+  // Underline.
+  &__textarea-wrap--underline:after {
+    content: '';
+    position: absolute;
+    bottom: -1px;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border-bottom: 2px solid currentColor;
+    transition: $transition-duration;
+    transform: translateX(-50%);
+    pointer-events: none;
+  }
+
+  &--focused &__textarea-wrap--underline:after {width: 100%;}
+  &__textarea-wrap--round.w-textarea__textarea-wrap--underline:after {
+    border-radius: 9em;
+    transition: $transition-duration, height 0.035s;
+  }
+  &--focused &__textarea-wrap--round.w-textarea__textarea-wrap--underline:after {
+    height: 100%;
+    transition: $transition-duration, height 0s ($transition-duration - 0.035s);
+  }
+
+  // Textarea field.
+  // ------------------------------------------------------
+  &__textarea {
+    width: 100%;
+    height: 100%;
+    font-size: inherit;
+    line-height: $line-height;
+    color: inherit;
+    background: none;
+    border: none;
+    outline: none;
+    padding-left: 2 * $base-increment;
+    padding-right: 2 * $base-increment;
+    resize: vertical;
+
+    // For type="search" on Safari.
+    // -webkit-appearance: none;
+  }
+
+  &--no-padding &__textarea {
+    padding-left: 0;
+    padding-right: 0;
+  }
+
+  &__textarea-wrap--round &__textarea {
+    padding-left: 3 * $base-increment;
+    padding-right: 3 * $base-increment;
+  }
+
+  &--inner-icon-left &__textarea {padding-left: 27px;}
+  &--inner-icon-right &__textarea {padding-right: 27px;}
+
+  &--disabled &__textarea {
+    color: $disabled-color;
+    cursor: not-allowed;
+  }
+
+  &--disabled input::placeholder {color: inherit;}
+
+  // Icons inside.
+  // ------------------------------------------------------
+  &__icon {position: absolute;}
+  &__icon--inner-left {left: 6px;}
+  &__icon--inner-right {right: 6px;}
+  &--no-padding &__icon--inner-left {left: 1px;}
+  &--no-padding &__icon--inner-right {right: 1px;}
+
+  .w-textarea--focused &__icon {color: currentColor;}
+
+  &--disabled &__icon {
+    color: $disabled-color;
+    cursor: not-allowed;
+  }
+
+  // Label.
+  // ------------------------------------------------------
+  &__label {
+    transition: color $transition-duration;
+    cursor: pointer;
+
+    &--left {margin-right: 2 * $base-increment;}
+    &--right {margin-left: 2 * $base-increment;}
+    .w-textarea--disabled & {color: $disabled-color;cursor: not-allowed;}
+  }
+
+  &__label--inside {
+    position: absolute;
+    top: 50%;
+    left: 0;
+    padding-left: 2 * $base-increment;
+    transform: translateY(-50%);
+    pointer-events: none;
+
+    .w-textarea--no-padding & {
+      left: 0;
+      padding-left: 0;
+      padding-right: 0;
+    }
+    .w-textarea__textarea-wrap--round & {
+      padding-left: round(3 * $base-increment);
+      padding-right: round(3 * $base-increment);
+    }
+    .w-textarea--inner-icon-left & {left: 18px;}
+    .w-textarea--no-padding.w-textarea--inner-icon-left & {left: 26px;}
+
+    .w-textarea--floating-label & {
+      transform-origin: 0 0;
+      transition: $transition-duration ease;
+    }
+
+    // move label with underline style.
+    .w-textarea--focused.w-textarea--floating-label &,
+    .w-textarea--filled.w-textarea--floating-label &,
+    .w-textarea--has-placeholder.w-textarea--floating-label & {
+      transform: translateY(-160%) scale(0.85);
+    }
+    // Chrome & Safari - Must remain in a separated rule as Firefox discard the whole rule seeing -webkit-.
+    .w-textarea--floating-label .w-textarea__textarea:-webkit-autofill & {
+      transform: translateY(-160%) scale(0.85);
+    }
+    // Move label with outline style or with shadow.
+    .w-textarea--focused.w-textarea--floating-label .w-textarea__textarea-wrap--box &,
+    .w-textarea--filled.w-textarea--floating-label .w-textarea__textarea-wrap--box &,
+    .w-textarea--has-placeholder.w-textarea--floating-label .w-textarea__textarea-wrap--box & {
+      transform: translateY(-180%) scale(0.85);
+    }
+    .w-textarea--focused.w-textarea--floating-label.w-textarea--inner-icon-left &,
+    .w-textarea--filled.w-textarea--floating-label.w-textarea--inner-icon-left & {left: 0;}
+    // Chrome & Safari - Must remain in a separated rule as Firefox discard the whole rule seeing -webkit-.
+    .w-textarea--floating-label.w-textarea--inner-icon-left .w-textarea__textarea:-webkit-autofill & {left: 0;}
+
+    .w-textarea--focused & {color: currentColor;}
+  }
 }
 </style>
