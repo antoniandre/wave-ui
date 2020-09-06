69a473cf8c6d509fff4a19ea19c2618e3a7dc2d9
Antoni Andre <antoniandre.web@gmail.com>
2020-09-06 19:40:37 +1000

    Add 5 more components to be coming soon.

---
+++ src/wave-ui/components/w-breadcrumbs.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .w-breadcrumbs
+</template>
+
+<script>
+export default {
+  name: 'w-breadcrumbs',
+  props: {
+
+  },
+
+  data: () => ({
+
+  })
+}
+</script>
+
+<style lang="scss">
+.w-breadcrumbs {
+
+}
+</style>
