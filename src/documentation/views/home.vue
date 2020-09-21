65585e8e52e12c4bc85ff9009f4998580bc002ed
Antoni Andre <antoniandre.web@gmail.com>
2020-09-21 22:51:51 +1000

    Add GreenSock animation on home page.

--- src/documentation/views/home.vue
+++ src/documentation/views/home.vue
@@ -42,92 +42,95 @@ w-app.home
       path.path1.bottom(fill="#1a6fb4" filter="url(#filter1)" d="M 1 155 C 554 521 1126 -37 1440 321 V 400 H 0 Z")
 
     .section__content
-      w-flex.title2
-        w-icon.mr2 wi-check
-        | All the components that you need in a lightweight package.
-      p.my4.
-        More than 30 UI &amp; form elements are available as well as a form validation.#[br]
-        This form uses 9 different components and a form validation.
-      w-card.white--bg(content-class="pa0")
-        .message-box
-          w-transition-fade
-            w-alert.my0.text-light(v-if="form6.submitted" success no-border)
-              | The form is valid, ready to send it!
-            w-alert.my0.text-light(v-else-if="form6.valid === false" error no-border)
-              | The form has {{ form6.errorsCount }} errors.
-        w-form.px8.pt2.pb10.grey(
-          v-model="form6.valid"
-          :errors-count.sync="form6.errorsCount"
-          @validate="onValidate"
-          @success="onSuccess")
-          w-input(required label="First name" :validators="[validators.required]")
-          w-input.mt3(required label="Last name" :validators="[validators.required]")
+      .block.block--1
+        w-flex.title2
+          w-icon.mr2 wi-check
+          | All the components that you need in a lightweight package.
+        p.my4.
+          More than 30 UI &amp; form elements are available as well as a form validation.#[br]
+          This form uses 9 different components and a form validation.
+        w-card.white--bg(content-class="pa0")
+          .message-box
+            w-transition-fade
+              w-alert.my0.text-light(v-if="form6.submitted" success no-border)
+                | The form is valid, ready to send it!
+              w-alert.my0.text-light(v-else-if="form6.valid === false" error no-border)
+                | The form has {{ form6.errorsCount }} errors.
+          w-form.px8.pt2.pb10.grey(
+            v-model="form6.valid"
+            :errors-count.sync="form6.errorsCount"
+            @validate="onValidate"
+            @success="onSuccess")
+            w-input(required label="First name" :validators="[validators.required]")
+            w-input.mt3(required label="Last name" :validators="[validators.required]")
 
-          w-flex.mt4(wrap align-center justify-end)
-            w-checkbox(required :validators="[validators.consent]") I agree to the terms &amp; conditions
-            .spacer
-            div
-              w-button.mr2(
-                bg-color="warning"
-                type="reset"
-                @click="form6.submitted = form6.sent = false") Reset
-              w-button(
-                type="submit"
-                :disabled="form6.valid === false"
-                :loading="form6.submitted && !form6.sent") Validate
-        w-notification(
-          v-model="form6.sent"
-          success
-          transition="bounce"
-          absolute
-          plain
-          round
-          bottom) The form was sent successfully!
+            w-flex.mt4(wrap align-center justify-end)
+              w-checkbox(required :validators="[validators.consent]") I agree to the terms &amp; conditions
+              .spacer
+              div
+                w-button.mr2(
+                  bg-color="warning"
+                  type="reset"
+                  @click="form6.submitted = form6.sent = false") Reset
+                w-button(
+                  type="submit"
+                  :disabled="form6.valid === false"
+                  :loading="form6.submitted && !form6.sent") Validate
+          w-notification(
+            v-model="form6.sent"
+            success
+            transition="bounce"
+            absolute
+            plain
+            round
+            bottom) The form was sent successfully!
 
-      w-flex.title2.mt12
-        w-icon.mr2 wi-check
-        | Build great UIs with flexible &amp; powerful elements.
-      p.my4 See for yourself with this checklist component for instance.
-      .w-flex.example.pa1.basis-zero.align-center
-        .grow(style="min-width: 200px")
-          ssh-pre(language="html-vue" dark).
-            &lt;w-list
-              v-model="selection"
-              :items="items"
-              checklist
-              round-checkboxes
-              color="blue-light1"&gt;
-            &lt;/w-list&gt;
-          ssh-pre(language="js" dark).
-            data: () => ({
-              items: [
-                { label: 'Item 1' },
-                { label: 'Item 2' },
-                { label: 'Item 3' }
-              ],
-              selection: ['Item 2']
-            })
-        w-list.ml2.grow(
-          v-model="selectedItem"
-          :items="demoListItems"
-          checklist
-          round-checkboxes
-          color="blue-light1"
-          style="min-width: 150px")
+      .block.block--2
+        w-flex.title2.mt12
+          w-icon.mr2 wi-check
+          | Build great UIs with flexible &amp; powerful elements.
+        p.my4 See for yourself with this checklist component for instance.
+        .w-flex.example.pa1.basis-zero.align-center
+          .grow(style="min-width: 200px")
+            ssh-pre(language="html-vue" dark).
+              &lt;w-list
+                v-model="selection"
+                :items="items"
+                checklist
+                round-checkboxes
+                color="blue-light1"&gt;
+              &lt;/w-list&gt;
+            ssh-pre(language="js" dark).
+              data: () => ({
+                items: [
+                  { label: 'Item 1' },
+                  { label: 'Item 2' },
+                  { label: 'Item 3' }
+                ],
+                selection: ['Item 2']
+              })
+          w-list.ml2.grow(
+            v-model="selectedItem"
+            :items="demoListItems"
+            checklist
+            round-checkboxes
+            color="blue-light1"
+            style="min-width: 150px")
 
-      w-flex.title2.mt12
-        w-icon.mr2 wi-check
-        | Keep full control on the CSS.
-      p.my4.
-        Wave UI is developed with a particular care about CSS.#[br]
-        Practically Wave UI gives you more flexibility and more control over the CSS.
-      p.my4.
-        If you use SASS, you can even control Wave UI's CSS rules priorities by overriding the CSS base scope like so.
-      pre.example.ssh-pre.pa2(dark)
-        span.pink $css-scope
-        span.blue-grey.mr1 :
-        span.red-light2 '.w-app'
-        span.blue-grey ;
+      .block.block--3
+        w-flex.title2.mt12
+          w-icon.mr2 wi-check
+          | Keep full control on the CSS.
+        p.my4.
+          Wave UI is developed with a particular care about CSS.#[br]
+          Practically Wave UI gives you more flexibility and more control over the CSS.
+        p.my4.
+          If you use SASS, you can even control Wave UI's CSS rules priorities by overriding the CSS base scope like so.
+        pre.example.ssh-pre.pa2(dark)
+          span.pink $css-scope
+          span.blue-grey.mr1 :
+          span.red-light2 '.w-app'
+          span.blue-grey ;
 
   section.section.section--2
     svg(viewBox="0 0 1440 90" preserveAspectRatio="none")
@@ -136,14 +139,14 @@ w-app.home
     .section__content
       .w-flex.basis-zero.text-center.mb10
         .feature.grow
-          svg(viewBox="6 0 194 194" preserveAspectRatio="none")
+          svg.blob(viewBox="6 0 194 194" preserveAspectRatio="none")
             path(fill="#61829d" d="M 57 -32 C 70 -10 74 18 63 35 C 52 51 26 58 1 57 C -24 56 -48 49 -58 33 C -67 17 -62 -8 -50 -29 C -38 -51 -19 -69 1 -70 C 22 -70 44 -54 57 -32 Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 432.4 432.4")
             path(d="M217 93a111 111 0 00-74 195c18 18 16 55 16 56 0 2 0 3 2 5l4 2h102l5-2 2-5c0-1-2-38 16-56l1-1a111 111 0 00-74-194zm64 185l-2 1c-15 17-18 45-18 58h-89c0-13-3-42-20-59a97 97 0 11129 0z")
             path(d="M216 122c-3 0-7 3-7 6 0 4 4 7 7 7 41 0 73 33 73 73 0 4 3 7 7 7 3 0 7-3 7-7 0-48-39-86-87-86zM261 358h-89c-9 0-17 8-17 17s8 17 17 17h88a17 17 0 000-34zm0 20h-89c-2 0-3-1-3-3s1-3 3-3h88c2 0 3 1 3 3s-1 3-2 3zM247 399h-62c-9 0-17 7-17 17s8 16 17 16h62c10 0 17-7 17-16 0-10-7-17-17-17zm0 19h-62c-2 0-3-1-3-3s1-3 3-3h62c2 0 4 1 4 3s-2 3-4 3zM216 60c4 0 7-3 7-7V7c0-4-3-7-7-7-3 0-7 3-7 7v46c0 4 4 7 7 7zM329 34c-3-2-7-1-9 2l-25 38c-3 4-2 8 1 10l4 1c2 0 4-1 5-3l26-38c2-3 2-8-2-10zM135 84l4-2c3-2 4-6 2-9l-25-39c-2-3-7-4-10-2s-4 6-2 10l25 38c2 3 4 4 6 4zM87 126l-41-22c-3-2-7 0-9 3s-1 7 3 9l40 22 3 1c3 0 5-1 6-4 2-3 1-7-2-9zM396 107c-2-3-6-5-9-3l-41 22c-3 2-5 6-3 9 1 3 4 4 6 4l3-1 41-22c3-2 4-6 3-9z")
           p Modern design
         .feature.grow
-          svg(viewBox="0 0 200 200" preserveAspectRatio="none")
+          svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#135182" d="M45.8,-34C59.2,-19.7,69.8,-0.7,67.7,18.2C65.6,37.1,50.7,55.8,31.3,65.4C11.8,74.9,-12.3,75.3,-30.4,65.6C-48.4,55.9,-60.4,36.2,-67,13.5C-73.6,-9.2,-74.9,-34.9,-62.8,-48.9C-50.7,-62.8,-25.4,-65,-4.6,-61.3C16.2,-57.6,32.4,-48.2,45.8,-34Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 512 512")
             path(d="M90 396c-7 0-14 2-20 5A236 236 0 01303 25a10 10 0 004-20A258 258 0 0075 75a254 254 0 00-20 340 42 42 0 005 53 42 42 0 0060 0 42 42 0 000-60c-8-8-19-12-30-12zm16 58a22 22 0 01-32 0 22 22 0 010-32 22 22 0 0132 0 22 22 0 010 32zm0 0M457 97a42 42 0 10-15 14 236 236 0 01-233 376 10 10 0 00-3 20 258 258 0 00231-70 254 254 0 0020-340zm-51-7a22 22 0 010-32 22 22 0 0132 0 22 22 0 01-32 32zm0 0")
@@ -151,7 +154,7 @@ w-app.home
             path(d="M339 256a83 83 0 10-166 0 83 83 0 00166 0zm-146 0a63 63 0 11126 0 63 63 0 01-126 0zm0 0M348 38a10 10 0 0010-10c0-3-1-5-3-7a10 10 0 00-14 0 10 10 0 000 14c2 2 4 3 7 3zm0 0M164 474a10 10 0 00-10 10 10 10 0 0010 10 10 10 0 0010-10 10 10 0 00-10-10zm0 0")
           p Highly customizable
         .feature.grow
-          svg(viewBox="0 0 200 200" preserveAspectRatio="none")
+          svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#3877a9" d="M54.1,-52.3C67.8,-40.3,75.2,-20.1,73.4,-1.8C71.6,16.5,60.6,33,46.8,48.6C33,64.2,16.5,78.8,0.6,78.2C-15.2,77.6,-30.5,61.6,-41.2,46.1C-51.8,30.5,-58,15.2,-60.2,-2.2C-62.4,-19.6,-60.6,-39.3,-50,-51.2C-39.3,-63.2,-19.6,-67.5,0.3,-67.8C20.1,-68,40.3,-64.2,54.1,-52.3Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 512 512")
             circle(cx="279.3" cy="349.1" r="11.6")
@@ -160,7 +163,7 @@ w-app.home
             path(d="M454 303H198c-7 0-12 4-12 11s5 12 12 12h256c7 0 11-5 11-12s-4-11-11-11zM128 233H35c-20 0-35 15-35 35v163c0 19 15 34 35 34h93c20 0 35-15 35-34V268c0-20-15-35-35-35zm12 198c0 7-5 11-12 11H35c-7 0-12-4-12-11V268c0-7 5-12 12-12h93c7 0 12 5 12 12v163z")
           p Fully responsive
         .feature.grow
-          svg(viewBox="0 0 200 200" preserveAspectRatio="none")
+          svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#7db0da" d="M60.4,-48.9C73.3,-32.1,75.2,-7.3,67.4,10.9C59.7,29,42.3,40.3,23,50.8C3.7,61.3,-17.6,71,-32.9,64.9C-48.3,58.9,-57.7,37.1,-62.2,14.9C-66.8,-7.3,-66.3,-29.9,-55.1,-46.3C-43.9,-62.7,-22,-72.9,0.9,-73.6C23.8,-74.4,47.6,-65.7,60.4,-48.9Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 461.8 461.8")
             path(d="M362 219h-1c-8 1-16 4-23 9-4-15-18-26-34-26-8 0-16 3-22 8-3-17-18-30-35-31-8 0-15 3-21 7v-19a88 88 0 10-78 0v84l-19-24a38 38 0 00-53-8 38 38 0 00-5 53l71 139c22 43 57 49 72 50l37 1c39 0 72-2 72-2h1c58 0 74-62 75-96V258c0-20-16-38-37-39zM147 110v38a72 72 0 1178 0v-38a39 39 0 00-78 0zm175 334s-57 3-108 1c-11 0-40-5-58-41L85 264v-1c-8-9-8-23 2-31 9-7 23-5 30 5l32 44 3 2 1 1h5l1-1h1v-1l2-1v-1l1-1V110a23 23 0 1146 0v141a8 8 0 0016 0v-33a21 21 0 1141 0v58a8 8 0 0016 0v-35a21 21 0 1142 0v64a8 8 0 0016 0v-47a21 21 0 1141 0l1 106c0 4-2 80-60 80z")
@@ -203,6 +206,9 @@ w-app.home
 </template>
 
 <script>
+import { gsap, TimelineMax, Power4 } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(TimelineMax, Power4, ScrollTrigger)
 
 export default {
   data: () => ({
@@ -234,7 +240,35 @@ export default {
     }
   },
 
+  mounted () {
+    setTimeout(this.initScrollAnimation, 500)
+  },
+
   methods: {
+    initScrollAnimation () {
+      gsap.defaults({ ease: 'power4' })
+
+      gsap.set('.block', { y: 30, opacity: 0 })
+      ScrollTrigger.batch('.block', {
+        start: 'top 88%',
+        // markers: true,
+        onEnter: batch => gsap.to(batch, { opacity: 1, y: -30, duration: 1, stagger: { each: 0.15 }, overwrite: true }),
+        onLeave: batch => gsap.to(batch, { opacity: 0, y: 30, duration: 1, stagger: { each: 0.15 }, overwrite: true }),
+        onEnterBack: batch => gsap.to(batch, { opacity: 1, y: -30, duration: 1, stagger: { each: 0.15 }, overwrite: true }),
+        onLeaveBack: batch => gsap.to(batch, { opacity: 0, y: 30, duration: 1, stagger: { each: 0.15 }, overwrite: true })
+      })
+
+      gsap.set('.feature', { y: 60 })
+      ScrollTrigger.batch('.feature', {
+        start: 'top 88%',
+        onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: { each: 0.15 }, overwrite: true }),
+        onLeave: batch => gsap.set(batch, { opacity: 0, y: -60, stagger: { each: 0.15 }, overwrite: true }),
+        onEnterBack: batch => gsap.to(batch, { opacity: 1, y: -60, stagger: { each: 0.15 }, overwrite: true }),
+        onLeaveBack: batch => gsap.to(batch, { opacity: 0, y: 60, stagger: { each: 0.15 }, overwrite: true })
+      })
+
+      ScrollTrigger.addEventListener('refreshInit', () => gsap.set('.feature', { y: 0 }))
+    },
     onSuccess () {
       setTimeout(() => (this.form6.sent = true), 2000)
     },
@@ -301,6 +335,8 @@ export default {
     }
   }
 
+  // Section 1.
+  // ------------------------------------------------------
   .section--1 {
     background-image: linear-gradient(#1a6fb4, #09304f);
     color: #fff;
@@ -310,6 +346,20 @@ export default {
     .w-list__item-bullet {color: inherit;}
   }
 
+  // Appear transition.
+  .section--1 .block {
+    opacity: 0;
+    // transform: translateX(16px);
+    // transition: 0.5s ease-in-out;
+
+    // &.active {
+    //   opacity: 1;
+    //   transform: translateX(0);
+    // }
+  }
+
+  // Section 2.
+  // ------------------------------------------------------
   .section--2 {
     background-color: #fff;
     color: #666;
@@ -333,6 +383,9 @@ export default {
       position: relative;
       height: 0;
       padding-bottom: 25%;
+      opacity: 0;
+
+      // .blob {animation: jelly 1s linear both;}
 
       p {
         position: absolute;
@@ -357,6 +410,8 @@ export default {
     .ready-to-dive {margin-top: 6em;}
   }
 
+  // Section 3.
+  // ------------------------------------------------------
   .section--3 {
     color: #666;
     padding: 0;
@@ -378,6 +433,35 @@ export default {
   }
 
   .message-box {min-height: 35px;}
+}
+
+@keyframes jelly {
+  0% { -webkit-transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  3.4% { -webkit-transform: matrix3d(1.316, 0, 0, 0, 0, 1.407, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.316, 0, 0, 0, 0, 1.407, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  4.7% { -webkit-transform: matrix3d(1.45, 0, 0, 0, 0, 1.599, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.45, 0, 0, 0, 0, 1.599, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  6.81% { -webkit-transform: matrix3d(1.659, 0, 0, 0, 0, 1.893, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.659, 0, 0, 0, 0, 1.893, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  9.41% { -webkit-transform: matrix3d(1.883, 0, 0, 0, 0, 2.168, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.883, 0, 0, 0, 0, 2.168, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  10.21% { -webkit-transform: matrix3d(1.942, 0, 0, 0, 0, 2.226, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.942, 0, 0, 0, 0, 2.226, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  13.61% { -webkit-transform: matrix3d(2.123, 0, 0, 0, 0, 2.332, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.123, 0, 0, 0, 0, 2.332, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  14.11% { -webkit-transform: matrix3d(2.141, 0, 0, 0, 0, 2.331, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.141, 0, 0, 0, 0, 2.331, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  17.52% { -webkit-transform: matrix3d(2.208, 0, 0, 0, 0, 2.239, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.208, 0, 0, 0, 0, 2.239, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  18.72% { -webkit-transform: matrix3d(2.212, 0, 0, 0, 0, 2.187, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.212, 0, 0, 0, 0, 2.187, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  21.32% { -webkit-transform: matrix3d(2.196, 0, 0, 0, 0, 2.069, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.196, 0, 0, 0, 0, 2.069, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  24.32% { -webkit-transform: matrix3d(2.151, 0, 0, 0, 0, 1.96, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.151, 0, 0, 0, 0, 1.96, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  25.23% { -webkit-transform: matrix3d(2.134, 0, 0, 0, 0, 1.938, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.134, 0, 0, 0, 0, 1.938, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  29.03% { -webkit-transform: matrix3d(2.063, 0, 0, 0, 0, 1.897, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.063, 0, 0, 0, 0, 1.897, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  29.93% { -webkit-transform: matrix3d(2.048, 0, 0, 0, 0, 1.899, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.048, 0, 0, 0, 0, 1.899, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  35.54% { -webkit-transform: matrix3d(1.979, 0, 0, 0, 0, 1.962, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.979, 0, 0, 0, 0, 1.962, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  36.74% { -webkit-transform: matrix3d(1.972, 0, 0, 0, 0, 1.979, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.972, 0, 0, 0, 0, 1.979, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  41.04% { -webkit-transform: matrix3d(1.961, 0, 0, 0, 0, 2.022, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.961, 0, 0, 0, 0, 2.022, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  44.44% { -webkit-transform: matrix3d(1.966, 0, 0, 0, 0, 2.032, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.966, 0, 0, 0, 0, 2.032, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  52.15% { -webkit-transform: matrix3d(1.991, 0, 0, 0, 0, 2.006, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.991, 0, 0, 0, 0, 2.006, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  59.86% { -webkit-transform: matrix3d(2.006, 0, 0, 0, 0, 1.99, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.006, 0, 0, 0, 0, 1.99, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  63.26% { -webkit-transform: matrix3d(2.007, 0, 0, 0, 0, 1.992, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.007, 0, 0, 0, 0, 1.992, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  75.28% { -webkit-transform: matrix3d(2.001, 0, 0, 0, 0, 2.003, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2.001, 0, 0, 0, 0, 2.003, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  85.49% { -webkit-transform: matrix3d(1.999, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.999, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  90.69% { -webkit-transform: matrix3d(1.999, 0, 0, 0, 0, 1.999, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(1.999, 0, 0, 0, 0, 1.999, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
+  100% { -webkit-transform: matrix3d(2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); transform: matrix3d(2, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); }
 }
 
 @media screen and (max-width: 600px) {
