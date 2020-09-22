4a07df51f2ac58328c378826f94ce68392fcdb02
Antoni Andre <antoniandre.web@gmail.com>
2020-09-22 17:04:05 +1000

    Update w-radio & w-checkbox documentations.

--- src/documentation/views/ui-components/radio.vue
+++ src/documentation/views/ui-components/radio.vue
@@ -5,7 +5,7 @@ div
 
   w-card(bg-color="blue-light5")
     p.mb3 What it looks like:
-    w-radios(v-model="selectedRadio1" :items="radios1" inline)
+    w-radios(v-model="selection1" :items="radios1" inline)
 
     .title4.mt4 States
     .w-flex.wrap
@@ -25,12 +25,21 @@ div
   .w-divider
 
   alert(tip)
-    strong Important
-    div.
-      With single radio buttons (using &lt;w-radio&gt;), the #[code name] prop is needed for the
-      radios to work together. This is how HTML knows they are part of the same group of choices.#[br]
-      But when you use a group of radio buttons via &lt;w-radios&gt;, Wave UI will add the name
-      attributes for you.
+    .title3 Important notes
+    strong.title5 Name attribute
+    p.mb4.
+      With individual radio buttons (using #[span.code &lt;w-radio&gt;]), the #[code name] prop is
+      needed for the radios to work together. This is how HTML knows they are part of the same group
+      of choices.#[br]
+      But when you use a group of radio buttons via #[span.code &lt;w-radios&gt;], Wave UI will add the
+      name attributes for you.
+    strong.title5 Form validation
+    p.
+      When a validation is needed on radio buttons, each individual radio button (#[span.code w-radio])
+      has a separate validation message.#[br]
+      If you want only one validation for a group of radio buttons, you must use the #[span.code w-radios]
+      component.
+
   example
     .title4.mb2 1. Here is a single radio button
     w-radio I am single - not very useful
@@ -81,7 +90,7 @@ div
       have to worry about it if you don't need it.
 
   title-link(h2) Custom label in w-radio
-  p #[code w-radio] provides a default slot to customize the content.
+  p #[span.code w-radio] provides a default slot to customize the content.
   example(content-class="mt3")
     w-radio.mr5(name="radio4")
       span.pr1 Choice 1 has an icon
@@ -104,38 +113,13 @@ div
     But because in most cases you won't need that, the #[span.code w-radios] component will make you go
     much faster.
 
-  title-link(h2) Color
-  p By default the #[code w-radio] &amp; #[code w-radios] components will use the primary color.
-  alert(info).
-    One thing that the #[span.code w-radios] component does not do is mixing colors: you can do it with
-    #[span.code w-radio] like in this example.
-  example(content-class="mt3")
-    w-radio.mr2(v-model="selectedRadio5" name="radio5" color="teal-light1" :return-value="1") Option 1
-    w-radio(v-model="selectedRadio5" name="radio5" color="teal-light1" :return-value="2") Option 2
-
-    .title4.mt8.mb2 Mixing colors
-    w-radio.mr2(v-model="selectedRadio6" name="radio6" color="pink-light3" :return-value="1") Option 1
-    w-radio(v-model="selectedRadio6" name="radio6" color="blue-light1" :return-value="2") Option 2
-    template(#pug).
-      w-radio.mr2(v-model="selectedRadio1" name="radio1" color="teal-light1" :return-value="1") Option 1
-      w-radio(v-model="selectedRadio1" name="radio1" color="teal-light1" :return-value="2") Option 2
-
-      .title4.mt8.mb2 Mixing colors
-      w-radio.mr2(v-model="selectedRadio2" name="radio2" color="pink-light3" :return-value="1") Option 1
-      w-radio(v-model="selectedRadio2" name="radio2" color="blue-light1" :return-value="2") Option 2
-    template(#js).
-      data: () => ({
-        selectedRadio1: 1,
-        selectedRadio2: 1
-      })
-
   //- W-radios.
   //-------------------------------------------------------
   title-link.title1.mt12(h2 slug="w-radios") &lt;w-radios&gt;
   .w-divider
 
   p.my5.
-    The #[code w-radios] component allows a fast and easy rendering of
+    The #[span.code w-radios] component allows a fast and easy rendering of
     multiple radio buttons by passing the items through a prop.#[br]
     It accepts an inline parameter to display the radio buttons inline,
     and allows you to customize the label of each radio button through slot.
@@ -147,15 +131,15 @@ div
   p The v-model can also be preset, or updated externally.
   example
     .w-flex.align-center
-      w-radios(v-model="selectedRadio1" :items="radios1")
-      div.title3.ml10 v-model: #[code {{ selectedRadio1 || 'null' }}]
+      w-radios(v-model="selection1" :items="radios1")
+      div.title3.ml10 v-model: #[code {{ selection1 || 'null' }}]
     template(#pug).
       .w-flex.align-center
-        w-radios(v-model="selectedRadio" :items="radioItems")
-        div.title3.ml10 v-model: {{ "\{\{ selectedRadio || 'null' \}\}" }}
+        w-radios(v-model="selection" :items="radioItems")
+        div.title3.ml10 v-model: {{ "\{\{ selection || 'null' \}\}" }}
     template(#js).
       data: () => ({
-        selectedRadio: 1,
+        selection: 1,
         radioItems: [
           { label: 'Choice 1', value: 1 },
           { label: 'Choice 2', value: 2 },
@@ -173,46 +157,105 @@ div
         radioItems: [
           { label: 'Choice 1' },
           { label: 'Choice 2' },
-          { label: 'Choice 3' },
-          { label: 'Choice 4' }
+          { label: 'Choice 3' }
         ]
       })
 
   title-link(h2) Custom label content
-  p #[code w-radios] also provides a slot to customize the label of each radio button: #[code item].
+  p #[span.code w-radios] also provides a slot to customize the label of each radio button: #[code item].
   p The great thing is that you can access any custom data from this slot!
   example
-    w-radios(v-model="selectedRadio7" :items="radios3" color="green")
+    w-radios(v-model="selection5" :items="radios3" color="green")
       template(#item="{ item }")
         span.pr2 {{ item.label }}
         w-icon.success {{ item.icon }}
     template(#pug).
-      w-radios(v-model="selectedRadio" :items="radioItems" color="green")
+      w-radios(v-model="selection" :items="radioItems" color="green")
         template(#item="{ item }")
           span.pr2 {{ '\{\{ item.label \}\}' }}
           w-icon.success {{ '\{\{ item.icon \}\}' }}
     template(#js).
       data: () => ({
-        selectedRadio: 'Square',
+        selection: 'Square',
         radioItems: [
           { label: 'Square', icon: 'mdi mdi-square' },
           { label: 'Circle', icon: 'mdi mdi-circle' },
           { label: 'Triangle', icon: 'mdi mdi-triangle' }
         ],
       })
+
+  .w-divider.grey-light5.my12
+
+  //- Other examples.
+  //-------------------------------------------------------
+  title-link(h2) Color
+  p.
+    By default the #[span.code w-radio] &amp; #[span.code w-radios] components will use the
+    primary color.#[br]
+    You can provide a different color with the #[code color] option, or you can even set a different
+    color per item by providing a #[code color] attribute in each of the item objects.
+
+  title-link(h3 slug="w-radios--colors") #[span.code w-radios] colors
+  example
+    w-radios.mr2(v-model="selection6" :items="radios1" color="purple") Option 1
+
+    .title4.mt8.mb2 Different color per item
+    w-radios(v-model="selection7" :items="radios4") Option 2
+    template(#pug).
+      w-radios.mr2(v-model="selection1" :items="radios1" color="purple") Option 1
+
+      .title4.mt8.mb2 Different color per item
+      w-radios(v-model="selection2" :items="radios2") Option 2
+    template(#js).
+      data: () => ({
+        selection1: 1,
+        radios1: [
+          { label: 'Choice 1', value: 1 },
+          { label: 'Choice 2', value: 2 },
+          { label: 'Choice 3', value: 3 }
+        ],
+        selection2: 1,
+        radios2: [
+          { label: 'Choice 1', value: 1, color: 'amber' },
+          { label: 'Choice 2', value: 2, color: 'warning' },
+          { label: 'Choice 3', value: 3, color: 'error' }
+        ]
+      })
+
+  title-link(h3 slug="w-radio--colors") #[span.code w-radio] colors
+  example
+    w-radio.mr2(v-model="selection3" name="radio5" color="teal-light1" :return-value="1") Option 1
+    w-radio(v-model="selection3" name="radio5" color="teal-light1" :return-value="2") Option 2
+
+    .title4.mt8.mb2 Different color per item
+    w-radio.mr2(v-model="selection4" name="radio6" color="pink-light3" :return-value="1") Option 1
+    w-radio(v-model="selection4" name="radio6" color="blue-light1" :return-value="2") Option 2
+    template(#pug).
+      w-radio.mr2(v-model="selection1" name="radio1" color="teal-light1" :return-value="1") Option 1
+      w-radio(v-model="selection1" name="radio1" color="teal-light1" :return-value="2") Option 2
+
+      .title4.mt8.mb2 Different color per item
+      w-radio.mr2(v-model="selection2" name="radio2" color="pink-light3" :return-value="1") Option 1
+      w-radio(v-model="selection2" name="radio2" color="blue-light1" :return-value="2") Option 2
+    template(#js).
+      data: () => ({
+        selection1: 1,
+        selection2: 1
+      })
 </template>
 
 <script>
 export default {
   data: () => ({
-    selectedRadio1: 1,
-    selectedRadio3: 1,
-    selectedRadio5: 1,
-    selectedRadio6: 1,
-    selectedRadio7: 'Square',
+    selection1: 1,
+    selection2: 1,
+    selection3: 1,
+    selection4: 1,
+    selection5: 'Square',
+    selection6: 1,
+    selection7: 1,
     radio2: false,
     radio3: false,
-    radio4: 'Choice 1',
     radios1: [
       { label: 'Choice 1', value: 1 },
       { label: 'Choice 2', value: 2 },
@@ -221,13 +264,17 @@ export default {
     radios2: [
       { label: 'Choice 1' },
       { label: 'Choice 2' },
-      { label: 'Choice 3' },
-      { label: 'Choice 4' }
+      { label: 'Choice 3' }
     ],
     radios3: [
       { label: 'Square', icon: 'mdi mdi-square' },
       { label: 'Circle', icon: 'mdi mdi-circle' },
       { label: 'Triangle', icon: 'mdi mdi-triangle' }
+    ],
+    radios4: [
+      { label: 'Choice 1', value: 1, color: 'amber' },
+      { label: 'Choice 2', value: 2, color: 'warning' },
+      { label: 'Choice 3', value: 3, color: 'error' }
     ]
   })
 }
