6a507f3d408f099436bd197ae632bfdb60817b3a
Antoni Andre <antoniandre.web@gmail.com>
2020-09-24 00:29:16 +1000

    Improve Home page layout.

--- src/documentation/views/home.vue
+++ src/documentation/views/home.vue
@@ -7,12 +7,8 @@ w-app.home
         | Wave UI
       em A Vue.js UI framework with only the bright side.
     .buttons
-      w-button(outline sm round to="/why-wave-ui" color="white")
-        w-icon.ml-1 wi-chevron-right
-        | Why Wave UI
-      w-button.ml2(outline sm round to="/getting-started" color="white")
-        w-icon.ml-1 wi-chevron-right
-        | Get started
+      w-button(outline sm round to="/why-wave-ui" color="white") Why Wave UI
+      w-button.ml2(outline sm round to="/getting-started" color="white") Get started
 
     .version
       w-tooltip(z-index="20")
@@ -30,12 +26,11 @@ w-app.home
       path(d="M 0 32 C 240 53 480 75 720 80 C 960 85 1200 75 1440 60 V 100 H 0 Z")
 
   section.section.section--1
-
     .section__content
       .block.block--1
         w-flex.title2
           w-icon.mr2 wi-check
-          | All the components that you need in a lightweight package.
+          | All the components that you need in a lightweight package
         p.my4.
           More than 30 UI &amp; form elements are available as well as a form validation.#[br]
           This form uses 9 different components and a form validation.
@@ -55,7 +50,7 @@ w-app.home
             w-input.mt3(required label="Last name" :validators="[validators.required]")
 
             w-flex.mt4(wrap align-center justify-end)
-              w-checkbox(required :validators="[validators.consent]") I agree to the terms &amp; conditions
+              w-checkbox.my4(required :validators="[validators.consent]") I agree to the terms &amp; conditions
               .spacer
               div
                 w-button.mr2(
@@ -78,9 +73,9 @@ w-app.home
       .block.block--2
         w-flex.title2
           w-icon.mr2 wi-check
-          | Build great UIs with flexible &amp; powerful elements.
+          | Build great UIs with flexible &amp; powerful elements
         p.my4 See for yourself with this checklist component for instance.
-        .w-flex.example.pa1.basis-zero.align-center
+        .w-flex.example.pa1.basis-zero.align-center.wrap
           .grow(style="min-width: 200px")
             ssh-pre(language="html-vue" dark).
               &lt;w-list
@@ -99,7 +94,7 @@ w-app.home
                 ],
                 selection: ['Item 2']
               })
-          w-list.ml2.grow(
+          w-list.ml2.my2.grow(
             v-model="selectedItem"
             :items="demoListItems"
             checklist
@@ -110,7 +105,7 @@ w-app.home
       .block.block--3
         w-flex.title2
           w-icon.mr2 wi-check
-          | Keep full control on the CSS.
+          | Keep full control on the CSS
         p.my4.
           Wave UI is developed with a particular care about CSS.#[br]
           Practically Wave UI gives you more flexibility and more control over the CSS.
@@ -127,15 +122,15 @@ w-app.home
       path(d="M 0 0 H 1440 V 81 C 1250 61 552 0 0 67 Z")
 
     .section__content
-      .w-flex.basis-zero.text-center.mb10
-        .feature.grow
+      .w-flex.wrap.text-center.mb10
+        .feature.xs6.sm3
           svg.blob(viewBox="6 0 194 194" preserveAspectRatio="none")
             path(fill="rgb(255, 255, 255, 0.15)" d="M 57 -32 C 70 -10 74 18 63 35 C 52 51 26 58 1 57 C -24 56 -48 49 -58 33 C -67 17 -62 -8 -50 -29 C -38 -51 -19 -69 1 -70 C 22 -70 44 -54 57 -32 Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 432.4 432.4")
             path(d="M217 93a111 111 0 00-74 195c18 18 16 55 16 56 0 2 0 3 2 5l4 2h102l5-2 2-5c0-1-2-38 16-56l1-1a111 111 0 00-74-194zm64 185l-2 1c-15 17-18 45-18 58h-89c0-13-3-42-20-59a97 97 0 11129 0z")
             path(d="M216 122c-3 0-7 3-7 6 0 4 4 7 7 7 41 0 73 33 73 73 0 4 3 7 7 7 3 0 7-3 7-7 0-48-39-86-87-86zM261 358h-89c-9 0-17 8-17 17s8 17 17 17h88a17 17 0 000-34zm0 20h-89c-2 0-3-1-3-3s1-3 3-3h88c2 0 3 1 3 3s-1 3-2 3zM247 399h-62c-9 0-17 7-17 17s8 16 17 16h62c10 0 17-7 17-16 0-10-7-17-17-17zm0 19h-62c-2 0-3-1-3-3s1-3 3-3h62c2 0 4 1 4 3s-2 3-4 3zM216 60c4 0 7-3 7-7V7c0-4-3-7-7-7-3 0-7 3-7 7v46c0 4 4 7 7 7zM329 34c-3-2-7-1-9 2l-25 38c-3 4-2 8 1 10l4 1c2 0 4-1 5-3l26-38c2-3 2-8-2-10zM135 84l4-2c3-2 4-6 2-9l-25-39c-2-3-7-4-10-2s-4 6-2 10l25 38c2 3 4 4 6 4zM87 126l-41-22c-3-2-7 0-9 3s-1 7 3 9l40 22 3 1c3 0 5-1 6-4 2-3 1-7-2-9zM396 107c-2-3-6-5-9-3l-41 22c-3 2-5 6-3 9 1 3 4 4 6 4l3-1 41-22c3-2 4-6 3-9z")
           p Modern design
-        .feature.grow
+        .feature.xs6.sm3
           svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#135182" d="M45.8,-34C59.2,-19.7,69.8,-0.7,67.7,18.2C65.6,37.1,50.7,55.8,31.3,65.4C11.8,74.9,-12.3,75.3,-30.4,65.6C-48.4,55.9,-60.4,36.2,-67,13.5C-73.6,-9.2,-74.9,-34.9,-62.8,-48.9C-50.7,-62.8,-25.4,-65,-4.6,-61.3C16.2,-57.6,32.4,-48.2,45.8,-34Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 512 512")
@@ -143,7 +138,7 @@ w-app.home
             path(d="M215 428c0 6 4 10 10 10h62c6 0 10-4 10-10v-19c13-4 26-9 38-16l13 14a10 10 0 0015 0l44-44c4-4 4-11 0-15l-14-13c7-12 12-25 16-38h19c6 0 10-4 10-10v-62c0-6-4-10-10-10h-19c-4-13-9-26-16-38l14-13a10 10 0 000-15l-44-44c-4-4-11-4-15 0l-13 14c-12-7-25-12-38-16V84c0-6-4-10-10-10h-62c-6 0-10 4-10 10v19c-13 4-26 9-38 16l-13-14c-4-4-11-4-15 0l-44 44a10 10 0 000 15l14 13c-7 12-12 24-16 38H84c-6 0-10 4-10 10v62c0 3 1 5 3 7s4 3 7 3h19c4 13 9 26 16 38l-14 13c-4 4-4 11 0 15l44 44c2 2 5 3 7 3 3 0 6-1 8-3l13-14c12 7 25 12 38 16zm-34-56c-4-3-9-2-12 1l-13 13-30-31 13-12c3-3 3-8 1-12-9-14-16-30-19-46-1-5-5-8-10-8H94v-42h17c5 0 9-4 10-8 3-17 10-32 19-46 2-4 2-9-1-12l-13-13 31-30 12 12c3 4 8 4 12 2 14-9 30-16 46-19 4-1 8-5 8-10V94h42v17c0 5 4 9 8 10 16 3 32 10 46 19 4 2 9 2 12-2l12-12 31 30-13 13c-3 3-3 8-1 12 9 14 16 29 19 46 1 4 5 8 10 8h17v42h-17c-5 0-9 3-10 8-3 16-10 32-19 46-2 4-2 9 1 12l13 12-30 31-13-13c-3-3-8-4-12-1-14 9-30 15-46 19-4 1-8 5-8 10v17h-42v-17c0-5-4-9-8-10-16-4-32-10-46-19zm0 0")
             path(d="M339 256a83 83 0 10-166 0 83 83 0 00166 0zm-146 0a63 63 0 11126 0 63 63 0 01-126 0zm0 0M348 38a10 10 0 0010-10c0-3-1-5-3-7a10 10 0 00-14 0 10 10 0 000 14c2 2 4 3 7 3zm0 0M164 474a10 10 0 00-10 10 10 10 0 0010 10 10 10 0 0010-10 10 10 0 00-10-10zm0 0")
           p Highly customizable
-        .feature.grow
+        .feature.xs6.sm3
           svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#3877a9" d="M54.1,-52.3C67.8,-40.3,75.2,-20.1,73.4,-1.8C71.6,16.5,60.6,33,46.8,48.6C33,64.2,16.5,78.8,0.6,78.2C-15.2,77.6,-30.5,61.6,-41.2,46.1C-51.8,30.5,-58,15.2,-60.2,-2.2C-62.4,-19.6,-60.6,-39.3,-50,-51.2C-39.3,-63.2,-19.6,-67.5,0.3,-67.8C20.1,-68,40.3,-64.2,54.1,-52.3Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 512 512")
@@ -152,7 +147,7 @@ w-app.home
             path(d="M477 47H81c-19 0-34 15-34 34v117c0 7 4 11 11 11s12-4 12-11V81c0-7 4-11 11-11h396c7 0 12 4 12 11v280c0 7-5 11-12 11H198c-7 0-12 5-12 12s5 12 12 12h23v35c0 7-5 11-12 11h-11c-7 0-12 5-12 12s5 11 12 11h163c7 0 11-4 11-11s-4-12-11-12h-12c-7 0-12-4-12-11v-35h140c20 0 35-15 35-35V81c0-19-15-34-35-34zM242 442l2-11v-35h70v35c0 4 1 8 3 11h-75z")
             path(d="M454 303H198c-7 0-12 4-12 11s5 12 12 12h256c7 0 11-5 11-12s-4-11-11-11zM128 233H35c-20 0-35 15-35 35v163c0 19 15 34 35 34h93c20 0 35-15 35-34V268c0-20-15-35-35-35zm12 198c0 7-5 11-12 11H35c-7 0-12-4-12-11V268c0-7 5-12 12-12h93c7 0 12 5 12 12v163z")
           p Fully responsive
-        .feature.grow
+        .feature.xs6.sm3
           svg.blob(viewBox="0 0 200 200" preserveAspectRatio="none")
             path(fill="#7db0da" d="M60.4,-48.9C73.3,-32.1,75.2,-7.3,67.4,10.9C59.7,29,42.3,40.3,23,50.8C3.7,61.3,-17.6,71,-32.9,64.9C-48.3,58.9,-57.7,37.1,-62.2,14.9C-66.8,-7.3,-66.3,-29.9,-55.1,-46.3C-43.9,-62.7,-22,-72.9,0.9,-73.6C23.8,-74.4,47.6,-65.7,60.4,-48.9Z" transform="translate(100 100)")
           svg.icon(viewBox="0 0 461.8 461.8")
@@ -303,7 +298,7 @@ export default {
 
     .buttons .w-button {
       margin-top: 1.5em;
-      width: 9em;
+      width: 8.5em;
       height: 24px;
       z-index: 1;
       border: 1px solid rgba(255, 255, 255, 0.4);
@@ -311,8 +306,9 @@ export default {
 
     svg {
       position: absolute;
-      left: -10px;
-      right: -10px;
+      left: -1%;
+      right: -1%;
+      width: 102%;
       fill: #3b588d;
     }
     svg:nth-of-type(1) {
@@ -349,7 +345,7 @@ export default {
     }
 
     &__content {
-      padding: 8px;
+      padding: 12px;
       margin: auto;
       max-width: 700px;
       z-index: 1;
@@ -367,6 +363,7 @@ export default {
 
     p, ul {font-size: 1.1em;}
     .w-list__item-bullet {color: inherit;}
+    .w-card {background: linear-gradient(45deg, rgba(0, 0, 0, 0.05), #fff);}
   }
 
   // Appear transition.
@@ -377,7 +374,7 @@ export default {
   .section--2 {
     background: linear-gradient(to right, #145181, #3777a9);
     color: #fff;
-    padding: 13em 0 9em;
+    padding: 13em 0 11em;
 
     .section__content {overflow: hidden;}
 
@@ -478,8 +475,17 @@ export default {
 
 @media screen and (max-width: 600px) {
   .home__header .title {
-    h1 {font-size: 2.8em;}
-    em {font-size: 1em;}
+    h1 {font-size: 2.8em;letter-spacing: 2px;}
+    em {
+      font-size: 1em;
+      margin-top: 4px;
+      display: block;
+    }
+  }
+  .home .section--2 {padding-top: 4em;}
+  .home .section--2 .feature {
+    margin-top: 4em;
+    padding-bottom: 50%;
   }
 }
 </style>
