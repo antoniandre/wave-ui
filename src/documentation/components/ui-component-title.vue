02e3ab1c79d8759b0d8552b2b5c2a98a956a0a92
Antoni Andre <antoniandre.web@gmail.com>
2020-09-27 17:58:49 +1000

    Create a component for standard UI component title.

---
+++ src/documentation/components/ui-component-title.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+.w-flex.wrap.align-center
+  title-link.mt4.mb3(h1 :class="{ code }" :slug="slug")
+    slot
+  w-button.mla.mb3(to="#api" outline force-link)
+    w-icon.ml-1.mr1(lg) wi-chevron-down
+    | Jump to the API
+</template>
+
+<script>
+export default {
+  props: {
+    code: { type: Boolean, default: true },
+    slug: String
+  }
+}
+</script>
