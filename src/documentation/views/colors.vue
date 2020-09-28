b313fc38ff4ef9a0d507b5123257018d47173ef7
Antoni Andre <antoniandre.web@gmail.com>
2020-09-28 11:15:58 +1000

    Fix v-for key on tpl tag & tpl compiling issues.

--- src/documentation/views/colors.vue
+++ src/documentation/views/colors.vue
@@ -103,16 +103,15 @@ div.ovh
   .text-center(:class="`${horizontal ? 'horizontal' : 'vertical'}`")
     .w-flex.wrap.ma-2.basis-zero
       .color-palette.ma2(
-        v-for="({ color, label, shades }, i) in colors"
-        :key="i")
-        template(v-for="(shade, i) in shades")
+        v-for="({ color, label, shades }, j) in colors"
+        :key="j")
+        template(v-for="(shade, i) in shades" :key="i")
           //- top color.
           .color.color--top(v-if="i === 5" :class="`${label}--bg`")
             span {{ label }}
             small {{ color }}
           //- shades colors.
           .color.color--shade(
-            :key="i"
             :class="[`${shade.label}--bg`, i >= 5 ? 'color--darker' : '']")
             span {{ shade.label }}
             small {{ shade.color }}
