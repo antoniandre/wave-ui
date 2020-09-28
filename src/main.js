3d90fd41b73041f8a9fe7346575a7b24f021efd8
Antoni Andre <antoniandre.web@gmail.com>
2020-09-28 18:05:38 +1000

    Move global components registration to main.js.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import WaveUI from '@/wave-ui/index'
 import '@mdi/font/css/materialdesignicons.min.css'
 
+import SshPre from 'simple-syntax-highlighter'
+import 'simple-syntax-highlighter/dist/sshpre.css'
+import TitleLink from '@/documentation/components/title-link'
+import UiComponentTitle from '@/documentation/components/ui-component-title'
+import Example from '@/documentation/components/example'
+import Alert from '@/documentation/components/alert'
+import Api from '@/documentation/components/api'
+
 const app = createApp({
   render: () => h(App)
 })
@@ -20,4 +28,12 @@ const waveui = new WaveUI(app, {
 })
 
 app.use(router).use(waveui)
+
+app.component('ssh-pre', SshPre)
+app.component('title-link', TitleLink)
+app.component('ui-component-title', UiComponentTitle)
+app.component('example', Example)
+app.component('alert', Alert)
+app.component('api', Api)
+
 app.mount('#app')
