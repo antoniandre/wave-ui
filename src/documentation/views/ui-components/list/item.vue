2c61c79ff6dff3093a1822db182e74c1f085fc6a
Antoni Andre <antoniandre.web@gmail.com>
2020-10-04 16:44:42 +1100

    Cleanup.

--- src/documentation/views/ui-components/list/item.vue
+++ src/documentation/views/ui-components/list/item.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-w-card(bg-color="blue-light5" title="Selected nested router view")
-  .title1 List item: {{ item }}
+w-card(bg-color="blue-light5" title="Nested &lt;router-view&gt;" title-class="title3")
+  .title1 Selected list item: {{ item }}
 </template>
 
 <script>
