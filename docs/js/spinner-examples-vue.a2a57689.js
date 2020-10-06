8051e3a10de022819d71c15af38fd44ccdcb7c77
Antoni Andre <antoniandre.web@gmail.com>
2020-10-07 00:09:14 +1100

    Publish documentation on Github.

--- docs/js/spinner-examples-vue.a2a57689.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpWaveUI"]=window["webpackJsonpWaveUI"]||[]).push([["spinner-examples-vue"],{"042b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div")},l=[],s={},u=s,r=t("2877"),c=Object(r["a"])(u,a,l,!1,null,null,null);n["default"]=c.exports}}]);
-//# sourceMappingURL=spinner-examples-vue.a2a57689.js.map
