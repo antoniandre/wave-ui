99cb6709ad9a8946e143ac76991a5192287c45e7
Antoni Andre <antoniandre.web@gmail.com>
2020-10-06 15:22:09 +1100

    W-alert emits close on dismiss.

--- src/wave-ui/components/w-alert.vue
+++ src/wave-ui/components/w-alert.vue
@@ -7,7 +7,7 @@
       slot
     w-button.w-alert__dismiss(
       v-if="dismiss"
-      @click="$emit('update:modelValue', show = false);$emit('input', false)"
+      @click="$emit('update:modelValue', show = false);$emit('input', false);$emit('close', false)"
       icon="wi-cross"
       color="inherit"
       sm
@@ -51,7 +51,7 @@ export default {
     outline: { type: Boolean }
   },
 
-  emits: ['input', 'update:modelValue'],
+  emits: ['input', 'update:modelValue', 'close'],
 
   data () {
     return {
