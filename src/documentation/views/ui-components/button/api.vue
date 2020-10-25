6ed862e9918c7f0229a85df6d8254f9fc739a75d
Antoni Andre <antoniandre.web@gmail.com>
2020-10-25 19:31:54 +1100

    Update the UI components APIs.

--- src/documentation/views/ui-components/button/api.vue
+++ src/documentation/views/ui-components/button/api.vue
@@ -2,7 +2,6 @@
 div
   .w-divider.my12
   title-link.title1(h2) API
-  alert.mb6(info) The missing props descriptions will be added shortly (all the props are already listed).
 
   api.mt0(:items="props" :descriptions="propsDescs" title="Props")
 
@@ -19,16 +18,16 @@ const propsDescs = {
   bgColor: 'Applies a color to the button\'s background.<br>If no <code>color</code> and no <code>bg-color</code> are set, and if neither <code>outline</code> nor <code>text</code> are set to true, the <code>primary</code> color will be applied.<br>Accepts all the color names of the color palette, status colors, or custom colors (learn more about the colors in the <a href="/colors">colors</a> knowledge base page).<br>Providing a color hex, rgb(a) or hsl(a) will not work.',
   dark: 'When set to true, the text color will be set to white.',
   outline: 'The outline style applies the provided <code>color</code> (by default the <code>primary</code> color is used) to the text and border and no background color is set.',
-  text: '',
+  text: 'Sets the background of the button to transparent. On mouse hover the button background is set to a very light opacity of the given <code>color</code> (primary by default).',
   round: 'Sets a maximum border-radius on the corners of the button, giving it a round look.',
   shadow: 'Applies a drop shadow to the button.',
   tile: 'Removes the default border-radius and sets sharp edges on the button.',
-  to: '',
+  route: 'When provided, the button becomes a link to this route. A <code>router-link</code> will be generated if you use Vue Router, or a normal link otherwise.',
   forceLink: 'In some cases, you may want to use a normal link instead of a <code>router-link</code>, for instance when using anchor links (starting with <code>#</code>), you can use the <code>force-link</code> prop to force a normal link.',
-  type: '',
+  type: 'Provide a native HTML type attribute.',
   disabled: 'Disables the button making it unreactive to user interactions.',
-  loading: '',
-  icon: '',
+  loading: 'Sets the loading state of the button. While loading, the button is unclickable and a spinner is displayed instead of the button label.',
+  icon: 'Sets the button to a round icon style, containing only an icon. This prop accepts a string: e.g. <code>mdi mdi-home</code>.',
   absolute: 'Sets the CSS position of the button to <code>absolute</code>.',
   fixed: 'Sets the CSS position of the button to <code>fixed</code>.',
   top: 'Places the button at the top of the screen when the <code>fixed</code> prop is set to true or at the top of its container when the <code>absolute</code> prop is set to true.',
@@ -47,7 +46,7 @@ const propsDescs = {
 
 const slots = {
   default: { description: 'The button content.' },
-  loading: { description: 'Provide a custom loading content, if the default spinner doesn\'t fit.' }
+  loading: { description: 'Provide a custom loading content, if the default spinner doesn\'t suit in your app.' }
 }
 
 const eventsDescs = {}
