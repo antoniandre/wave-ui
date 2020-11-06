2575e48615d0fea7ddce4d4b75accd495061a3c5
Antoni Andre <antoniandre.web@gmail.com>
2020-11-06 11:06:52 +1100

    Add an in-progress param to ui-component-title.

--- src/documentation/components/ui-component-title.vue
+++ src/documentation/components/ui-component-title.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
 .w-flex.wrap.align-center
-  title-link.mt4.mb3(h1 :class="{ code }" :slug="slug")
-    slot
+  title-link.mt4.mb3(h1 :class="{ code: code && !inProgress }" :slug="slug")
+    .code(v-if="inProgress")
+      slot
+    slot(v-else)
+    w-tag.ml2.text-bold.text-upper(v-if="inProgress" color="orange" outline round) In progress
   w-button.mla.mb3(route="#api" outline force-link)
     w-icon.ml-1.mr1(lg) wi-chevron-down
     | Jump to the API
@@ -11,6 +14,7 @@
 export default {
   props: {
     code: { type: Boolean, default: true },
+    inProgress: { type: Boolean },
     slug: String
   }
 }
