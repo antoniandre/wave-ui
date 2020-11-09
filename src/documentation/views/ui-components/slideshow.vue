705b0287c22558366a12560f1369ba3589b1d7c9
Antoni Andre <antoniandre.web@gmail.com>
2020-11-09 15:26:33 +1100

    Add a full demo of Vueperslides.

--- src/documentation/views/ui-components/slideshow.vue
+++ src/documentation/views/ui-components/slideshow.vue
@@ -17,33 +17,92 @@ main
 
   ssh-pre.mt6(language="shell") npm i vueper-slides
 
-  img.mt6(src="@/assets/vueperslides.jpg" alt="Vueper Slides")
+  vueper-slides.hero.mt6(
+    lazy
+    lazy-load-on-drag
+    parallax
+    parallax-fixed-content
+    :breakpoints="{ 800: { fixedHeight: '300px' } }")
+    vueper-slide(v-for="(slide, i) in slides" :key="i" :image="slide.image")
+      template(v-slot:content)
+        .vueperslide__title {{ slide.title }}
+        .vueperslide__content
+          | Photo by
+          a(:href="slide.link" target="_blank") {{ slide.content.substring(9) }}
+      template(v-slot:loader)
+        w-progress(circle indeterminate)
+        .title3.mt3.primary Loading...
 
   title-link(h2) Features
   w-list(:items="listItems" icon="wi-check")
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
+  components: { VueperSlides, VueperSlide },
+
   data: () => ({
     listItems: [
-      { label: '<strong>Fully responsive</strong> and scales with its container.' },
-      { label: '<strong>Touch ready</strong> & mouse dragging for desktop.' },
-      { label: '<strong>Accessibility friendly</strong> & keyboard navigation.' },
+      { label: '<strong>Vue 3 compatible</strong>.' },
+      { label: '<strong class="mr1">Fully responsive</strong> and scales with its container.' },
+      { label: '<strong class="mr1">Touch ready</strong> &amp; mouse dragging for desktop.' },
+      { label: '<strong class="mr1">Accessibility friendly</strong> &amp; keyboard navigation.' },
       { label: '<strong>Highly customizable</strong>.' },
       { label: '<strong>Lazy loading</strong>.' },
       { label: 'Show multiple items per slides.' },
       { label: '<strong>Infinite looping</strong>, customizable arrows or disable arrow on a slideshow end, autoplay.' },
-      { label: 'Built-in <strong>parallax</strong> effect & <strong>3D rotation</strong>.' },
-      { label: '<strong>Breakpoints</strong> with different configuration.' },
-      { label: 'Slide content supports <strong>title & description, inside OR outside</strong> the current slide.' },
-      { label: '<strong>Add or remove slides</strong> on the fly, <strong>disable or enable the slideshow</strong>.' },
-      { label: 'Uses <strong>CSS animations</strong> & comes with a minimum of styles (using the BEM convention).' },
-      { label: '<strong>Emitted events</strong> for callbacks, etc...' }
+      { label: 'Built-in <strong class="mx1">parallax</strong> effect &amp; <strong class="ml1">3D rotation</strong>.' },
+      { label: '<strong class="mr1">Breakpoints</strong> with different configuration.' },
+      { label: 'Slide content supports <strong class="mx1">title &amp; description, inside OR outside</strong> the current slide.' },
+      { label: '<strong class="mr1">Add or remove slides</strong> on the fly, <strong class="mr1">disable or enable the slideshow</strong>.' },
+      { label: 'Uses <strong class="mx1">CSS animations</strong> &amp; comes with a minimum of styles (using the BEM convention).' },
+      { label: '<strong class="mr1">Emitted events</strong> for callbacks, etc...' }
+    ],
+    slides: [
+      {
+        title: 'El Teide Volcano, Spain',
+        content: 'Photo by Max Rive',
+        image: 'https://antoniandre.github.io/vueper-slides/images/el-teide-volcano-spain.jpg',
+        link: 'https://www.maxrivephotography.com/index/C0000rU1RKCHdqwI/G0000X57AtIzuRX0/I0000Gvr9HqdtyXk'
+      },
+      {
+        title: 'Chernobyl, Ukraine',
+        content: 'Photo by Jesse Moran',
+        image: 'https://antoniandre.github.io/vueper-slides/images/chernobyl-ukraine.jpg',
+        link: 'https://www.flickr.com/photos/jessemoran'
+      },
+      {
+        title: 'Crater Lake, Oregon, USA',
+        content: 'Photo by Jesse Moran',
+        image: 'https://antoniandre.github.io/vueper-slides/images/crater-lake-oregon-usa.jpg',
+        link: 'https://flic.kr/p/2cxrCmp'
+      }
     ]
   })
 }
 </script>
 <style lang="scss">
-.main--slideshow img {max-width: 100%;}
+.main--slideshow .hero {
+  &, & button {color: #fff;}
+
+  .vueperslide__content-wrapper {
+    font-family: inherit;
+    justify-content: flex-end;
+    align-items: flex-end;
+    opacity: 0.8;
+    padding-bottom: 0.5em;
+    padding-right: 1em;
+  }
+  .vueperslide__title {font-size: 2.3em;line-height: 1.3;}
+
+  a {
+    padding-left: 0.3em;
+    pointer-events: all;
+    color: inherit;
+    text-decoration: underline;
+  }
+}
 </style>
