a3b5b371ec22819960684d3bdae2331647ef5b06
antoniandre <antoniandre.web@gmail.com>
2020-12-16 19:43:40 -0500

    Create the bare bones w-table component.

--- src/wave-ui/components/w-table.vue
+++ src/wave-ui/components/w-table.vue
@@ -1,24 +1,54 @@
 <template lang="pug">
-.w-table
+.w-table-wrap
+  table.w-table
+    thead(v-if="!noHeaders")
+      tr
+        th(v-for="(header, i) in headers" v-html="header.label || ''")
+    tbody
+      template(v-if="items.length")
+        tr(v-for="(item, i) in sortedItems")
+          td(v-for="(header, i) in headers" v-html="item[header.key] || ''")
+      tr(v-else)
+        td.text-center(:colspan="headers.length")
+          slot(name="no-data") No data to show.
 </template>
 
 <script>
 export default {
   name: 'w-table',
   props: {
-
+    items: { type: Array, required: true },
+    headers: { type: Array, required: true },
+    noHeaders: { type: Boolean },
+    fixedHeaders: { type: Boolean },
+    sort: { type: Object },
+    filters: { type: Object }
   },
 
   emits: [],
 
   data: () => ({
 
-  })
+  }),
+
+  computed: {
+    filteredItems () {
+      return this.items
+    },
+    sortedItems () {
+      return this.filteredItems
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.w-table {
+.w-table-wrap {position: relative;}
 
+.w-table {
+  width: 100%;
+
+  th {text-align: left;}
+  th, td {padding-left: $base-increment;padding-right: $base-increment;}
 }
 </style>
