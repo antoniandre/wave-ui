62bffad886cfb9b3bb3aed476d4bd972b202f5e9
antoniandre <antoniandre.web@gmail.com>
2020-12-19 00:45:05 -0500

    Update docs & Publish on Github.

--- src/documentation/views/ui-components/table/examples.vue
+++ src/documentation/views/ui-components/table/examples.vue
@@ -5,11 +5,40 @@ div
   example
     w-table(:items="table1.items" :headers="table1.headers")
     template(#pug).
+      w-table(:items="table1.items" :headers="table1.headers")
+    template(#js).
+      data: () => ({
+        table1: {
+          headers: [
+            { label: 'ID', key: 'id' },
+            { label: 'First name', key: 'firstName' },
+            { label: 'Last name', key: 'lastName' },
+          ],
+          items: [
+            { id: '1', firstName: 'Floretta', lastName: 'Sampson' },
+            { id: '2', firstName: 'Nellie', lastName: 'Lynn' },
+            { id: '3', firstName: 'Rory', lastName: 'Bristol' },
+            { id: '4', firstName: 'Daley', lastName: 'Elliott' },
+            { id: '5', firstName: 'Virgil', lastName: 'Carman' },
+          ]
+        }
+      })
 
   .title3 Without data
   example
     w-table(:items="[]" :headers="table1.headers")
     template(#pug).
+      w-table(:items="[]" :headers="table1.headers")
+    template(#js).
+      data: () => ({
+        table1: {
+          headers: [
+            { label: 'ID', key: 'id' },
+            { label: 'First name', key: 'firstName' },
+            { label: 'Last name', key: 'lastName' },
+          ]
+        }
+      })
 
   title-link(h2) No headers
   p.
@@ -18,14 +47,48 @@ div
   example
     w-table(:items="table1.items" :headers="table1.headers" no-headers)
     template(#pug).
+      w-table(:items="table1.items" :headers="table1.headers" no-headers)
+    template(#js).
+      data: () => ({
+        table1: {
+          headers: [
+            { label: 'ID', key: 'id' },
+            { label: 'First name', key: 'firstName' },
+            { label: 'Last name', key: 'lastName' },
+          ],
+          items: [
+            { id: '1', firstName: 'Floretta', lastName: 'Sampson' },
+            { id: '2', firstName: 'Nellie', lastName: 'Lynn' },
+            { id: '3', firstName: 'Rory', lastName: 'Bristol' },
+            { id: '4', firstName: 'Daley', lastName: 'Elliott' },
+            { id: '5', firstName: 'Virgil', lastName: 'Carman' },
+          ]
+        }
+      })
 
   title-link(h2) Initial Sorting
   example
-    w-table(
-      :items="table1.items"
-      :headers="table1.headers"
-      :sort.sync="table1.sort")
+    w-table(:items="table1.items" :headers="table1.headers" :sort.sync="table1.sort")
     template(#pug).
+      w-table(:items="table1.items" :headers="table1.headers" :sort.sync="table1.sort")
+    template(#js).
+      data: () => ({
+        table1: {
+          headers: [
+            { label: 'ID', key: 'id' },
+            { label: 'First name', key: 'firstName' },
+            { label: 'Last name', key: 'lastName' },
+          ],
+          items: [
+            { id: '1', firstName: 'Floretta', lastName: 'Sampson' },
+            { id: '2', firstName: 'Nellie', lastName: 'Lynn' },
+            { id: '3', firstName: 'Rory', lastName: 'Bristol' },
+            { id: '4', firstName: 'Daley', lastName: 'Elliott' },
+            { id: '5', firstName: 'Virgil', lastName: 'Carman' },
+          ],
+          sort: [{ firstName: 'asc' }]
+        }
+      })
 
   title-link(h2) Filters
   example
@@ -35,6 +98,23 @@ div
       :headers="table1.headers"
       :filters="() => {}")
     template(#pug).
+    template(#js).
+      data: () => ({
+        table1: {
+          headers: [
+            { label: 'ID', key: 'id' },
+            { label: 'First name', key: 'firstName' },
+            { label: 'Last name', key: 'lastName' },
+          ],
+          items: [
+            { id: '1', firstName: 'Floretta', lastName: 'Sampson' },
+            { id: '2', firstName: 'Nellie', lastName: 'Lynn' },
+            { id: '3', firstName: 'Rory', lastName: 'Bristol' },
+            { id: '4', firstName: 'Daley', lastName: 'Elliott' },
+            { id: '5', firstName: 'Virgil', lastName: 'Carman' },
+          ]
+        }
+      })
 </template>
 
 <script>
