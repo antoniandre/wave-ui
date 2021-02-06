3174cfa2bccd3ceb1f23f52da05672c0f432ad22
antoni andre <antoniandre.web@gmail.com>
2021-02-05 23:25:14 -0500

    Update the release notes.

--- src/documentation/views/release-notes.vue
+++ src/documentation/views/release-notes.vue
@@ -23,6 +23,10 @@ main
     //- li Added the #[code hide-error] option on all the form elements.
 
     li.minor
+      strong.version v1.19.0
+      p Add the row selection feature to the w-table.
+
+    li.minor
       strong.version v1.18.0
       p Add an image option to the #[strong.code w-card] component.
 
