efc34b96df9e823cda3a00c7bfe62fabd3b01412
antoni andre <antoniandre.web@gmail.com>
2021-02-07 19:21:20 -0500

    Update the documentation about a-la-carte install.

---
+++ src/documentation/views/getting-started/index.vue
@@ -0,0 +1,103 @@
+<template lang="pug">
+main
+  title-link.mt4(h1) Getting started
+
+  .title3.mb10
+    w-icon.ml-2.mr2 wi-chevron-right
+    | Pick 1 of the 3 following installation methods.
+
+  w-accordion(
+    :items="3"
+    :value="openPane"
+    expand-single
+    title-class="justify-space-between py4 pl6 blue-light5--bg"
+    expand-icon="wi-chevron-right"
+    collapse-icon="wi-chevron-up"
+    expand-icon-right)
+    //- Standard install.
+    template(#item-title.1)
+      title-link.ma0(h2 slug="standard-installation") Standard Installation
+    template(#item-content.1)
+      standard-install
+
+    //- Custom install.
+    template(#item-title.2)
+      title-link.grey-light2.ma0(h2 slug="a-la-carte") À la carte (tree shaking) - Coming soon
+    //- template(#item-content.2)
+      custom-install
+
+    //- Nuxt install.
+    template(#item-title.3)
+      title-link.ma0(h2 slug="installation-on-nuxt") Installation on Nuxt (Vue 2)
+    template(#item-content.3)
+      nuxt-install
+
+  w-divider.my12
+  title-link(h2) What next?
+  p Read on the knowledge base pages! It will give you an overview of all the power in your hands!
+  h3 Then what next?
+  p Now that you are more familiar with Wave UI, check out a UI component of your choice and explore its examples.
+  p You can open each example in Codepen, that will help you learn faster by trying it yourself.
+
+  title-link(h3).mt8 Icons font
+  p.
+    An icon font is probably the next thing you need at this point.#[br]
+    You are free to use the one of your choice, but if you don't know where to start, we have
+    picked and tested a handful of them for you:
+    check out the #[router-link(to="w-icon") w-icon] page to know more.
+
+  w-divider.my12
+  title-link(h2) Wave UI Options
+  p Wave UI accepts these options:
+  w-list(:items="waveUiOptions" icon="wi-check")
+    template(#item="{ item }")
+      span {{ item.label }}
+      template(v-if="item.route")
+        span.ml1 - read on in the
+        router-link.mx1(:to="item.route") {{ item.label }}
+        | knowledge base page.
+</template>
+
+<script>
+import StandardInstall from './install-standard'
+import CustomInstall from './install-custom'
+import NuxtInstall from './install-nuxt'
+
+export default {
+  components: { StandardInstall, CustomInstall, NuxtInstall },
+
+  data: () => ({
+    waveUiOptions: [
+      { label: 'colors', route: 'colors' },
+      { label: 'breakpoints', route: 'breakpoints' },
+      { label: 'presets', route: 'customization#presets' }
+    ],
+    openPane: [true, false, false]
+  }),
+
+  mounted () {
+    const hash = document.location.hash || ''
+    if (hash.includes('a-la-carte')) this.openPane = [false, true, false]
+    else if (hash.includes('nuxt')) this.openPane = [false, false, true]
+  }
+}
+</script>
+
+<style lang="scss">
+.main--getting-started {
+  .w-accordion__item {margin-bottom: 12px;}
+  .w-accordion__item-title {border-top: none;}
+  .w-accordion__item-content {background: #f7fbff;}
+  .w-button--icon.size--md {font-size: 20px;}
+
+  // .w-accordion ol {font-size: 1.3em;}
+  .w-accordion .ssh-pre, .w-accordion em {font-size: 1rem;}
+
+  .vue-install .comment {user-select: none;}
+  .ssh-pre[data-label]:before {font-size: 12px;}
+}
+
+@media screen and (max-width: 600px) {
+  .main--getting-started .w-accordion {margin-left: -16px;margin-right: -16px;}
+}
+</style>
---
+++ src/documentation/views/getting-started/install-custom.vue
@@ -0,0 +1,121 @@
+<template lang="pug">
+div
+  p.mt4.
+    The #[em à la carte] system lets you choose which components to import and discard the ones you don't need
+    on the final build, which will considerably reduce the build file size.
+
+  ol
+    li.mt8
+      .title4 Install the dependencies &amp; devDependencies:
+      ssh-pre(language="shell") npm i wave-ui
+      ssh-pre.mb2(language="shell") npm i -D pug pug-plain-loader node-sass sass-loader
+      small.text-italic.grey.
+        The dev dependencies are only needed for building the project. They will not ship to production.
+
+    li.mt8
+      .title4 Create a #[span.code scss/_variables.scss] file
+      p.
+        In your project #[span.code src] folder, create a #[span.code _variables.scss] file in a
+        #[span.code scss/] folder, and import Wave UI's variables:
+
+      ssh-pre(language="css" label="scss/_variables.scss").
+        @import 'wave-ui/src/wave-ui/scss/_variables';
+
+        /* You can add variables overrides here. */
+
+    li.mt8
+      .title4 Update #[span.code vue.config.js]
+      p.
+        In #[span.code vue.config.js], transpile #[span.code wave-ui] and import your variables file
+        globally. Then re-serve the app.
+      ssh-pre(language="js" label="vue.config.js").
+        module.exports = {
+          transpileDependencies: ['wave-ui'], // ! \\
+          css: {
+            loaderOptions: {
+              // `additionalData` was called `prependData` prior sass-loader 9.
+              sass: { additionalData: '@import "@/scss/_variables.scss";' }
+            }
+          }
+        }
+
+    li.mt8
+      .title4 Import only what you need
+      p Update the #[span.code main.js] file, adding only the Wave UI components you will need.
+
+      w-tabs.mt4(:items="2" content-class="pa0")
+        template(#item-title.1) Vue 2.x
+        template(#item-content.1)
+          ssh-pre.ma0(language="js").
+            // Keep these 2 imports.
+            import Vue from 'vue'
+            import App from './App.vue'
+
+            // 1.
+            // Import Wave UI from the src directory.
+            import WaveUI from 'wave-ui/src/wave-ui/core'
+
+            // 2.
+            // Import only the components you need. e.g.
+            import { WApp, WButton } from 'wave-ui/src/wave-ui/components'
+
+            Vue.use(WaveUI, {
+              components: { WApp, WButton }
+            })
+
+            // 3.
+            // Instantiate the WaveUI class and register it in the Vue instance.
+            const waveui = new WaveUI({
+              // Some Wave UI options.
+            })
+
+            new Vue({
+              waveui,
+              render: h => h(App)
+            }).$mount('#app')
+
+        template(#item-title.2) Vue 3
+        template(#item-content.2)
+          ssh-pre.ma0.py12.text-center Coming Soon.
+          //- ssh-pre.ma0(language="js").
+            // Keep these 2 imports.
+            import { createApp, h } from 'vue'
+            import App from './App.vue'
+
+            // 1.
+            // Import Wave UI from the src directory &amp; create the Vue app.
+            import WaveUI from 'wave-ui/src/wave-ui'
+
+            const app = createApp({
+              render: () => h(App)
+            })
+
+            // 2.
+            // Import only the components you need. e.g.
+            import { WApp, WButton } from 'wave-ui/src/wave-ui/components'
+
+            Vue.use(WaveUI, {
+              components: { WApp, WButton }
+            })
+
+            // 3.
+            // Instantiate the WaveUI class and register it in the Vue instance.
+            new WaveUI(app, {
+              // Some Wave UI options.
+            })
+
+            app.mount('#app')
+      alert(info)
+        div.mb4.
+          Some Wave UI elements use other elements under the hood. For instance #[strong.code w-dialog] uses
+          #[strong.code w-overlay] &amp; #[strong.code w-card].#[br]#[br]
+          If you encounter the following error, you need to import the missing component:
+        strong.error [Vue warn]: Unknown custom element: &lt;w-overlay&gt; - did you register the component correctly?
+</template>
+
+<style lang="scss">
+.main--getting-started .w-tabs .ssh-pre {
+  border-radius: 0;
+  border-width: 1px 0 0;
+}
+</style>
