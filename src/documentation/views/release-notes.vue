747d6d8c00669d1319bebc025b6edd4293237e95
antoni andre <antoniandre.web@gmail.com>
2021-02-20 18:01:08 -0500

    Update the release notes.

--- src/documentation/views/release-notes.vue
+++ src/documentation/views/release-notes.vue
@@ -26,6 +26,7 @@ main
       strong.version v1.20.7
       ul
         li Add #[code item-title-key] &amp; #[code item-content-key] options on #[strong.code w-tabs] component and remove internal #[code _active] prop.
+        li #[strong.code w-select]: allow selecting items using the keyboard arrows when the select list is closed. #[issue-link 25]
 
     li.patch
       strong.version v1.20.6
