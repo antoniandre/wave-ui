f65d0f2d39dac7eec9a393747e5d9a8a9fc7477e
antoni andre <antoniandre.web@gmail.com>
2021-03-28 17:22:24 -0400

    Improve UI.

--- src/documentation/components/ui-component-title.vue
+++ src/documentation/components/ui-component-title.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
-.w-flex.wrap.align-center
-  title-link.mt4.mb3(h1 :class="{ code: code && !inProgress }" :slug="slug")
-    .code(v-if="inProgress")
-      slot
-    slot(v-else)
-    w-tag.ml2.text-bold.text-upper(v-if="inProgress" color="orange" outline round) In progress
-  w-button.mla.mb3(route="#api" outline force-link)
-    w-icon.ml-1.mr1(lg) wi-chevron-down
-    | Jump to the API
+.ui-component-title
+  .w-flex.wrap.align-center
+    title-link.mt4.mb3(h1 :class="{ code: code && !inProgress }" :slug="slug")
+      .code(v-if="inProgress")
+        slot
+      slot(v-else)
+      w-tag.ml2.text-bold.text-upper(v-if="inProgress" color="orange" outline round) In progress
+    w-button.mla.mb3(route="#api" outline force-link)
+      w-icon.ml-1.mr1.chevron-down(lg) wi-chevron-down
+      | Jump to the API
+
+  alert.mt4.mb12(warning).
+    While a component is in progress, the props, slots and events may change when you upgrade.#[br]
+    Always keep track in the #[router-link(to="/release-notes") release notes].
 </template>
 
 <script>
@@ -19,3 +24,7 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.ui-component-title .chevron-down:before {margin-top: 3px;}
+</style>
