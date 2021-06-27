cbf21fc7f1ca20c7b89d2d0795f67c47a4f6dbc3
antoni andre <antoniandre.web@gmail.com>
2021-06-27 18:05:07 -0400

    Add notification-manager align & transition options.

--- src/wave-ui/utils/notification-manager.js
+++ src/wave-ui/utils/notification-manager.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 
+import config from '../utils/config'
+
 export default class NotificationManager {
   static instance
   notifications
+  align
+  transition
    // Private fields.
-  #notificationManagerEl
-  #uid = 0 // A unique ID for each notification.
+  #uid // A unique ID for each notification.
   #notificationDefaults
 
   constructor () {
@@ -13,7 +16,10 @@ export default class NotificationManager {
     if (NotificationManager.instance) return NotificationManager.instance
 
     NotificationManager.instance = this
+    this.align = config.notificationManager.align === 'left' ? 'left' : 'right'
+    this.transition = config.notificationManager.transition || 'slide'
     this.notifications = []
+    this.#uid = 0
     this.#notificationDefaults = {
       _uid: 0,
       _value: true,
@@ -21,10 +27,6 @@ export default class NotificationManager {
       timeout: 4000,
       dismiss: true
     }
-  }
-
-  init () {
-    this.#notificationManagerEl = document.querySelector('.w-notification-manager')
   }
 
   notify (...args) {
