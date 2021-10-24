dac2a0ad1a68f0af8bdf8e2be07cfb7c6d727d05
antoni andre <antoniandre.web@gmail.com>
2021-10-24 19:00:31 -0400

    Add w-divider examples source code in Pug.

--- src/documentation/views/ui-components/divider/examples.vue
+++ src/documentation/views/ui-components/divider/examples.vue
@@ -45,6 +45,18 @@ div
       w-divider.mx1(vertical)
       span.mx1 Item 5
       span.mx1 Item 6
+    template(#pug).
+      w-toolbar.py0(bg-color="grey-light5")
+        .title2.my2 Toolbar
+        .spacer
+        span.mx1 Item 1
+        span.mx1 Item 2
+        w-divider.mx1(vertical)
+        span.mx1 Item 3
+        span.mx1 Item 4
+        w-divider.mx1(vertical)
+        span.mx1 Item 5
+        span.mx1 Item 6
     template(#html).
       &lt;w-toolbar class="py0" bg-color="grey-light5"&gt;
         &lt;div class="title2 my2"&gt;Toolbar&lt;/div&gt;
@@ -62,6 +74,8 @@ div
   title-link(h2) Color
   example
     w-divider.ma6(color="green")
+    template(#pug).
+      w-divider.ma6(color="green")
     template(#html).
       &lt;w-divider class="ma6" color="green" /&gt;
 
@@ -70,21 +84,31 @@ div
     A default slot is available to provide some content to display in the middle of the divider.
     This is often use with a OR keyword to offer an alternative.
   example
-    .w-flex.align-center.justify-center
+    w-flex(align-center justify-center)
       w-button.my6 Log in
       w-divider.mx6(vertical color="pink") OR
       w-button.my6 Sign in
+    template(#pug).
+      w-flex(align-center justify-center)
+        w-button.my6 Log in
+        w-divider.mx6(vertical color="pink") OR
+        w-button.my6 Sign in
     template(#html).
-      &lt;div class="w-flex align-center justify-center"&gt;
+      &lt;w-flex align-center justify-center&gt;
         &lt;w-button class="my6"&gt;Log in&lt;/w-button&gt;
         &lt;w-divider class="mx6" vertical color="pink"&gt;OR&lt;/w-divider&gt;
         &lt;w-button class="my6"&gt;Sign in&lt;/w-button&gt;
-      &lt;/div&gt;
+      &lt;/w-flex&gt;
   example
     w-card.md6.maa.text-center
       w-button.my12 Log in
       w-divider.mx6(color="pink") OR
       w-button.my12 Sign in
+    template(#pug).
+      w-card.md6.maa.text-center
+        w-button.my12 Log in
+        w-divider.mx6(color="pink") OR
+        w-button.my12 Sign in
     template(#html).
       &lt;w-card class="md6 maa text-center"&gt;
         &lt;w-button class="my12"&gt;Log in&lt;/w-button&gt;
