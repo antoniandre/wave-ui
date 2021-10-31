b75bfe2036e19b4c42e0017b722627e2a8f34411
antoni andre <antoniandre.web@gmail.com>
2021-10-31 12:43:40 -0400

    Create a new confirm button component.

---
+++ src/documentation/views/ui-components/confirm/examples.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  title-link(h2) Default
+  example
+    w-confirm Test
+    template(#html).
+    template(#pug).
+</template>
+
+<script>
+export default {
+
+}
+</script>
