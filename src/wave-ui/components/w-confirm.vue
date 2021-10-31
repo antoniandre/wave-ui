424cbe79cee5a27ea0ca7156290004e64297a2e5
antoni andre <antoniandre.web@gmail.com>
2021-10-31 13:36:43 -0400

    Handle basic confirm button cases.

--- src/wave-ui/components/w-confirm.vue
+++ src/wave-ui/components/w-confirm.vue
@@ -1,20 +1,66 @@
 <template lang="pug">
 .w-confirm
-  w-button Click me
+  w-menu(v-model="showPopup" v-bind="menuProps")
+    template(#activator="{ on }")
+      w-button.w-confirm__button(v-on="on" v-bind="buttonProps")
+        slot
+    .w-confirm__menu
+      div
+        slot(name="question") Are you sure?
+      .w-flex.justify
+        w-button(@click="showPopup = false") Cancel
+        w-button(@click="showPopup = false") Ok
 </template>
 
 <script>
 export default {
   name: 'w-confirm',
   props: {
+    bgColor: { type: String },
+    color: { type: String },
+    icon: { type: String },
 
+    // W-menu props.
+    top: { type: Boolean },
+    bottom: { type: Boolean },
+    left: { type: Boolean },
+    right: { type: Boolean },
+    alignTop: { type: Boolean },
+    alignBottom: { type: Boolean },
+    alignLeft: { type: Boolean },
+    alignRight: { type: Boolean },
+    persistent: { type: Boolean }
   },
 
   emits: [],
 
   data: () => ({
+    showPopup: false,
+    props: []
+  }),
 
-  })
+  computed: {
+    menuProps () {
+      return {
+        top: this.top,
+        bottom: this.bottom,
+        left: this.left,
+        right: this.right,
+        alignTop: this.alignTop,
+        alignBottom: this.alignBottom,
+        alignLeft: this.alignLeft,
+        alignRight: this.alignRight,
+        persistent: this.persistent
+      }
+    },
+    buttonProps () {
+      return {
+        bgColor: this.bgColor,
+        color: this.color,
+        icon: this.icon
+      }
+    }
+  }
 }
 </script>
 
