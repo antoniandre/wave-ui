d60688eccf2dcd29fd360ce7d36ee50de396c276
antoni andre <antoniandre.web@gmail.com>
2021-11-06 18:48:57 -0400

    Allow transitions on w-confirm's menu.

--- src/documentation/views/ui-components/confirm/examples.vue
+++ src/documentation/views/ui-components/confirm/examples.vue
@@ -7,53 +7,79 @@ div
   title-link(h2) Custom question
   example
     w-confirm
-      strong something bold
+      strong Ask for confirm
       template(#question) Are you sure you want to delete this?
 
   title-link(h2) Icon button
   example
-    w-confirm(icon="wi-cross")
+    w-confirm(icon="mdi mdi-delete")
 
   title-link(h2) Transitions
   example
-    w-confirm(transition="fade") Confirm button
+    w-flex(wrap)
+      w-confirm.ma1 Default
+      w-confirm.ma1(transition="fade") Fade
+      w-confirm.ma1(transition="twist") Twist
+      w-confirm.ma1(transition="bounce") Bounce
+      w-confirm.ma1(transition="slide-up") Slide up
+      w-confirm.ma1(transition="slide-right") Slide right
+      w-confirm.ma1(transition="slide-down") Slide down
+      w-confirm.ma1(transition="slide-left") Slide left
+      w-confirm.ma1(transition="slide-fade-up") Slide fade up
+      w-confirm.ma1(transition="slide-fade-right") Slide fade right
+      w-confirm.ma1(transition="slide-fade-down") Slide fade down
+      w-confirm.ma1(transition="slide-fade-left") Slide fade left
+      w-confirm.ma1(transition="bounce") Bounce
+      w-confirm.ma1(transition="bounce") Bounce
 
   title-link(h2) Colors
   example
-    w-confirm(bg-color="success" color="white") Confirm button
-    w-confirm(bg-color="indigo" color="yellow") Confirm button
+    w-confirm.mb2(bg-color="success" color="white") Ask for confirm
+    w-confirm(bg-color="indigo" color="yellow") Ask for confirm
 
-  title-link(h2) Alignments
+  title-link(h2) Positions &amp; alignments
+  title-link(h3) Positions
   example
-    w-confirm.my5(bg-color="red" align-right) Confirm button
-    w-confirm.my5(bg-color="red" align-left) Confirm button
-    w-confirm.my5(bg-color="red" top) Confirm button
-    w-confirm.my5(bg-color="red" top align-left) Confirm button
-    w-confirm.my5(bg-color="red" left align-bottom) Confirm button
+    w-confirm.my5(top) Top
+    w-confirm.my5(right) Right
+    w-confirm.my5(bottom) Bottom
+    w-confirm.my5(left) Left
+
+  title-link(h3) Alignments
+  example
+    w-confirm.my5(top align-right) Top, align right
+    w-confirm.my5(top align-left) Top, align left
+
+    w-confirm.my5(right align-top) Right, align top
+    w-confirm.my5(right) Right, align center
+    w-confirm.my5(right align-bottom) Right, align bottom
 
   title-link(h2) Persistent
   example
-    w-confirm(persistent) Confirm button
+    w-confirm(persistent) Ask for confirm
 
-  title-link(h2) Cancel &amp; accept buttons
+  title-link(h2) Cancel &amp; confirm buttons
   example
     w-confirm
-      | Confirm button
+      | Ask for confirm
       template(#cancel) Dismiss
-      template(#accept) Accept
+      template(#confirm) Agree
+
+  example
+    w-confirm(no-cancel) Ask for confirm
 
   title-link(h2) Inline question
   example
     w-confirm(inline)
-      | Confirm button
+      | Ask for confirm
       template(#cancel) Dismiss
-      template(#accept) Accept
+      template(#confirm) Confirm
 
   title-link(h2) Events
   example
     w-confirm(
       @cancel="$waveui.notify('Canceled.', 'error')"
-      @accept="$waveui.notify('Accepted!', 'success')") Confirm button
+      @confirm="$waveui.notify('Confirmed!', 'success')") Ask for confirm
 </template>
 
 <script>
--- src/wave-ui/components/w-confirm.vue
+++ src/wave-ui/components/w-confirm.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .w-confirm
-  w-menu(v-model="showPopup" v-bind="menuProps")
+  w-menu(v-model="showPopup" v-bind="wMenuProps")
     template(#activator="{ on }")
       w-button.w-confirm__button(v-on="on" v-bind="buttonProps")
         slot
@@ -8,24 +8,35 @@
       div
         slot(name="question") Are you sure?
       .w-flex.justify-end(:class="inline ? 'ml2' : 'mt2'")
-        w-button.mr2(bg-color="error" @click="onCancel")
+        w-button.mr2(v-if="!noCancel" :bg-color="cancelButton.bgColor || 'error'" @click="onCancel")
           slot(name="cancel") Cancel
-        w-button(bg-color="success" @click="onAccept")
-          slot(name="accept") Ok
+        w-button(:bg-color="confirmButton.bgColor || 'success'" @click="onConfirm")
+          slot(name="confirm") Confirm
 </template>
 
 <script>
 export default {
   name: 'w-confirm',
   props: {
+    // Main button props.
     bgColor: { type: String },
     color: { type: String },
     icon: { type: String },
+    tooltip: { type: Boolean },
+
+    // Cancel & confirm buttons props.
     cancelButtonColor: { type: String },
-    acceptButtonColor: { type: String },
-    inline: { type: Boolean },
+    confirmButtonColor: { type: String },
+    noCancel: { type: Boolean }, // Removes the cancel button.
+    cancelButton: { type: [Boolean, Object] }, // Allow passing down an object of props to the w-button component.
+    confirmButton: { type: [Boolean, Object] }, // Allow passing down an object of props to the w-button component.
+
+    // global menu props.
+    inline: { type: Boolean }, // The layout inside the menu.
 
     // W-menu props.
+    menuProps: { type: Object }, // Allow passing down an object of props to the w-menu component.
+    // All the menu props shorthands, as long as they don't conflict with the button props.
     top: { type: Boolean },
     bottom: { type: Boolean },
     left: { type: Boolean },
@@ -38,7 +49,7 @@ export default {
     transition: { type: String }
   },
 
-  emits: [],
+  emits: ['cancel', 'confirm'],
 
   data: () => ({
     showPopup: false,
@@ -46,7 +57,7 @@ export default {
   }),
 
   computed: {
-    menuProps () {
+    wMenuProps () {
       return {
         top: this.top,
         bottom: this.bottom,
@@ -57,7 +68,8 @@ export default {
         alignLeft: this.alignLeft,
         alignRight: this.alignRight,
         persistent: this.persistent,
-        transition: this.transition
+        transition: this.transition,
+        ...this.menuProps
       }
     },
     buttonProps () {
@@ -74,8 +86,8 @@ export default {
       this.$emit('cancel')
       this.showPopup = false
     },
-    onAccept () {
-      this.$emit('accept')
+    onConfirm () {
+      this.$emit('confirm')
       this.showPopup = false
     }
   }
