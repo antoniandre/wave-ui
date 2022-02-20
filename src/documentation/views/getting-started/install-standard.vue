8290824e02dff28cf5c3fcab6cd271e8e1788714
antoni andre <antoniandre.web@gmail.com>
2022-02-20 07:47:38 -0500

    Update the documentation standard install steps.

--- src/documentation/views/getting-started/install-standard.vue
+++ src/documentation/views/getting-started/install-standard.vue
@@ -2,13 +2,30 @@
 div
   title-link(h3) 1. Download from NPM
   .ml5
-    ssh-pre.vue-install(language="shell") npm i wave-ui # Vue 2.x.
-    ssh-pre.vue-install(language="shell") npm i wave-ui@next # Vue 3.
+    ssh-pre.vue-install.mb0(language="shell") npm i wave-ui # Vue 3.
+    .w-flex.align-center
+      | or
+      ssh-pre.vue-install.grow.ml2(language="shell") npm i wave-ui@legacy # Vue 2.x.
 
   title-link(h3) 2. Import in your project
   .ml5
     p Import Wave UI library at the root of your app - usually #[span.code main.js].
     w-flex(basis-zero wrap :gap="4")
+      ssh-pre.grow.ma2.mt8(language="js" label="VUE 3 - main.js" style="min-width: 290px").
+        // VUE 3.
+        import { createApp } from 'vue'
+        import App from './App.vue'
+        import WaveUI from 'wave-ui'
+        import 'wave-ui/dist/wave-ui.css'
+
+        const app = createApp(App)
+
+        new WaveUI(app, {
+          // Some Wave UI options.
+        })
+
+        app.mount('#app')
+
       ssh-pre.grow.ma2.mt8(language="js" label="VUE 2.x - main.js" style="min-width: 290px").
         // VUE 2.x.
         import Vue from 'vue'
@@ -26,27 +43,18 @@ div
           waveui,
           render: h => h(App)
         }).$mount('#app')
-      ssh-pre.grow.ma2.mt8(language="js" label="VUE 3 - main.js" style="min-width: 290px").
-        // VUE 3.
-        import { createApp } from 'vue'
-        import App from './App.vue'
-        import WaveUI from 'wave-ui'
-        import 'wave-ui/dist/wave-ui.css'
 
-        const app = createApp(App)
-
-        new WaveUI(app, {
-          // Some Wave UI options.
-        })
-
-        app.mount('#app')
-
-    p.mt2.grey-light1.text-right
+    p.mt2.grey-light1
       w-icon.mr1 wi-chevron-right
       | Edit a
-      a.ml1(href="https://codepen.io/antoniandre/pen/RwaXMLd?editors=1010" target="_blank")
-        | Vue 3 Wave UI demo on Codepen
+      a.mx1(href="https://codepen.io/antoniandre/pen/RwaXMLd?editors=1010" target="_blank")
+        | #[strong Vue 3] Wave UI demo on Codepen
         w-icon.ml1 mdi mdi-open-in-new
+      | or a
+      a.ml1(href="https://codepen.io/antoniandre/pen/KKyoKrZ?editors=1010" target="_blank")
+        | #[strong Vue 2] Wave UI demo on Codepen
+        w-icon.ml1 mdi mdi-open-in-new
+      | .
 
   title-link(h3).mt8 3. Place a #[span.code w-app] at the root of your app
   .ml5
