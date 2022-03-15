5a29ee41f42f7723598c4f71852a9ad99f577a1f
antoni andre <antoniandre.web@gmail.com>
2022-03-15 18:31:25 -0400

    Cleanup.

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,11 @@
 <template lang="pug">
-router-view.grow(v-if="$route.name === 'home'")
-router-view.grow(v-else-if="$route.name === 'test'")
+router-view.grow(v-if="['home', 'test'].includes($route.name)")
 documentation(v-else)
 </template>
 
 <script>
-import Documentation from '@/documentation/index.vue'
-
 export default {
-  components: { Documentation },
+  components: { Documentation: () => import('@/documentation/index.vue') },
 
   created () {
     this.$store.commit('initUsePug')
