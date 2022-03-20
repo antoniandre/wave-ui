3dd8d78093634fe0ac7208bd9c82f65a02c5ea66
antoni andre <antoniandre.web@gmail.com>
2022-03-19 20:40:42 -0400

    Update the testing playground for issue 82.

--- src/documentation/views/test.vue
+++ src/documentation/views/test.vue
@@ -2,49 +2,23 @@
 w-app
   .test.ma12.pa12.bd1.bdrs2.blue-light5--bg
     h1.mt0 Testing playground
-    .tabs-multi-mount
-      w-tabs(:items="tabs")
-        //- template(#item-content.1="{ item }")
-          component(:is="item.component")
-        //- template(#item-content.2="{ item }")
-          component(:is="item.component")
-        //- template(#item-content.3="{ item }")
-          component(:is="item.component")
-        template(#item-content="{ item, index }")
-          component(:is="item.component" :key="index")
+    .select-in-dialog
+      w-button(@click="showDialog = true") open
+      w-dialog(v-model="showDialog")
+        w-checkbox test
+        w-select(:items="items" label="Select" :menu-props="{ appendTo: '.w-dialog' }")
 </template>
 
 <script>
-const Component1 = {
-  template: '<div>Component 1</div>',
-  created () {
-    console.log('Component 1 created.')
-  }
-}
-
-const Component2 = {
-  template: '<div>Component 2</div>',
-  created () {
-    console.log('Component 2 created.')
-  }
-}
-
-const Component3 = {
-  template: '<div>Component 3</div>',
-  created () {
-    console.log('Component 3 created.')
-  }
-}
-
 export default {
-  components: { Component1, Component2, Component3 },
   data: () => ({
-    tabs: [
-      { title: 'Tab 1', component: 'component1', content: 'test content 1' },
-      { title: 'Tab 2', component: 'component2', content: 'test content 2' },
-      { title: 'Tab 3', component: 'component3', content: 'test content 3' }
-    ]
-  })
+    showDialog: true,
+    ref: null,
+    items: [{ value: 'value 1', label: 'Item 1' }, { value: 'value 2', label: 'Item 2' }]
+  }),
+  mounted () {
+    this.ref = this.$refs['test-here']
+  }
 }
 </script>
 
