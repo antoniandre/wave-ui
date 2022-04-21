fa768849d0904143657589c741a48f333e5b7a2b
antoni andre <antoniandre.web@gmail.com>
2022-04-21 19:31:38 -0400

    Update the release notes.

--- src/documentation/views/release-notes.vue
+++ src/documentation/views/release-notes.vue
@@ -22,7 +22,7 @@ main
 
     //- #[strong Form elements]: li Added the #[code hide-error] option on all the form elements.
     li.patch
-      strong.version v1.36.1
+      strong.version v1.36.3
       ul
         li #[strong.code w-textarea]: fix an issue with the auto-resize after value update. #[issue-link 88]
 
