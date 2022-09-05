d4ac0fa4910ed9740a0d3e13931cc4215a4d53b7
antoniandre <antoniandre.web@gmail.com>
2022-09-05 20:28:49 +0200

    Revert to the standard WaveUI install temporarily.

--- src/wave-ui/index.js
+++ src/wave-ui/index.js
@@ -4,4 +4,4 @@ import * as components from './components'
 const install = WaveUI.install
 WaveUI.install = (app, options = {}) => install.call(WaveUI, app, { components, ...options })
 
-export { default, useWaveUI } from './core'
+export default WaveUI
