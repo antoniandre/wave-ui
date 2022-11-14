3ddae1eef68849a6d8d59f27cb62af20dafa817f
antoniandre <antoniandre.web@gmail.com>
2022-11-14 16:57:56 +0100

    Cleanup.

--- src/documentation/components/example/index.vue
+++ src/documentation/components/example/index.vue
@@ -117,8 +117,8 @@ export default {
     getSlotChildrenText (children) {
       return children.map(node => {
         if (!node.children || typeof node.children === 'string') return node.children || ''
-        else if (Array.isArray(node.children)) return getSlotChildrenText(node.children)
-        else if (node.children.default) return getSlotChildrenText(node.children.default())
+        else if (Array.isArray(node.children)) return this.getSlotChildrenText(node.children)
+        else if (node.children.default) return this.getSlotChildrenText(node.children.default())
       }).join('')
     },
 
