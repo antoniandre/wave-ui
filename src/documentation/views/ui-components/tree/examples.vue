a806588ff51cdd4c7f785208d7fe79ddac1effa4
antoniandre <antoniandre.web@gmail.com>
2022-12-08 12:06:59 +0100

    Update the w-tree component examples.

--- src/documentation/views/ui-components/tree/examples.vue
+++ src/documentation/views/ui-components/tree/examples.vue
@@ -47,6 +47,116 @@ div
         ]
       })
 
+  title-link(h2) No transition
+  example
+    w-tree(:data="tree1" no-transition)
+    template(#pug).
+      w-tree(:data="tree" no-transition)
+    template(#html).
+      &lt;w-tree:data="tree" no-transition&gt;&lt;/w-tree&gt;
+    template(#js).
+      data: () => ({
+        tree: [
+          {
+            label: 'src',
+            children: [
+              {
+                label: 'assets',
+                children: [
+                  { label: 'wave.svg' },
+                ]
+              },
+              { label: 'views' },
+              { label: 'index.vue' },
+              { label: 'router.vue' },
+              { label: 'main.js' },
+              {
+                label: 'scss',
+                children: [
+                  { label: 'index.scss' },
+                  { label: '_variables.scss' },
+                  { label: '_base.scss' }
+                ]
+              },
+              { label: 'store.js' },
+            ]
+          },
+          { label: '.editorconfig' },
+          { label: '.gitignore' },
+          { label: 'index.html' },
+          { label: 'jsconfig.json' },
+          { label: 'LICENSE' },
+          { label: 'package.json' },
+          { label: 'pnpm-lock.yaml' },
+          { label: 'postcss.config.js' },
+          { label: 'README.md' },
+          { label: 'vite.config.js' }
+        ]
+      })
+
+  title-link(h2) Events
+  example
+    w-flex(gap="12")
+      w-tree(
+        :data="tree1"
+        @before-open="log($event)"
+        @before-close="log($event)")
+      w-card(bg-color="amber-light5")
+        pre {{ logs }}
+    template(#pug).
+      w-tree(
+        :data="tree"
+        @before-open="log($event)"
+        @before-close="log($event)")
+    template(#html).
+      &lt;w-tree
+      :data="tree"
+        @before-open="log($event)"
+        @before-close="log($event)"&gt;&lt;/w-tree&gt;
+    template(#js).
+      data: () => ({
+        tree: [
+          {
+            label: 'src',
+            children: [
+              {
+                label: 'assets',
+                children: [
+                  { label: 'wave.svg' },
+                ]
+              },
+              { label: 'views' },
+              { label: 'index.vue' },
+              { label: 'router.vue' },
+              { label: 'main.js' },
+              {
+                label: 'scss',
+                children: [
+                  { label: 'index.scss' },
+                  { label: '_variables.scss' },
+                  { label: '_base.scss' }
+                ]
+              },
+              { label: 'store.js' },
+            ]
+          },
+          { label: '.editorconfig' },
+          { label: '.gitignore' },
+          { label: 'index.html' },
+          { label: 'jsconfig.json' },
+          { label: 'LICENSE' },
+          { label: 'package.json' },
+          { label: 'pnpm-lock.yaml' },
+          { label: 'postcss.config.js' },
+          { label: 'README.md' },
+          { label: 'vite.config.js' }
+        ]
+      }),
+
+      methods: {
+        log: (...args) => console.log(...args)
+      }
+
 </template>
 
 <script>
@@ -87,8 +197,16 @@ export default {
       { label: 'postcss.config.js' },
       { label: 'README.md' },
       { label: 'vite.config.js' }
-    ]
-  })
+    ],
+    logs: []
+  }),
+
+  methods: {
+    log (...args) {
+      console.log(...args)
+      this.logs = args
+    }
+  }
 }
 </script>
 
