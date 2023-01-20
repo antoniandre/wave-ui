0d705b95b0c05de3dc98a899211609c11cc00068
antoniandre <antoniandre.web@gmail.com>
2023-01-20 19:38:14 +0100

    Export `useWaveUI` from the root for use with comp API.

--- src/wave-ui/index.js
+++ src/wave-ui/index.js
@@ -4,4 +4,5 @@ import * as components from './components'
 const install = WaveUI.install
 WaveUI.install = (app, options = {}) => install.call(WaveUI, app, { components, ...options })
 
+export { useWaveUI } from './core'
 export default WaveUI
