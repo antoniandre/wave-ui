e5c57c9356fa9910dd87133d68918ba985041793
antoniandre <antoniandre.web@gmail.com>
2023-01-26 19:42:06 +0100

    Move the `wApp` CSS to the root.

--- src/wave-ui/components/w-app.vue
+++ src/wave-ui/components/w-app.vue
@@ -6,6 +6,7 @@
 <script>
 export default {
   name: 'w-app',
+  // Keep the props for the API documentation.
   props: {
     block: { type: Boolean },
     row: { type: Boolean },
@@ -21,26 +22,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import '../scss/index';
-
-.w-app {
-  position: relative; // Make the .w-app a referential for tooltips / menus.
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-
-  &.row {flex-direction: row;}
-  &.d-block {display: block;}
-  &.align-center {align-items: center;}
-  &.align-end {align-items: flex-end;}
-  &.justify-center {justify-content: center;}
-  &.justify-end {justify-content: flex-end;}
-  &.justify-space-between {justify-content: space-between;}
-  &.justify-space-around {justify-content: space-around;}
-  &.justify-space-evenly {justify-content: space-evenly;}
-  &.text-center {text-align: center;}
-  &.text-right {text-align: right;}
-}
-</style>
