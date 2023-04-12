21db4d4d49161db9f2232af0e21c56b1b5787755
antoniandre <antoniandre.web@gmail.com>
2023-04-12 11:51:46 +0200

    Cleanup.

--- src/documentation/views/release-notes.vue
+++ src/documentation/views/release-notes.vue
@@ -21,7 +21,7 @@ main
     //-----------------------------------------------------
 
     //- #[strong Form elements]: li Added the #[code hide-error] option on all the form elements.
-    li.minor
+    li.patch
       strong.version v3.1.1
       ul
         li
