680102ccb83ec5f1799663f5904fbd6533cf193d
antoniandre <antoniandre.web@gmail.com>
2023-04-12 11:55:08 +0200

    Publish documentation on Github.

---
+++ docs/assets/404.44d82761.js
@@ -0,0 +1,1 @@
+import{g as o,a as t,w as n,e as a,r as s,o as r,b as c,_ as l}from"./index.b0d51f4b.js";const _=a("p",null,"The page you are looking for was not found on the server.",-1);function i(f,u){const e=s("title-link");return r(),o("main",null,[t(e,{class:"mt4",h1:""},{default:n(()=>[c("404 - Not found")]),_:1}),_])}const d={},p=l(d,[["render",i]]);export{p as default};
