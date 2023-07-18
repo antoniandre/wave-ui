eec379ced78830e1f847395fc957ae0045c294fa
antoniandre <antoniandre.web@gmail.com>
2023-07-18 13:20:12 +0200

    Update the w-tabs test case.

--- src/documentation/views/test.vue
+++ src/documentation/views/test.vue
@@ -6,9 +6,20 @@
   w-button.ma2(@click="addTab(1)") inject tab #2
   w-button.ma2(@click="addTab()") Append tab
   w-button.ma2(@click="restoreTab()") Restore last deleted tab
-  w-tabs(v-model="currTab" :items="tabs")
+  w-button.ma2(@click="replaceTab(1)") Replace tab #1
+  w-tabs(v-model="currTab" :items="tabs" :keep-alive="false")
     template(#item-content="{ item }")
-      pre {{ item }}
+      w-checkbox(v-if="item.id2 === 1")
+      iframe(
+        v-if="item.id2 === 2"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/gzmA1kkk660"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen)
+  //- w-tabs.mt12(v-model="currTab" :items="tabs" keep-in-dom)
+    template(#item-content="{ item }")
       iframe(
         v-if="item.id === 2"
         width="560"
@@ -28,11 +39,11 @@
 <script>
 export default {
   data: () => ({
-    currTab: 0,
+    currTab: 2,
     tabs: [
-      { id: 1, title: 'Tab 1', content: 'Tab 1 content.' },
-      { id: 2, title: 'Tab 2', content: 'Tab 2 content.' },
-      { id: 3, title: 'Tab 3 has a long title', content: 'Tab 3 content.' }
+      { id2: 1, title: 'Tab 1', content: 'Tab 1 content.' },
+      { id2: 2, title: 'Tab 2', content: 'Tab 2 content.' },
+      { id2: 3, title: 'Tab 3 has a long title', content: 'Tab 3 content.' }
     ],
     deletedTabs: []
   }),
@@ -45,7 +56,10 @@ export default {
       this.tabs.push(this.deletedTabs[0])
     },
     addTab (index) {
-      this.tabs.splice(index ?? this.tabs.length, 0, { title: `Tab ${this.tabs.length}`, content: `Tab ${this.tabs.length} content.` })
+      this.tabs.splice(index ?? this.tabs.length, 0, { title: `injected tab ${this.tabs.length}`, content: `injected Tab ${this.tabs.length} content.` })
+    },
+    replaceTab (index) {
+      this.tabs.splice(index, 1, { title: `Replaced tab ${this.tabs.length}`, content: `Replaced Tab ${this.tabs.length} content.` })
     }
   }
 }
