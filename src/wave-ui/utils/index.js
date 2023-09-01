9fa39377cafcfc99a3bb2286f677e64e59b627da
Luke Nelson <luke@nelson.zone>
2023-09-01 10:22:03 +0100

    Example JSDoc

--- src/wave-ui/utils/index.js
+++ src/wave-ui/utils/index.js
@@ -1,8 +1,7 @@
 /**
  * Takes CSS classes as a string array or object and turn them into an object.
- *
- * @param {String|Array|Object} classes the CSS classes to merge into an object
- * @return {Object}
+ * @param {string|Array<string>|object} classes the CSS classes to merge into an object
+ * @returns {object} the CSS classes as an object
  */
 export const objectifyClasses = (classes = {}) => {
   if (typeof classes === 'string') classes = { [classes]: true }
