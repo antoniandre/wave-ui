4a8816cfa6f5b7953b4c2c4796a3f601698abe8f
antoniandre <antoniandre.web@gmail.com>
2023-09-29 10:05:43 +0200

    Update the release notes.

--- src/documentation/views/release-notes.vue
+++ src/documentation/views/release-notes.vue
@@ -22,6 +22,14 @@ main
 
     //- #[strong Form elements]: li Added the #[code hide-error] option on all the form elements.
 
+    li.patch
+      strong.version v3.7.1
+      ul
+        li
+          strong.code w-table
+          ul
+            li Fixed the default row selection color.
+
     li.minor
       strong.version v3.7.0
       ul
