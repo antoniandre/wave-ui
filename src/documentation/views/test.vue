38b11fb01db7c851fd1d7e2c61eaa2a940377cea
antoniandre <antoniandre.web@gmail.com>
2023-11-08 21:47:39 +0100

    Update the test case.

--- src/documentation/views/test.vue
+++ src/documentation/views/test.vue
@@ -1,28 +1,21 @@
 <template lang="pug">
 .test.ma12.pa12.bd1.bdrs2
   h1.mt0.mb8 Testing playground
-  p {{ file }}
-  w-form(v-model="valid" :no-keyup-validation="true")
-    w-select(
-      class="mt3"
-      label="Last name"
-      :items="items"
-      :validators="[validators.required]")
-    //- code {{ valid === false ? 'false' : valid || 'null' }}
-    //- w-button(type="submit" :disabled="valid === false") Validate
-
-    w-input(v-model="file" type="file" multiple)
-    w-button(@click="file = null") clear
+  w-select.mt3(
+    :items="items"
+    label="Select an item"
+    autocomplete)
+  p.my12.mb4 multiple
+  w-select.mt3(
+    :items="items"
+    label="Select an item"
+    autocomplete
+    multiple)
 </template>
 
 <script>
 export default {
   data: () => ({
-    file: null,
-    valid: null,
-    validators: {
-      required: value => !!value || 'This field is required'
-    },
     items: [
       { label: 'Item 1', value: 1, disabled: true },
       { label: 'Item 2', value: 2 },
