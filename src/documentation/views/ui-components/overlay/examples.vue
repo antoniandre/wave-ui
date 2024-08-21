e27680f6c5fe781b1121e6db1f5dd6fd1cb645d3
antoniandre <antoniandre.web@gmail.com>
2024-08-21 15:54:21 +0200

    Update the documentation.

--- src/documentation/views/ui-components/overlay/examples.vue
+++ src/documentation/views/ui-components/overlay/examples.vue
@@ -38,12 +38,12 @@ div
       :persistent="persistent"
       :persistent-no-animation="persistentNoAnimation"
       :opacity="opacity")
-      w-button(bg-color="primary" lg dark @click="showOverlay = false")
+      w-button(lg @click="showOverlay = false")
         w-icon.mr2 wi-cross
         | Hide overlay
     template(#pug).
       w-flex(wrap)
-        w-button(bg-color="primary" dark @click="showOverlay = true") Show overlay
+        w-button(@click="showOverlay = true") Show overlay
 
         w-divider.mx6(vertical)
         div
@@ -70,12 +70,12 @@ div
         :persistent="persistent"
         :persistent-no-animation="persistentNoAnimation"
         :opacity="opacity")
-        w-button(bg-color="primary" lg dark @click="showOverlay = false")
+        w-button(lg @click="showOverlay = false")
           w-icon.mr2 wi-cross
           | Hide overlay
     template(#html).
       &lt;w-flex wrap&gt;
-        &lt;w-button bg-color="primary" dark @click="showOverlay = true"&gt;
+        &lt;w-button @click="showOverlay = true"&gt;
           Show overlay
         &lt;/w-button&gt;
 
@@ -120,10 +120,7 @@ div
         :persistent="persistent"
         :persistent-no-animation="persistentNoAnimation"
         :opacity="opacity"&gt;
-        &lt;w-button
-          bg-color="primary"
-          lg
-          @click="showOverlay = false"&gt;
+        &lt;w-button lg @click="showOverlay = false"&gt;
           &lt;w-icon class="mr2"&gt;wi-cross&lt;/w-icon&gt;
           Hide overlay
         &lt;/w-button&gt;
@@ -138,34 +135,28 @@ div
 
   title-link(h2) Overlay with custom background color
   example(content-class="w-flex align-center")
-    w-button(bg-color="primary" dark @click="showBlueOverlay = true") Show a blue overlay
+    w-button(@click="showBlueOverlay = true") Show a blue overlay
 
     w-overlay(v-model="showBlueOverlay" bg-color="rgba(35, 71, 129, 0.4)")
-      w-button(bg-color="primary" lg dark @click="showBlueOverlay = false")
+      w-button(lg @click="showBlueOverlay = false")
         w-icon.mr2 wi-cross
         | Hide overlay
     template(#pug).
-      w-button(bg-color="primary" dark @click="showOverlay = true") Show a blue overlay
+      w-button(@click="showOverlay = true") Show a blue overlay
 
       w-overlay(v-model="showOverlay" bg-color="rgba(35, 71, 129, 0.4)")
-        w-button(bg-color="primary" lg dark @click="showOverlay = false")
+        w-button(lg @click="showOverlay = false")
           w-icon.mr2 wi-cross
           | Hide overlay
     template(#html).
-      &lt;w-button
-        bg-color="primary"
-        dark
-        @click="showOverlay = true"&gt;
+      &lt;w-button @click="showOverlay = true"&gt;
         Show a blue overlay
       &lt;/w-button&gt;
 
       &lt;w-overlay
         v-model="showOverlay"
         bg-color="rgba(35, 71, 129, 0.4)"&gt;
-        &lt;w-button
-          bg-color="primary"
-          lg
-          @click="showOverlay = false"&gt;
+        &lt;w-button lg @click="showOverlay = false"&gt;
           &lt;w-icon class="mr2"&gt;wi-cross&lt;/w-icon&gt;
           Hide overlay
         &lt;/w-button&gt;
@@ -182,10 +173,10 @@ div
     and very nice.
   ssh-pre(language="css" :dark="$store.state.darkMode").
     .w-overlay {backdrop-filter: blur(10px);}
-  w-button(bg-color="primary" dark @click="showBlurBackdropOverlay = true") Show a blur backdrop
+  w-button(@click="showBlurBackdropOverlay = true") Show a blur backdrop
 
   w-overlay.blur-backdrop(v-model="showBlurBackdropOverlay")
-    w-button(bg-color="primary" lg dark @click="showBlurBackdropOverlay = false")
+    w-button(lg @click="showBlurBackdropOverlay = false")
       w-icon.mr2 wi-cross
       | Hide overlay
 </template>
