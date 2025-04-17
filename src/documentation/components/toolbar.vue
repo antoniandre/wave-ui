8c687a6a155c9199af26c4888251cf57567cf3df
antoniandre <antoniandre.web@gmail.com>
2025-04-17 17:45:52 +0200

    Docs: Add a link to release notes from top bar release number.

--- src/documentation/components/toolbar.vue
+++ src/documentation/components/toolbar.vue
@@ -12,7 +12,8 @@ w-toolbar.main-toolbar(fixed)
     template(#thumb)
       w-icon mdi {{ $store.state.darkMode ? 'mdi-weather-night' : 'mdi-white-balance-sunny' }}
 
-  strong.version.size--xs(v-html="`v<code>${version}</code>`")
+  router-link(to="/release-notes" @click.native="scrollTop(true)")
+    strong.version.size--xs(v-html="`v<code>${version}</code>`")
 
   w-tooltip(z-index="20" append-to=".main-toolbar")
     template(#activator="{ on }")
